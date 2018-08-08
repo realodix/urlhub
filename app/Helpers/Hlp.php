4853035d1375d8e72a47bfa76055a40be29c00ba
Kei <realodix@yahoo.com>
2018-08-09 02:24:27 +0700

    rev-180808-3

--- app/Helpers/Hlp.php
+++ app/Helpers/Hlp.php
@@ -20,7 +20,7 @@ class Hlp
             ->setErrorCorrection('high')
             ->setForegroundColor(array('r' => 0, 'g' => 0, 'b' => 0, 'a' => 0))
             ->setBackgroundColor(array('r' => 255, 'g' => 255, 'b' => 255, 'a' => 0))
-            ->setLabel('Scan Qr Code')
+            ->setLabel('Scan QR Code')
             ->setLabelFontSize(12)
             ->setImageType(QrCode::IMAGE_TYPE_PNG)
         ;
--- resources/views/short.blade.php
+++ resources/views/short.blade.php
@@ -4,44 +4,48 @@
 
 @section('content')
 <div class="container">
-  <div class="row justify-content-md-cente mt-5">
-    <div class="col">
-      @if (session('msgLinkAlreadyExists'))
-      <div class="alert alert-success">
-        {{ session('msgLinkAlreadyExists') }}
-      </div>
-      @endif
-
-      <ul class="list-inline">
-        <li class="list-inline-item">
-          <i class="far fa-clock"></i>
-          <i>{{ $created_at }}</i>
-        </li>
-        <li class="list-inline-item">
-          <i class="far fa-eye"></i>
-          <i>{{ $views }}</i>
-        </li>
-      <ul>
-      <div class="item-detail--title">{{ $long_url_title }}</div>
-
-      <div class="row mt-3">
-        <div class="col-md-9 content">
-          <div class="row">
-            <div class="col-md">
-              <img src="data:{{$qrCodeData}};base64,{{$qrCodebase64}}" alt="QR Code">
-            </div>
-            <div class="col-md-9">
-              <b>Original URL</b>
-              <div class="item-detail--long-url"><a href="{{ $long_url_href }}" target="_blank" title="{{ $long_url_href }}">{{ $long_url }}</a></div>
-
-              <br>
-              <b>Short URL</b>
-              <div class="item-detail--short-url"><a href="{{ $short_url_href }}" target="_blank">{{ $short_url }}</a></div>
-            </div>
-          </div>
+  <div class="row mt-5">
+  <div class="col-md-9">
+    <div class="row header">
+      <div class="col">
+        @if (session('msgLinkAlreadyExists'))
+        <div class="alert alert-success">
+          {{ session('msgLinkAlreadyExists') }}
         </div>
+        @endif
+
+        <ul class="list-inline">
+          <li class="list-inline-item">
+            <i class="far fa-clock"></i>
+            <i>{{ $created_at }}</i>
+          </li>
+          <li class="list-inline-item">
+            <i class="far fa-eye"></i>
+            <i>{{ $views }}</i>
+          </li>
+        </ul>
+        <div class="title">{{ $long_url_title }}</div>
       </div>
     </div>
+  </div>
+  </div>
+
+  <div class="row mt-3">
+  <div class="col-md-9">
+    <div class="row content">
+      <div class="col-md">
+        <img src="data:{{$qrCodeData}};base64,{{$qrCodebase64}}" alt="QR Code">
+      </div>
+      <div class="col-md-9">
+        <b>Original URL</b>
+        <div class="long-url"><a href="{{ $long_url_href }}" target="_blank" title="{{ $long_url_href }}">{{ $long_url }}</a></div>
+
+        <br>
+        <b>Short URL</b>
+        <div class="short-url"><a href="{{ $short_url_href }}" target="_blank">{{ $short_url }}</a></div>
+      </div>
+    </div>
+  </div>
   </div>
 </div>
 @endsection
