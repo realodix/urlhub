6dbc8a08c3bdc32c96016fe053daca56bb7b2b1d
Kei <realodix@yahoo.com>
2018-08-11 02:25:38 +0700

    rev-180810

--- app/Http/Controllers/UrlController.php
+++ app/Http/Controllers/UrlController.php
@@ -15,41 +15,33 @@ class UrlController extends Controller
 {
     public function create(Requests\StoreUrl $request)
     {
-        $getLongURL = Input::get('long_url');
-        $shortUrlCustom = Input::get('short_url_custom');
+        $long_url = Input::get('long_url');
         $short_url = UrlHlp::url_generator();
+        $short_url_custom = Input::get('short_url_custom');
 
-        // if (UrlHlp::domainBlocked($getLongURL) == true) {
-        //     return back()->with('msgDomainBlocked', 'Sorry, we cannot continue. We believe the URL you submitted has been shortened by a similar service.');
-        // }
-
-        $getUrlInDB = Url::where('long_url', $getLongURL)->first();
-        if ($getUrlInDB == true) {
-            return redirect('/+'.$getUrlInDB->short_url)->with('msgLinkAlreadyExists', 'Link already exists');
+        $s_url = Url::where('long_url', $long_url)->first();
+        if ($s_url) {
+            return redirect('/+'.$s_url->short_url)->with('msgLinkAlreadyExists', 'Link already exists');
         }
 
-        $cst = Url::where('short_url_custom', $shortUrlCustom)->first();
-        if ($cst == true) {
+        $s_url_cst = Url::where('short_url_custom', $short_url_custom)->first();
+        if ($s_url_cst) {
             return back()->with('cst_exist', 'Sorry');
         }
 
-        if ($shortUrlCustom) {
-            $blabla = $shortUrlCustom;
-        } else {
-            $blabla = $short_url;
-        }
+        $shortUrl = $short_url_custom ?? $short_url;
 
         Url::create([
             'users_id'          => 0,
-            'long_url'          => $getLongURL,
-            'long_url_title'    => UrlHlp::get_title($getLongURL),
+            'long_url'          => $long_url,
+            'long_url_title'    => UrlHlp::get_title($long_url),
             'short_url'         => $short_url,
-            'short_url_custom'  => $shortUrlCustom ?? 0,
+            'short_url_custom'  => $short_url_custom ?? 0,
             'views'             => 0,
             'ip'                => $request->ip(),
         ]);
 
-        return redirect('/+'.$blabla);
+        return redirect('/+'.$shortUrl);
     }
 
     public function view($link)
