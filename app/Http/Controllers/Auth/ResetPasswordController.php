1d4d2ba031672793bd80ed061e0248f3b8a26bf7
Vu Quang Tam <tam@putatu.com>
2018-09-06 19:47:07 +0700

    Fix redirect after register, reset password to /home - page not found

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -26,7 +26,7 @@ class ResetPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
