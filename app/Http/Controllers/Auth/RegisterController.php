c02e66ba1a48bef7d86080461eb1bf8b42e93fdf
Kei <realodix@yahoo.com>
2018-11-30 14:18:35 +0700

    Name: max-lenght 50

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -60,7 +60,7 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name'     => ['required', 'string', 'max:255'],
+            'name'     => ['required', 'string', 'max:50'],
             'email'    => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:6', 'confirmed'],
         ]);
