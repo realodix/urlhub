d562e61df8bedf0d80d0ad1d5c05d41d2ae5058b
Kei <realodix@yahoo.com>
2018-12-02 02:17:33 +0700

    Apply fixes from StyleCI (#48)

--- app/Helpers/NumHlp.php
+++ app/Helpers/NumHlp.php
@@ -14,40 +14,40 @@ class NumHlp
      */
     public function readable_int(int $n)
     {
-        if ($n >= 0 && $n < pow(10,3)) {
+        if ($n >= 0 && $n < pow(10, 3)) {
             // 1 - 999
             $n_format = floor($n);
             $suffix = '';
-        } elseif ($n >= pow(10,3) && $n < pow(10,6)) {
+        } elseif ($n >= pow(10, 3) && $n < pow(10, 6)) {
             // 1k-999k
-            $n_format = round($n / pow(10,3));
+            $n_format = round($n / pow(10, 3));
             $suffix = 'K+';
 
-            if (($n/pow(10,3) == 1) || ($n/pow(10,4) == 1) || ($n/pow(10,5) == 1)) {
+            if (($n / pow(10, 3) == 1) || ($n / pow(10, 4) == 1) || ($n / pow(10, 5) == 1)) {
                 $suffix = 'K';
             }
-        } elseif ($n >= pow(10,6) && $n < pow(10,9)) {
+        } elseif ($n >= pow(10, 6) && $n < pow(10, 9)) {
             // 1m-999m
-            $n_format = round($n / pow(10,6));
+            $n_format = round($n / pow(10, 6));
             $suffix = 'M+';
 
-            if (($n/pow(10,6) == 1) || ($n/pow(10,7) == 1) || ($n/pow(10,8) == 1)) {
+            if (($n / pow(10, 6) == 1) || ($n / pow(10, 7) == 1) || ($n / pow(10, 8) == 1)) {
                 $suffix = 'M';
             }
-        } elseif ($n >= pow(10,9) && $n < pow(10,12)) {
+        } elseif ($n >= pow(10, 9) && $n < pow(10, 12)) {
             // 1b-999b
-            $n_format = round($n / pow(10,9));
+            $n_format = round($n / pow(10, 9));
             $suffix = 'B+';
 
-            if (($n/pow(10,9) == 1) || ($n/pow(10,10) == 1) || ($n/pow(10,11) == 1)) {
+            if (($n / pow(10, 9) == 1) || ($n / pow(10, 10) == 1) || ($n / pow(10, 11) == 1)) {
                 $suffix = 'B';
             }
-        } elseif ($n >= pow(10,12)) {
+        } elseif ($n >= pow(10, 12)) {
             // 1t+
-            $n_format = round($n / pow(10,12));
+            $n_format = round($n / pow(10, 12));
             $suffix = 'T+';
 
-            if (($n/pow(10,12) == 1) || ($n/pow(10,13) == 1) || ($n/pow(10,14) == 1)) {
+            if (($n / pow(10, 12) == 1) || ($n / pow(10, 13) == 1) || ($n / pow(10, 14) == 1)) {
                 $suffix = 'T';
             }
         }
