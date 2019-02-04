672d8526734e277efe2ad6630234bdffcf11058c
Kei <realodix@yahoo.com>
2019-02-05 02:44:09 +0700

    Date mutator for 'email_verified_at' attribute on User model stub

--- app/User.php
+++ app/User.php
@@ -34,6 +34,15 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'email_verified_at',
+    ];
+
     // Relations
     public function url()
     {
