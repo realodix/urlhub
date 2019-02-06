ba60b787d33d67f5e62036be1efc81dd565dc6a6
Kei <realodix@yahoo.com>
2019-02-06 16:11:49 +0700

    Remove extra whitespace

--- config/newt.php
+++ config/newt.php
@@ -3,8 +3,8 @@
 return [
     // Number of symbols in generating unique url_key. If hash_size_1 == hash_size_2,
     // hash_size_2 is automatically declared to be of no value.
-    'hash_size_1'   => env('HASH_SIZE_1', 6), // >= 1
-    'hash_size_2'   => env('HASH_SIZE_2', 7), // >= 0
+    'hash_size_1' => env('HASH_SIZE_1', 6), // >= 1
+    'hash_size_2' => env('HASH_SIZE_2', 7), // >= 0
 
     // Symbols to be used in generating unique url_key.
     'hash_alphabet' => env(
