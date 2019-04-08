f2748cddae5313f9774bb6206f3440af44bcf7b4
Kei <realodix@yahoo.com>
2019-04-09 02:52:10 +0700

    - Version

--- config/app.php
+++ config/app.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'version'     => 'build.190408-3',
+    'version'     => 'build.190408-4',
     'name'        => env('APP_NAME', 'Newt'),
     'description' => env('APP_DESCRIPTION', 'Simplify your links'),
 
