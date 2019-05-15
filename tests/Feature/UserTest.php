44928862af5275ede62f48dea85ea73c123a7b26
Kei <realodix@yahoo.com>
2019-05-15 21:30:28 +0700

    [TEST] Cleanup (#238)

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -19,11 +19,11 @@ class UserTest extends TestCase
      * Login
      */
 
-    public function test_user_can_view_a_login_form()
+    public function testLoginFormDisplayed()
     {
         $response = $this->get('/login');
-        $response->assertSuccessful();
         $response->assertViewIs('frontend.auth.login');
+        $response->assertStatus(200);
     }
 
     /** @test */
@@ -47,10 +47,11 @@ class UserTest extends TestCase
      * Register
      */
 
-    /** @test */
-    public function the_register_route_exists()
+    public function testRegisterFormDisplayed()
     {
-        $this->get('/register')->assertStatus(200);
+        $response = $this->get('/register');
+        $response->assertViewIs('frontend.auth.register');
+        $response->assertStatus(200);
     }
 
     /** @test */
@@ -58,10 +59,7 @@ class UserTest extends TestCase
     {
         $response = $this->post('/register', [
             'name'     => str_repeat('a', 51),
-            'email'    => $this->user->email,
-            'password' => 'secret',
         ]);
-
         $response->assertStatus(302);
         $response->assertSessionHasErrors([
             'name' => 'The name may not be greater than 50 characters.',
@@ -72,10 +70,8 @@ class UserTest extends TestCase
     public function email_should_not_be_too_long()
     {
         $response = $this->post('/register', [
-            'name'  => $this->user->name,
             'email' => str_repeat('a', 247).'@test.com', // 256
         ]);
-
         $response->assertStatus(302);
         $response->assertSessionHasErrors([
             'email' => 'The email may not be greater than 255 characters.',
