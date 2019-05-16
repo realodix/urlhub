5da31f3612ef4e4a8c47c9bc74700ad8bff822a9
Kei <realodix@yahoo.com>
2019-05-17 05:55:17 +0700

    Make shortUrlProtected rule

---
+++ app/Rules/shortUrlProtected.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+
+class shortUrlProtected implements Rule
+{
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        $routes = array_map(
+            function (\Illuminate\Routing\Route $route) {
+                return $route->uri;
+            }, (array) \Route::getRoutes()->getIterator()
+        );
+
+        return $value != in_array($value, $routes);
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'Not available.';
+    }
+}
