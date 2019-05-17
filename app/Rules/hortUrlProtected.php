8e1d4cf4fbfe250a75f2de4cda03d85f22d7c09f
Kei <realodix@yahoo.com>
2019-05-18 03:32:34 +0700

    update

--- app/Rules/hortUrlProtected.php
+++ app/Rules/hortUrlProtected.php
@@ -4,7 +4,7 @@ namespace App\Rules;
 
 use Illuminate\Contracts\Validation\Rule;
 
-class shortUrlProtected implements Rule
+class ShortUrlProtected implements Rule
 {
     /**
      * Create a new rule instance.
