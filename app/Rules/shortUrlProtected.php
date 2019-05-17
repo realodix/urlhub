04496417cb0c11908a1073139064a27920713e64
Kei <realodix@yahoo.com>
2019-05-18 02:06:19 +0700

    Apply fixes from StyleCI (#253)

--- app/Rules/shortUrlProtected.php
+++ app/Rules/shortUrlProtected.php
@@ -4,7 +4,7 @@ namespace App\Rules;
 
 use Illuminate\Contracts\Validation\Rule;
 
-class ShortUrlProtected implements Rule
+class shortUrlProtected implements Rule
 {
     /**
      * Create a new rule instance.
