b28bd270d968120ad386dbcfe6dd211273721a81
Kei <realodix@yahoo.com>
2019-05-18 20:15:25 +0700

    [TEST] Add more tests for auth (#236)
    - **Add** tests/Feature/Auth/ForgotPasswordTest.php
    - **Add** tests/Feature/Auth/LoginTest.php
    - **Add** tests/Feature/Auth/RegisterTest.php
    - **Add** tests/Feature/Auth/ResetPasswordTest.php
    - **Delete** tests/Feature/UserTest.php

---
+++ tests/Feature/Auth/LoginTest.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Tests\TestCase;
+
+class LoginTest extends TestCase
+{
+    protected function successfulLoginRoute()
+    {
+        return route('admin');
+    }
+
+    protected function loginGetRoute()
+    {
+        return route('login');
+    }
+
+    protected function loginPostRoute()
+    {
+        return route('login');
+    }
+
+    protected function logoutRoute()
+    {
+        return route('logout');
+    }
+
+    protected function successfulLogoutRoute()
+    {
+        return '/';
+    }
+
+    protected function guestMiddlewareRoute()
+    {
+        return route('home');
+    }
+
+    public function test_user_can_view_a_login_form()
+    {
+        $response = $this->get($this->loginGetRoute());
+        $response->assertSuccessful();
+        $response->assertViewIs('frontend.auth.login');
+    }
+
+    public function test_user_cannot_view_a_login_form_when_authenticated()
+    {
+        $user = factory(User::class)->make();
+        $response = $this->actingAs($user)->get($this->loginGetRoute());
+        $response->assertRedirect($this->guestMiddlewareRoute());
+    }
+
+    public function test_user_can_login_with_correct_credentials()
+    {
+        $user = factory(User::class)->create([
+            'password' => Hash::make($password = 'i-love-laravel'),
+        ]);
+        $response = $this->post($this->loginPostRoute(), [
+            'identity' => $user->email,
+            'password' => $password,
+        ]);
+        $response->assertRedirect($this->successfulLoginRoute());
+        $this->assertAuthenticatedAs($user);
+    }
+
+    public function test_user_cannot_login_with_incorrect_password()
+    {
+        $user = factory(User::class)->create([
+            'password' => Hash::make('i-love-laravel'),
+        ]);
+        $response = $this->from($this->loginGetRoute())->post($this->loginPostRoute(), [
+            'identity' => $user->email,
+            'password' => 'invalid-password',
+        ]);
+        $response->assertRedirect($this->loginGetRoute());
+        $response->assertSessionHasErrors('error');
+        $this->assertTrue(session()->hasOldInput('identity'));
+        $this->assertFalse(session()->hasOldInput('password'));
+        $this->assertGuest();
+    }
+
+    public function test_unauthenticated_users_cant_access_the_dashboard()
+    {
+        $this->get('/admin')->assertRedirect('/login');
+    }
+
+    public function test_user_cannot_login_with_email_that_does_not_exist()
+    {
+        $response = $this->from($this->loginGetRoute())->post($this->loginPostRoute(), [
+            'identity' => 'nobody@example.com',
+            'password' => 'invalid-password',
+        ]);
+        $response->assertRedirect($this->loginGetRoute());
+        $response->assertSessionHasErrors('error');
+        $this->assertTrue(session()->hasOldInput('identity'));
+        $this->assertFalse(session()->hasOldInput('password'));
+        $this->assertGuest();
+    }
+
+    public function test_user_can_logout()
+    {
+        $this->be(factory(User::class)->create());
+        $response = $this->post($this->logoutRoute());
+        $response->assertRedirect($this->successfulLogoutRoute());
+        $this->assertGuest();
+    }
+
+    public function test_user_cannot_logout_when_not_authenticated()
+    {
+        $response = $this->post($this->logoutRoute());
+        $response->assertRedirect($this->successfulLogoutRoute());
+        $this->assertGuest();
+    }
+}
