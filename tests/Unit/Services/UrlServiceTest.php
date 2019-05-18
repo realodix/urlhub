f3e811d0c0b6ccc8da080e60c4b944d2397a8d14
Kei <realodix@yahoo.com>
2019-05-19 05:15:14 +0700

    Update UrlServiceTest.php

--- tests/Unit/Services/UrlServiceTest.php
+++ tests/Unit/Services/UrlServiceTest.php
@@ -9,13 +9,13 @@ use Tests\TestCase;
 
 class UrlServiceTest extends TestCase
 {
-    protected $url;
+    protected $UrlService;
 
     public function setUp():void
     {
         parent::setUp();
 
-        $this->url = new UrlService();
+        $this->UrlService = new UrlService();
     }
 
     public function test_url_key_capacity()
@@ -23,18 +23,18 @@ class UrlServiceTest extends TestCase
         config()->set('urlhub.hash_alphabet', 'abc');
         config()->set('urlhub.hash_size_1', 0);
         config()->set('urlhub.hash_size_2', 0);
-        $this->assertSame(0, $this->url->url_key_capacity());
+        $this->assertSame(0, $this->UrlService->url_key_capacity());
 
         config()->set('urlhub.hash_alphabet', 'abc');
         config()->set('urlhub.hash_size_1', 1);
         config()->set('urlhub.hash_size_2', 2);
-        $this->assertSame(12, $this->url->url_key_capacity()); // (3^1)+(3^2)
+        $this->assertSame(12, $this->UrlService->url_key_capacity()); // (3^1)+(3^2)
 
         config()->set('urlhub.hash_alphabet', 'abc');
         config()->set('urlhub.hash_size_1', 2);
         config()->set('urlhub.hash_size_2', 2);
         // $alphabet_length^$hash_size_1 or 3^2
-        $this->assertSame(9, $this->url->url_key_capacity());
+        $this->assertSame(9, $this->UrlService->url_key_capacity());
     }
 
     public function test_url_key_capacity_input_negative()
@@ -42,17 +42,17 @@ class UrlServiceTest extends TestCase
         config()->set('urlhub.hash_alphabet', 'abc');
         config()->set('urlhub.hash_size_1', 1);
         config()->set('urlhub.hash_size_2', -2);
-        $this->assertSame(3, $this->url->url_key_capacity());
+        $this->assertSame(3, $this->UrlService->url_key_capacity());
 
         config()->set('urlhub.hash_alphabet', 'abc');
         config()->set('urlhub.hash_size_1', -1);
         config()->set('urlhub.hash_size_2', 2);
-        $this->assertSame(0, $this->url->url_key_capacity());
+        $this->assertSame(0, $this->UrlService->url_key_capacity());
 
         config()->set('urlhub.hash_alphabet', 'abc');
         config()->set('urlhub.hash_size_1', -1);
         config()->set('urlhub.hash_size_2', -2);
-        $this->assertSame(0, $this->url->url_key_capacity());
+        $this->assertSame(0, $this->UrlService->url_key_capacity());
     }
 
     public function test_url_key_capacity_input_number()
@@ -60,12 +60,12 @@ class UrlServiceTest extends TestCase
         config()->set('urlhub.hash_alphabet', 'abc');
         config()->set('urlhub.hash_size_1', 2.7);
         config()->set('urlhub.hash_size_2', 3);
-        $this->assertSame(36, $this->url->url_key_capacity()); // (3^2)+(3^3)
+        $this->assertSame(36, $this->UrlService->url_key_capacity()); // (3^2)+(3^3)
 
         config()->set('urlhub.hash_alphabet', 'abc');
         config()->set('urlhub.hash_size_1', 2);
         config()->set('urlhub.hash_size_2', 3.7);
-        $this->assertSame(36, $this->url->url_key_capacity()); // (3^2)+(3^3)
+        $this->assertSame(36, $this->UrlService->url_key_capacity()); // (3^2)+(3^3)
     }
 
     public function test_url_key_capacity_input_string()
@@ -73,17 +73,17 @@ class UrlServiceTest extends TestCase
         config()->set('urlhub.hash_alphabet', 'abc');
         config()->set('urlhub.hash_size_1', 'string');
         config()->set('urlhub.hash_size_2', 2);
-        $this->assertSame(0, $this->url->url_key_capacity());
+        $this->assertSame(0, $this->UrlService->url_key_capacity());
 
         config()->set('urlhub.hash_alphabet', 'abc');
         config()->set('urlhub.hash_size_1', 2);
         config()->set('urlhub.hash_size_2', 'string');
         // $alphabet_length^$hash_size_1 or 3^2
-        $this->assertSame(9, $this->url->url_key_capacity());
+        $this->assertSame(9, $this->UrlService->url_key_capacity());
 
         config()->set('urlhub.hash_alphabet', 'abc');
         config()->set('urlhub.hash_size_1', 'string');
         config()->set('urlhub.hash_size_2', 'string');
-        $this->assertSame(0, $this->url->url_key_capacity());
+        $this->assertSame(0, $this->UrlService->url_key_capacity());
     }
 }
