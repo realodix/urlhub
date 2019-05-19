45cfef658c9f724390245f32ad4eb6cc5fa05341
Kei <realodix@yahoo.com>
2019-05-20 02:10:55 +0700

    Create LowercaseRuleTest.php

---
+++ tests/Unit/Rule/LowercaseRuleTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Unit\Rule;
+
+use App\Rules\Lowercase;
+use Illuminate\Validation\Rule;
+use Tests\TestCase;
+
+class LowercaseRuleTest extends TestCase
+{
+    protected $rule;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->rule = new Lowercase();
+    }
+}
