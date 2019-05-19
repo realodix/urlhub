104e8e38fd8951826c97dde359aa11d3d52a9776
Kei <realodix@yahoo.com>
2019-05-20 02:32:24 +0700

    Add test for LowercaseRule

--- tests/Unit/Rule/LowercaseRuleTest.php
+++ tests/Unit/Rule/LowercaseRuleTest.php
@@ -15,4 +15,24 @@ class LowercaseRuleTest extends TestCase
 
         $this->rule = new Lowercase();
     }
+
+    /**
+     * @return void
+     */
+    public function testLowercaseRulePass()
+    {
+        $lowercase = 'abc';
+
+        $this->assertTrue($this->rule->passes('test', $lowercase));
+    }
+
+    /**
+     * @return void
+     */
+    public function testLowercaseRuleFail()
+    {
+        $uppercase = 'ABC';
+
+        $this->assertFalse($this->rule->passes('test', $uppercase));
+    }
 }
