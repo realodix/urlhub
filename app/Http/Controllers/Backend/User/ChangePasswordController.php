0e8f4b9cd4b3a1edf8c2280f1cb81200e122c83e
Kei <realodix@yahoo.com>
2019-05-19 21:18:01 +0700

    Update ChangePasswordController.php

--- app/Http/Controllers/Backend/User/ChangePasswordController.php
+++ app/Http/Controllers/Backend/User/ChangePasswordController.php
@@ -50,7 +50,7 @@ class ChangePasswordController extends Controller
         }
 
         $validatedData = $request->validate([
-            'new-password' => 'required|string|min:6|confirmed',
+            'new-password' => ['required', 'string', 'min:6', 'confirmed'],
         ]);
 
         $user->password = Hash::make($request->input('new-password'));
