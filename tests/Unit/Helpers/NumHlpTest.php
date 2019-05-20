7f5498055a94f90c819669e4520e22b62efc4097
Kei <realodix@yahoo.com>
2019-05-20 07:11:58 +0700

    Use @dataProvider

--- tests/Unit/Helpers/NumHlpTest.php
+++ tests/Unit/Helpers/NumHlpTest.php
@@ -7,28 +7,38 @@ use Tests\TestCase;
 
 class NumHlpTest extends TestCase
 {
-    public function test_readable_int()
+    /**
+     * @dataProvider readableInt
+     */
+    public function test_readable_int($expected, $actual)
     {
-        $this->assertSame('1K', readable_int(1000));
-        $this->assertSame('10K', readable_int(10000));
-        $this->assertSame('100K', readable_int(100000));
+        $this->assertSame($expected, $actual);
+    }
 
-        $this->assertSame('1M', readable_int(1000000));
-        $this->assertSame('10M', readable_int(10000000));
-        $this->assertSame('100M', readable_int(100000000));
+    public function readableInt()
+    {
+        return [
+            ['1K', readable_int(1000)],
+            ['10K', readable_int(10000)],
+            ['100K', readable_int(100000)],
 
-        $this->assertSame('1B', readable_int(1000000000));
-        $this->assertSame('10B', readable_int(10000000000));
-        $this->assertSame('100B', readable_int(100000000000));
+            ['1M', readable_int(1000000)],
+            ['10M', readable_int(10000000)],
+            ['100M', readable_int(100000000)],
 
-        $this->assertSame('1T', readable_int(1000000000000));
-        $this->assertSame('10T', readable_int(10000000000000));
-        $this->assertSame('100T', readable_int(100000000000000));
+            ['1B', readable_int(1000000000)],
+            ['10B', readable_int(10000000000)],
+            ['100B', readable_int(100000000000)],
 
-        $this->assertSame('12K+', readable_int(12345));
-        $this->assertSame('12M+', readable_int(12345678));
-        $this->assertSame('1B+', readable_int(1234567890));
-        $this->assertSame('1T+', readable_int(1234567890000));
+            ['1T', readable_int(1000000000000)],
+            ['10T', readable_int(10000000000000)],
+            ['100T', readable_int(100000000000000)],
+
+            ['12K+', readable_int(12345)],
+            ['12M+', readable_int(12345678)],
+            ['1B+', readable_int(1234567890)],
+            ['1T+', readable_int(1234567890000)],
+        ];
     }
 
     public function test_readable_int_input_num()
