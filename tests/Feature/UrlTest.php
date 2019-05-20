f38939caf8fb97b96b27b91d14bcd7195b208307
Kei <realodix@yahoo.com>
2019-05-20 23:42:32 +0700

    Update UrlTest.php

--- tests/Feature/UrlTest.php
+++ tests/Feature/UrlTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use App\Url;
 
 class UrlTest extends TestCase
 {
@@ -28,6 +29,22 @@ class UrlTest extends TestCase
     /**
      * @test
      */
+    public function create_short_url_with_wrong_url()
+    {
+        $long_url = 'wrong-url';
+
+        $response = $this->post(route('createshortlink'), [
+            'long_url' => $long_url,
+        ]);
+
+        $response->assertRedirect(route('home'));
+        $response->assertSessionHasErrors('long_url');
+        $response->assertStatus(302);
+    }
+
+    /**
+     * @test
+     */
     public function create_custom_short_url()
     {
         $long_url = 'https://laravel.com';
@@ -42,5 +59,46 @@ class UrlTest extends TestCase
             'long_url' => $long_url,
             'url_key'  => $custom_url_key,
         ]);
+
+        $response = $this->get(route('home').'/'.$custom_url_key);
+        $response->assertRedirect($long_url);
+    }
+
+    /**
+     * @test
+     */
+    public function short_url_redirect_to_original_url()
+    {
+        $long_url = 'https://laravel.com';
+
+        $this->post(route('createshortlink'), [
+            'long_url' => $long_url,
+        ]);
+
+        $url = Url::whereLongUrl($long_url)
+                    ->first();
+
+        $response = $this->get(route('home').'/'.$url->url_key);
+        $response->assertRedirect($long_url);
+    }
+
+    /**
+     * @test
+     */
+    public function custom_short_url_redirect_to_original_url()
+    {
+        $long_url = 'https://laravel.com';
+        $custom_url_key = 'laravel';
+
+        $this->post(route('createshortlink'), [
+            'long_url' => $long_url,
+            'url_key'  => $custom_url_key,
+        ]);
+
+        $url = Url::whereLongUrl($long_url)
+                    ->first();
+
+        $response = $this->get(route('home').'/'.$url->url_key);
+        $response->assertRedirect($long_url);
     }
 }
