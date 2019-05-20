225b0318fbcf71c596b309c620df91338fb3853a
Kei <realodix@yahoo.com>
2019-05-21 00:03:19 +0700

    Create UserTest.php

---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tests\Feature;
+
+// use App\User;
+// use Illuminate\Foundation\Testing\RefreshDatabase;
+// use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    use RefreshDatabase;
+}
