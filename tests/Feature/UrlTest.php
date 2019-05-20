b597f9e66e164ad046ba8213c23c8fe8d06d9ddd
Kei <realodix@yahoo.com>
2019-05-20 22:53:02 +0700

    Update UrlTest.php

--- tests/Feature/UrlTest.php
+++ tests/Feature/UrlTest.php
@@ -20,9 +20,24 @@ class UrlTest extends TestCase
             'long_url' => $long_url,
         ]);
 
-        // $this->assertEquals($long_url, $url->long_url);
         $this->assertDatabaseHas('urls', [
             'long_url' => $long_url,
+        ]);
+    }
+
+    public function create_custom_short_url()
+    {
+        $long_url = 'https://laravel.com';
+        $custom_url_key = 'laravel';
+
+        $response = $this->post(route('createshortlink'), [
+            'long_url'       => $long_url,
+            'custom_url_key' => $custom_url_key,
+        ]);
+
+        $this->assertDatabaseHas('urls', [
+            'long_url'       => $long_url,
+            'custom_url_key' => $custom_url_key,
         ]);
     }
 }
