a9129f35750424aeaf8247331745165d1f180f15
Kei <realodix@yahoo.com>
2019-05-23 03:07:27 +0700

    Refactor MigrateFreshSeedOnce trait (#274)

--- tests/MigrateFreshSeedOnce.php
+++ tests/MigrateFreshSeedOnce.php
@@ -33,22 +33,14 @@ trait MigrateFreshSeedOnce
         }
     }
 
-    protected function loginAsAdmin()
+    protected function admin()
     {
-        $admin = User::whereName('admin')->first();
-
-        $this->actingAs($admin);
-
-        return $admin;
+        return User::whereName('admin')->first();
     }
 
-    protected function loginAsUser()
+    protected function user()
     {
-        $user = User::whereName('user')->first();
-
-        $this->actingAs($user);
-
-        return $user;
+        return User::whereName('admin')->first();
     }
 
     protected function adminPassword()
@@ -59,5 +51,15 @@ trait MigrateFreshSeedOnce
     protected function userPassword()
     {
         return 'user';
+    }
+
+    protected function loginAsAdmin()
+    {
+        return $this->actingAs($this->admin());
+    }
+
+    protected function loginAsUser()
+    {
+        return $this->actingAs($this->user());
     }
 }
