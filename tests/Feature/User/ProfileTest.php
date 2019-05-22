c1b1e29d47932b8be43aee67381ea64e413b50a8
Kei <realodix@yahoo.com>
2019-05-23 06:22:41 +0700

    Update ProfileTest.php

--- tests/Feature/User/ProfileTest.php
+++ tests/Feature/User/ProfileTest.php
@@ -36,4 +36,78 @@ class ProfileTest extends TestCase
         $response = $this->get($this->profileGetRoute($this->admin()->name));
         $response->assertStatus(403);
     }
+
+    /** @test */
+    public function validation_email_required()
+    {
+        $this->loginAsAdmin();
+
+        $response = $this->from($this->profileGetRoute($this->admin()->name))
+                         ->post($this->profilePostRoute($this->admin()->id), [
+                            'email' => '',
+                         ]);
+
+        $response->assertRedirect($this->profileGetRoute($this->admin()->name));
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors('email');
+    }
+
+    /** @test */
+    public function validation_email_invalid_format()
+    {
+        $this->loginAsAdmin();
+
+        $response = $this->from($this->profileGetRoute($this->admin()->name))
+                         ->post($this->profilePostRoute($this->admin()->id), [
+                            'email' => 'invalid_format',
+                         ]);
+
+        $response->assertRedirect($this->profileGetRoute($this->admin()->name));
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors('email');
+    }
+
+    /** @test */
+    public function validation_email_unique()
+    {
+        $this->loginAsAdmin();
+
+        $response = $this->from($this->profileGetRoute($this->admin()->name))
+                         ->post($this->profilePostRoute($this->admin()->id), [
+                            'email' => $this->user()->email,
+                         ]);
+
+        $response->assertRedirect($this->profileGetRoute($this->admin()->name));
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors('email');
+    }
+
+    /** @test */
+    public function admin_can_change_user_email()
+    {
+        $this->loginAsAdmin();
+
+        $response = $this->from($this->profileGetRoute($this->user()->name))
+                         ->post($this->profilePostRoute($this->user()->id), [
+                            'email' => 'new_email_user@urlhub.test',
+                         ]);
+
+        $response->assertRedirect($this->profileGetRoute($this->user()->name));
+        $response->assertSessionHas(['flash_success']);
+        $this->assertSame('new_email_user@urlhub.test', $this->user()->email);
+    }
+
+    /** @test */
+    public function user_cant_change_admin_email()
+    {
+        $this->loginAsUser();
+
+        $response = $this->from($this->profileGetRoute($this->admin()->name))
+                         ->post($this->profilePostRoute($this->admin()->id), [
+                            'email' => 'new_email_admin@urlhub.test',
+                         ]);
+
+        $response->assertStatus(403);
+        $this->assertSame('admin@urlhub.test', $this->admin()->email);
+    }
 }
