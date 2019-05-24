cc1068106d405b1c3e5c334ce0d6802177c2129a
Kei <realodix@yahoo.com>
2019-05-24 18:51:40 +0700

    Create User & URL Policy test file

---
+++ tests/Unit/Policies/UrlPolicyTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tests\Unit\Policies;
+
+use App\Url;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UrlPolicyTest extends TestCase
+{
+    use RefreshDatabase;
+}
---
+++ tests/Unit/Policies/UserPolicyTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tests\Unit\Policies;
+
+use App\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UserPolicyTest extends TestCase
+{
+    use RefreshDatabase;
+}
