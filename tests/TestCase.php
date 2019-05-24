abafa4cac47571ed10974eff5250499be5685991
Kei <realodix@yahoo.com>
2019-05-25 03:25:03 +0700

    Apply fixes from StyleCI (#284)

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,9 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Tests\Support\Authentication;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\Support\CreatesApplication;
 
 abstract class TestCase extends BaseTestCase
@@ -12,5 +12,4 @@ abstract class TestCase extends BaseTestCase
     use CreatesApplication,
         RefreshDatabase,
         Authentication;
-
 }
