aa67f78e6d0808797afe7371693293b3636c7803
Kei <realodix@yahoo.com>
2019-05-24 21:25:04 +0700

    Update UrlPolicyTest.php

--- tests/Unit/Policies/UrlPolicyTest.php
+++ tests/Unit/Policies/UrlPolicyTest.php
@@ -2,10 +2,23 @@
 
 namespace Tests\Unit\Policies;
 
+use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class UrlPolicyTest extends TestCase
 {
     use RefreshDatabase;
+
+    /** @test */
+    public function admin_forceDelete(User $user, Url $url)
+    {
+        $admin = factory(User::class)->create();
+    }
+
+    /** @test */
+    public function user_forceDelete(User $user, Url $url)
+    {
+        $user = factory(User::class)->create();
+    }
 }
