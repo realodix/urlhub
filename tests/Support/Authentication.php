84755de0e9fd7f5eb70e36226aca6e674d2b3429
Kei <realodix@yahoo.com>
2019-05-24 18:58:30 +0700

    Buat trait Authentication

---
+++ tests/Support/Authentication.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Support;
+
+use App\User;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Tests\TestCase;
+
+trait Authentication
+{
+    /** @var User $user **/
+    protected $user;
+
+    public function setupUser()
+    {
+        $this->user = factory(User::class)->create();
+    }
+
+    public function authenticated(Authenticatable $user = null)
+    {
+        return $this->actingAs($user ?? $this->user);
+    }
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,22 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Tests\Support\Authentication;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    /**
+     * Boot the testing helper traits.
+     *
+     * @return array
+     */
+    protected function setUpTraits()
+    {
+        $uses = parent::setUpTraits();
+        if (isset($uses[Authentication::class])) {
+            $this->setUpUser();
+        }
+    }
 }
