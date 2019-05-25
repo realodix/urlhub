2d7e1f27da06857c89880a92fb8d84d2fceabad7
Kei <realodix@yahoo.com>
2019-05-26 06:14:50 +0700

    [TEST] Add more test for URL (#285)

--- tests/Feature/UrlTest.php
+++ tests/Feature/UrlTest.php
@@ -7,9 +7,7 @@ use Tests\TestCase;
 
 class UrlTest extends TestCase
 {
-    /**
-     * @test
-     */
+    /** @test */
     public function create_short_url()
     {
         $long_url = 'https://laravel.com';
@@ -23,9 +21,96 @@ class UrlTest extends TestCase
         ]);
     }
 
-    /**
-     * @test
-     */
+    /** @test */
+    public function long_url_already_exist()
+    {
+        $long_url = 'https://laravel.com';
+
+        factory(Url::class)->create([
+            'user_id' => null,
+            'long_url' => 'https://laravel.com',
+        ]);
+
+        $url = Url::whereLongUrl($long_url)->first();
+
+        $response = $this->post(route('createshortlink'), [
+            'long_url' => $long_url,
+        ]);
+        $response->assertRedirect(route('home').'/+'.$url->url_key);
+    }
+
+    /** @test */
+    public function long_url_already_exist_2()
+    {
+        $long_url = 'https://laravel.com';
+
+        factory(Url::class)->create([
+            'user_id'  => null,
+            'long_url' => $long_url,
+        ]);
+
+        $this->loginAsUser();
+
+        $response = $this->post(route('createshortlink'), [
+            'long_url' => $long_url,
+        ]);
+
+        $url = Url::whereUserId($this->user()->id)->first();
+        $response->assertRedirect(route('home').'/+'.$url->url_key);
+
+        $count = Url::where('long_url', '=', $long_url)->count();
+        $this->assertSame(2, $count);
+    }
+
+    /** @test */
+    public function long_url_already_exist_3()
+    {
+        $this->loginAsUser();
+
+        $long_url = 'https://laravel.com';
+
+        factory(Url::class)->create([
+            'user_id'  => $this->user()->id,
+            'long_url' => $long_url,
+        ]);
+
+        $response = $this->post(route('createshortlink'), [
+            'long_url' => $long_url,
+        ]);
+        $url = Url::whereUserId($this->user()->id)->first();
+        $response->assertRedirect(route('home').'/+'.$url->url_key);
+        $response->assertSessionHas(['msgLinkAlreadyExists']);
+
+        $count = Url::where('long_url', '=', $long_url)->count();
+        $this->assertSame(1, $count);
+    }
+
+    /** @test */
+    public function duplicate()
+    {
+        $this->loginAsUser();
+
+        $long_url = 'https://laravel.com';
+
+        factory(Url::class)->create([
+            'user_id'  => $this->user()->id,
+            'long_url' => $long_url,
+        ]);
+
+        $this->post(route('createshortlink'), [
+            'long_url' => $long_url,
+        ]);
+
+        $url = Url::whereUserId($this->user()->id)->first();
+
+        $response = $this->from(route('home').'/+'.$url->url_key)
+                         ->get(route('duplicate', $url->url_key));
+
+        $count = Url::where('long_url', '=', $long_url)->count();
+        $this->assertSame(2, $count);
+    }
+
+    /** @test */
     public function create_short_url_with_wrong_url_format()
     {
         $long_url = 'wrong-url-format';
@@ -39,9 +124,7 @@ class UrlTest extends TestCase
         $response->assertStatus(302);
     }
 
-    /**
-     * @test
-     */
+    /** @test */
     public function redirect_to_original_url()
     {
         $long_url = 'https://laravel.com';
@@ -64,9 +147,7 @@ class UrlTest extends TestCase
      |
      */
 
-    /**
-     * @test
-     */
+    /** @test */
     public function cst_create_short_url()
     {
         $long_url = 'https://laravel.com';
@@ -81,30 +162,112 @@ class UrlTest extends TestCase
             'long_url' => $long_url,
             'url_key'  => $custom_url_key,
         ]);
-
-        $response = $this->get(route('home').'/'.$custom_url_key);
-        $response->assertRedirect($long_url);
     }
 
-    /**
-     * @test
-     */
+    /** @test */
     public function cst_long_url_already_exist()
     {
-        $url = factory(Url::class)->make();
-        $custom_url_key = 'hello';
+        $long_url = 'https://laravel.com';
+        $custom_url_key = 'laravel';
 
-        $response = $this->post(route('createshortlink'), [
-            'long_url' => $url->long_url,
-            'custom_url_key'  => $custom_url_key,
+        $this->post(route('createshortlink'), [
+            'long_url'       => $long_url,
+            'custom_url_key' => $custom_url_key,
         ]);
 
-        $response->assertRedirect(route('short_url.stats', $custom_url_key));
+        $long_url_2 = 'https://laravel.com';
+        $custom_url_key_2 = 'laravel2';
+
+        $response = $this->post(route('createshortlink'), [
+            'long_url'       => $long_url_2,
+            'custom_url_key' => $custom_url_key_2,
+        ]);
+        $response->assertRedirect(route('home').'/+'.$custom_url_key);
+
+        $response2 = $this->get(route('home').'/'.$custom_url_key_2);
+        $response2->assertStatus(404);
     }
 
-    /**
-     * @test
-     */
+    /** @test */
+    public function cst_long_url_already_exist_2()
+    {
+        $long_url = 'https://laravel.com';
+        $custom_url_key = 'laravel';
+
+        $this->post(route('createshortlink'), [
+            'long_url'       => $long_url,
+            'custom_url_key' => $custom_url_key,
+        ]);
+
+        $this->loginAsUser();
+
+        $long_url_2 = 'https://laravel.com';
+        $custom_url_key_2 = 'laravel2';
+
+        $response = $this->post(route('createshortlink'), [
+            'long_url'       => $long_url_2,
+            'custom_url_key' => $custom_url_key_2,
+        ]);
+
+        $response->assertRedirect(route('home').'/+'.$custom_url_key_2);
+
+        $response2 = $this->get(route('home').'/'.$custom_url_key_2);
+        $response2->assertRedirect($long_url_2);
+
+        $count = Url::where('long_url', '=', $long_url)->count();
+        $this->assertSame(2, $count);
+    }
+
+    /** @test */
+    public function cst_cst_url_key_already_exist()
+    {
+        $long_url = 'https://laravel.com';
+        $custom_url_key = 'laravel';
+
+        $this->post(route('createshortlink'), [
+            'long_url'       => $long_url,
+            'custom_url_key' => $custom_url_key,
+        ]);
+
+        $this->loginAsUser();
+
+        $response = $this->post(route('createshortlink'), [
+            'long_url'       => 'https://laravel-news.com',
+            'custom_url_key' => $custom_url_key,
+        ]);
+
+        $response
+            ->assertRedirect(route('home'))
+            ->assertSessionHasErrors('custom_url_key');
+
+        $count = Url::where('long_url', '=', $long_url)->count();
+        $this->assertSame(1, $count);
+    }
+
+    /** @test */
+    public function cst_cst_url_key_already_exist_2()
+    {
+        $long_url = 'https://laravel.com';
+        $custom_url_key = 'laravel';
+
+        $this->post(route('createshortlink'), [
+            'long_url'       => $long_url,
+            'custom_url_key' => $custom_url_key,
+        ]);
+
+        $response = $this->post(route('createshortlink'), [
+            'long_url'       => 'https://laravel-news.com',
+            'custom_url_key' => $custom_url_key,
+        ]);
+        $response
+            ->assertRedirect(route('home'))
+            ->assertSessionHasErrors('custom_url_key');
+
+        $count = Url::where('long_url', '=', $long_url)->count();
+        $this->assertSame(1, $count);
+    }
+
+    /** @test */
     public function cst_redirect_to_original_url()
     {
         $long_url = 'https://laravel.com';
--- tests/Feature/User/AllUsersPageTest.php
+++ tests/Feature/User/AllUsersPageTest.php
@@ -2,10 +2,16 @@
 
 namespace Tests\Feature\User;
 
+use App\Url;
 use Tests\TestCase;
 
 class AllUsersPageTest extends TestCase
 {
+    protected function duplicateGetRoute($value)
+    {
+        return route('admin.duplicate', $value);
+    }
+
     /** @test */
     public function admin_can_access_all_users_page()
     {
@@ -22,5 +28,27 @@ class AllUsersPageTest extends TestCase
 
         $response = $this->get(route('user.index'));
         $response->assertStatus(403);
+    }
+
+    /** @test */
+    public function duplicate()
+    {
+        $long_url = 'https://laravel.com';
+
+        factory(Url::class)->create([
+            'user_id' => null,
+            'long_url' => $long_url,
+        ]);
+
+        $url = Url::whereLongUrl($long_url)->first();
+
+        $this->loginAsAdmin();
+
+        $response = $this->from(route('admin.allurl'))
+                         ->get($this->duplicateGetRoute($url->url_key));
+        $response->assertRedirect(route('admin.allurl'));
+
+        $count = Url::where('long_url', '=', $long_url)->count();
+        $this->assertSame(2, $count);
     }
 }
