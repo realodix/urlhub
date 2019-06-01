87e6bc82807be8991a350771ba201c9d0beaf32a
Kei <realodix@yahoo.com>
2019-06-02 03:18:52 +0700

    formatting

--- config/logging.php
+++ config/logging.php
@@ -62,9 +62,9 @@ return [
         ],
 
         'papertrail' => [
-            'driver'       => 'monolog',
-            'level'        => 'debug',
-            'handler'      => SyslogUdpHandler::class,
+            'driver'  => 'monolog',
+            'level'   => 'debug',
+            'handler' => SyslogUdpHandler::class,
             'handler_with' => [
                 'host' => env('PAPERTRAIL_URL'),
                 'port' => env('PAPERTRAIL_PORT'),
@@ -75,7 +75,7 @@ return [
             'driver'    => 'monolog',
             'handler'   => StreamHandler::class,
             'formatter' => env('LOG_STDERR_FORMATTER'),
-            'with'      => [
+            'with' => [
                 'stream' => 'php://stderr',
             ],
         ],
