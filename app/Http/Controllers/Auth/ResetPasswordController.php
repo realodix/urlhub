0d98897149f31a1e87f78cbc36b526fcc7cedb4d
Kei <realodix@yahoo.com>
2019-06-08 06:15:33 +0700

    Redirect authenticated user to dashboard (#317)

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -26,7 +26,7 @@ class ResetPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/';
+    protected $redirectTo = '/admin';
 
     /**
      * Create a new controller instance.
