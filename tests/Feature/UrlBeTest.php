b6cf4e2644b5eae9f11998d2d75dbb2061c5438d
Kei <realodix@yahoo.com>
2019-06-09 15:46:20 +0700

    admin_can_duplicate_own_short_url

--- tests/Feature/UrlBeTest.php
+++ tests/Feature/UrlBeTest.php
@@ -19,30 +19,26 @@ class UrlBeTest extends TestCase
         $response->assertStatus(200);
     }
 
-    // /** @test */
-    // public function admin_can_duplicate()
-    // {
-    //     $this->loginAsUser();
+    /** @test */
+    public function admin_can_duplicate_own_short_url()
+    {
+        $long_url = 'https://laravel.com';
 
-    //     $long_url = 'https://laravel.com';
+        factory(Url::class)->create([
+            'user_id'  => $this->user()->id,
+            'long_url' => $long_url,
+        ]);
 
-    //     factory(Url::class)->create([
-    //         'user_id'  => $this->user()->id,
-    //         'long_url' => $long_url,
-    //     ]);
+        $this->loginAsAdmin();
 
-    //     $this->post(route('createshortlink'), [
-    //         'long_url' => $long_url,
-    //     ]);
+        $url = Url::whereUserId($this->user()->id)->first();
 
-    //     $url = Url::whereUserId($this->user()->id)->first();
+        $response = $this->from(route('dashboard'))
+                         ->get(route('dashboard.duplicate'));
 
-    //     $response = $this->from(route('home').'/+'.$url->url_key)
-    //                      ->get(route('duplicate', $url->url_key));
-
-    //     $count = Url::where('long_url', '=', $long_url)->count();
-    //     $this->assertSame(2, $count);
-    // }
+        $count = Url::where('long_url', '=', $long_url)->count();
+        $this->assertSame(2, $count);
+    }
 
     // /** @test */
     // public function user_can_duplicate()
