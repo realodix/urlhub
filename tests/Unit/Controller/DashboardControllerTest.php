f6a14ef90c75e4e5bde8328b5fcc55dc0a8212ff
Kei <realodix@yahoo.com>
2019-06-09 23:57:44 +0700

    Create DashboardControllerTest.php

---
+++ tests/Unit/Controller/DashboardControllerTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Unit\Controller;
+
+use App\Url;
+use Illuminate\Support\Facades\DB;
+use Tests\TestCase;
+
+class DashboardControllerTest extends TestCase
+{
+    public function setUp():void
+    {
+        parent::setUp();
+
+        factory(Url::class)->create([
+            'user_id'  => 0,
+            'long_url' => 'https://laravel.com',
+            'clicks'   => 10,
+            'ip'       => '0.0.0.0'
+        ]);
+
+        factory(Url::class)->create([
+            'user_id'  => 0,
+            'long_url' => 'https://laravel.com',
+            'clicks'   => 10,
+            'ip'       => '1.1.1.1'
+        ]);
+    }
+
+    /** @test */
+    public function total_guest()
+    {
+        $count = Url::select('ip', DB::raw('count(*) as total'))
+                      ->whereNull('user_id')
+                      ->groupBy('ip')
+                      ->get()
+                      ->count();
+
+        $this->assertSame(2, $count);
+    }
+}
