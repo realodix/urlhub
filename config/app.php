3f3cc9486385deb0bdeb22cf585b13d89b219e1a
Kei <realodix@yahoo.com>
2019-06-10 08:33:07 +0700

    convert a string to an int

--- config/app.php
+++ config/app.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'version'     => 'build.190607',
+    'version'     => 'build.190610',
     'name'        => env('APP_NAME', 'UrlHub'),
     'description' => env('APP_DESCRIPTION', 'Simplify your links'),
 
