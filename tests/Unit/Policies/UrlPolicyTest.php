308ee4d3d784ed14dbebd52ce5cb4a3792eaecc9
Kei <realodix@yahoo.com>
2019-06-11 07:47:46 +0700

    Create UrlPolicyTest

--- tests/Unit/Policies/UrlPolicyTest.php
+++ tests/Unit/Policies/UrlPolicyTest.php
@@ -1,9 +1,38 @@
 <?php
 
-// namespace Tests\Unit\Policies;
+namespace Tests\Unit\Policies;
 
-// use Tests\TestCase;
+use App\Url;
+use App\User;
+use Tests\TestCase;
 
-// class UrlPolicyTest extends TestCase
-// {
-// }
+class UrlPolicyTest extends TestCase
+{
+    /**
+     * Determine whether the user can permanently delete the url.
+     *
+     * @test
+     */
+    public function force_delete_admin()
+    {
+        $this->loginAsAdmin();
+
+        $admin = $this->admin();
+
+        $this->assertTrue($admin->can('forceDelete', new Url));
+    }
+
+    /**
+     * Determine whether the user can permanently delete the url.
+     *
+     * @test
+     */
+    public function force_delete_non_admin()
+    {
+        $this->loginAsUser();
+
+        $non_admin = $this->user();
+
+        $this->assertFalse($non_admin->can('forceDelete', new Url));
+    }
+}
