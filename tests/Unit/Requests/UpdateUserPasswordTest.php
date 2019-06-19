7c8cb6a2e08b4a749d7ae7149694dfce6c82da91
Kei <realodix@yahoo.com>
2019-06-19 18:40:48 +0700

    Create UpdateUserPasswordTest.php

---
+++ tests/Unit/Requests/UpdateUserPasswordTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests\Unit\Requests;
+
+use Tests\TestCase;
+
+class UpdateUserPasswordTest extends TestCase
+{
+    // protected $rule;
+
+    // public function setUp():void
+    // {
+    //     parent::setUp();
+    // }
+
+    // private function createAttributes()
+    // {
+    //     $faker = \Faker\Factory::create();
+
+    //     $attributes = [
+    //         'play_date' => $faker->date('d/m/Y'),
+    //         'reservation_id' => null,
+    //         'am_tee' => $faker->time('H:m a'),
+    //         'howBooked' => 'drop in',
+    //         'ticket' => 905,
+    //         'pay_method' => 'cash',
+    //         'customer_id' => 1532
+    //     ];
+
+    //     return $attributes;
+
+
+    // }
+
+    // public function test_form_validation_passes()
+    // {
+    //     $request = new \App\Http\Requests\UpdateUserPassword();
+    //     $rules = $request->rules();
+
+    //     $attributes = $this->createAttributes();
+    //     $validator = Validator::make($attributes, $rules);
+    //     $passes = $validator->passes();
+
+    //     $this->assertEquals(true, $passes);
+    // }
+}
