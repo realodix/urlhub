baa073332dd9f5a85c1bfa722e59adc8cd34a16d
Kei <realodix@yahoo.com>
2019-06-19 11:49:09 +0700

    Create CurrentPasswordTest.php

---
+++ tests/Unit/Rule/Auth/CurrentPasswordTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Unit\Rule\Auth;
+
+use App\Rules\Auth\CurrentPassword;
+use Tests\TestCase;
+
+class CurrentPasswordTest extends TestCase
+{
+    protected $rule;
+
+    public function setUp():void
+    {
+        parent::setUp();
+
+        $this->rule = new CurrentPassword();
+    }
+
+    /**
+     * @return void
+     */
+    public function testCurrentPasswordPass()
+    {
+        $this->assertTrue($this->rule->passes('test', 'abc'));
+    }
+
+    /**
+     * @return void
+     */
+    public function testCurrentPasswordFail()
+    {
+        $this->assertFalse($this->rule->passes('test', 'ABC'));
+    }
+}
