49694b214e01675aedc81b6a71c71c6bd7356ebe
Kei <realodix@yahoo.com>
2019-06-22 12:50:46 +0700

    Create UrlStat.php

---
+++ app/UrlStat.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UrlStat extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'url_id',
+        'ip',
+    ];
+}
