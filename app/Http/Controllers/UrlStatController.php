510df18be6d415bd932ad7c2bd085ac8b558dff6
Kei <realodix@yahoo.com>
2019-06-22 12:50:58 +0700

    Create UrlStatController.php

---
+++ app/Http/Controllers/UrlStatController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\UrlStat;
+use Illuminate\Http\Request;
+
+class UrlStatController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\UrlStat  $urlStat
+     * @return \Illuminate\Http\Response
+     */
+    public function show(UrlStat $urlStat)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\UrlStat  $urlStat
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(UrlStat $urlStat)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\UrlStat  $urlStat
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, UrlStat $urlStat)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\UrlStat  $urlStat
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(UrlStat $urlStat)
+    {
+        //
+    }
+}
