3068dbe30eba184abca48924be866e6784bc4255
Kei <realodix@yahoo.com>
2019-06-30 12:31:49 +0700

    Update UrlTest.php

--- tests/Unit/Models/UrlTest.php
+++ tests/Unit/Models/UrlTest.php
@@ -171,99 +171,36 @@ class UrlTest extends TestCase
         );
     }
 
-    /** @test */
-    public function key_generator_length()
+    /**
+     * @test
+     * @dataProvider urlKeyCapacityProvider
+     */
+    public function url_key_capacity($size1, $size2, $expected)
     {
-        $this->assertSame(
-            config('urlhub.hash_size_1'),
-            strlen($this->url->key_generator())
-        );
+        config()->set('urlhub.hash_size_1', $size1);
+        config()->set('urlhub.hash_size_2', $size2);
+
+        $this->assertSame($expected, $this->url->url_key_capacity());
     }
 
-    /** @test */
-    public function key_generator_size1_equal_with_size2()
+    public function urlKeyCapacityProvider()
     {
-        config()->set('urlhub.hash_size_1', 2);
-        config()->set('urlhub.hash_size_2', 2);
+        return [
+            [0, 0, 0],
+            [1, 2, 12], // (3^1)+(3^2)
+            [2, 2, 9], // $alphabet_length^$hash_size_1 or 3^2
 
-        $this->assertSame(
-            config('urlhub.hash_size_1'),
-            strlen($this->url->key_generator())
-        );
-    }
+            [1, -2, 3],
+            [-1, 2, 0],
+            [-1, -2, 0],
 
-    /** @test */
-    public function key_generator_size2_with_zero_value()
-    {
-        config()->set('urlhub.hash_size_1', 2);
-        config()->set('urlhub.hash_size_2', 0);
+            [2.7, 3, 36], // (3^2)+(3^3)
+            [2, 3.7, 36], // (3^2)+(3^3)
 
-        $this->assertSame(
-            config('urlhub.hash_size_1'),
-            strlen($this->url->key_generator())
-        );
-    }
-
-    /** @test */
-    public function url_key_capacity()
-    {
-        config()->set('urlhub.hash_size_1', 0);
-        config()->set('urlhub.hash_size_2', 0);
-        $this->assertSame(0, $this->url->url_key_capacity());
-
-        config()->set('urlhub.hash_size_1', 1);
-        config()->set('urlhub.hash_size_2', 2);
-        $this->assertSame(12, $this->url->url_key_capacity()); // (3^1)+(3^2)
-
-        config()->set('urlhub.hash_size_1', 2);
-        config()->set('urlhub.hash_size_2', 2);
-        // $alphabet_length^$hash_size_1 or 3^2
-        $this->assertSame(9, $this->url->url_key_capacity());
-    }
-
-    /** @test */
-    public function url_key_capacity_input_negative()
-    {
-        config()->set('urlhub.hash_size_1', 1);
-        config()->set('urlhub.hash_size_2', -2);
-        $this->assertSame(3, $this->url->url_key_capacity());
-
-        config()->set('urlhub.hash_size_1', -1);
-        config()->set('urlhub.hash_size_2', 2);
-        $this->assertSame(0, $this->url->url_key_capacity());
-
-        config()->set('urlhub.hash_size_1', -1);
-        config()->set('urlhub.hash_size_2', -2);
-        $this->assertSame(0, $this->url->url_key_capacity());
-    }
-
-    /** @test */
-    public function url_key_capacity_input_number()
-    {
-         config()->set('urlhub.hash_size_1', 2.7);
-        config()->set('urlhub.hash_size_2', 3);
-        $this->assertSame(36, $this->url->url_key_capacity()); // (3^2)+(3^3)
-
-        config()->set('urlhub.hash_size_1', 2);
-        config()->set('urlhub.hash_size_2', 3.7);
-        $this->assertSame(36, $this->url->url_key_capacity()); // (3^2)+(3^3)
-    }
-
-    /** @test */
-    public function url_key_capacity_input_string()
-    {
-        config()->set('urlhub.hash_size_1', 'string');
-        config()->set('urlhub.hash_size_2', 2);
-        $this->assertSame(0, $this->url->url_key_capacity());
-
-        config()->set('urlhub.hash_size_1', 2);
-        config()->set('urlhub.hash_size_2', 'string');
-        // $alphabet_length^$hash_size_1 or 3^2
-        $this->assertSame(9, $this->url->url_key_capacity());
-
-        config()->set('urlhub.hash_size_1', 'string');
-        config()->set('urlhub.hash_size_2', 'string');
-        $this->assertSame(0, $this->url->url_key_capacity());
+            ['string', 2, 0],
+            [2, 'string', 9], // $alphabet_length^$hash_size_1 or 3^2
+            ['string', 'string', 0],
+        ];
     }
 
     /** @test */
