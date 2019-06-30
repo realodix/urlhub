589ccabfb98293fbfa0a28a23a18dc766c0ef27b
Kei <realodix@yahoo.com>
2019-06-30 12:00:41 +0700

    Jumlah config hash_alphabet dipindah ke setup

--- tests/Unit/Models/UrlTest.php
+++ tests/Unit/Models/UrlTest.php
@@ -25,6 +25,8 @@ class UrlTest extends TestCase
             'user_id' => null,
             'clicks'  => 10,
         ]);
+
+        config()->set('urlhub.hash_alphabet', 'abc');
     }
 
     /** @test */
@@ -181,7 +183,6 @@ class UrlTest extends TestCase
     /** @test */
     public function key_generator_size1_equal_with_size2()
     {
-        config()->set('urlhub.hash_alphabet', 'abc');
         config()->set('urlhub.hash_size_1', 2);
         config()->set('urlhub.hash_size_2', 2);
 
@@ -194,7 +195,6 @@ class UrlTest extends TestCase
     /** @test */
     public function key_generator_size2_with_zero_value()
     {
-        config()->set('urlhub.hash_alphabet', 'abc');
         config()->set('urlhub.hash_size_1', 2);
         config()->set('urlhub.hash_size_2', 0);
 
@@ -207,17 +207,14 @@ class UrlTest extends TestCase
     /** @test */
     public function url_key_capacity()
     {
-        config()->set('urlhub.hash_alphabet', 'abc');
         config()->set('urlhub.hash_size_1', 0);
         config()->set('urlhub.hash_size_2', 0);
         $this->assertSame(0, $this->url->url_key_capacity());
 
-        config()->set('urlhub.hash_alphabet', 'abc');
         config()->set('urlhub.hash_size_1', 1);
         config()->set('urlhub.hash_size_2', 2);
         $this->assertSame(12, $this->url->url_key_capacity()); // (3^1)+(3^2)
 
-        config()->set('urlhub.hash_alphabet', 'abc');
         config()->set('urlhub.hash_size_1', 2);
         config()->set('urlhub.hash_size_2', 2);
         // $alphabet_length^$hash_size_1 or 3^2
@@ -227,17 +224,14 @@ class UrlTest extends TestCase
     /** @test */
     public function url_key_capacity_input_negative()
     {
-        config()->set('urlhub.hash_alphabet', 'abc');
         config()->set('urlhub.hash_size_1', 1);
         config()->set('urlhub.hash_size_2', -2);
         $this->assertSame(3, $this->url->url_key_capacity());
 
-        config()->set('urlhub.hash_alphabet', 'abc');
         config()->set('urlhub.hash_size_1', -1);
         config()->set('urlhub.hash_size_2', 2);
         $this->assertSame(0, $this->url->url_key_capacity());
 
-        config()->set('urlhub.hash_alphabet', 'abc');
         config()->set('urlhub.hash_size_1', -1);
         config()->set('urlhub.hash_size_2', -2);
         $this->assertSame(0, $this->url->url_key_capacity());
@@ -246,12 +240,10 @@ class UrlTest extends TestCase
     /** @test */
     public function url_key_capacity_input_number()
     {
-        config()->set('urlhub.hash_alphabet', 'abc');
-        config()->set('urlhub.hash_size_1', 2.7);
+         config()->set('urlhub.hash_size_1', 2.7);
         config()->set('urlhub.hash_size_2', 3);
         $this->assertSame(36, $this->url->url_key_capacity()); // (3^2)+(3^3)
 
-        config()->set('urlhub.hash_alphabet', 'abc');
         config()->set('urlhub.hash_size_1', 2);
         config()->set('urlhub.hash_size_2', 3.7);
         $this->assertSame(36, $this->url->url_key_capacity()); // (3^2)+(3^3)
@@ -260,18 +252,15 @@ class UrlTest extends TestCase
     /** @test */
     public function url_key_capacity_input_string()
     {
-        config()->set('urlhub.hash_alphabet', 'abc');
         config()->set('urlhub.hash_size_1', 'string');
         config()->set('urlhub.hash_size_2', 2);
         $this->assertSame(0, $this->url->url_key_capacity());
 
-        config()->set('urlhub.hash_alphabet', 'abc');
         config()->set('urlhub.hash_size_1', 2);
         config()->set('urlhub.hash_size_2', 'string');
         // $alphabet_length^$hash_size_1 or 3^2
         $this->assertSame(9, $this->url->url_key_capacity());
 
-        config()->set('urlhub.hash_alphabet', 'abc');
         config()->set('urlhub.hash_size_1', 'string');
         config()->set('urlhub.hash_size_2', 'string');
         $this->assertSame(0, $this->url->url_key_capacity());
@@ -282,7 +271,6 @@ class UrlTest extends TestCase
     {
         factory(Url::class, 5)->create();
 
-        config()->set('urlhub.hash_alphabet', 'abc');
         config()->set('urlhub.hash_size_1', 1);
         config()->set('urlhub.hash_size_2', 0);
 
