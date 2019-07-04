ccf486bc6692671424ecbba8f2c96857a2a77da2
Kei <realodix@yahoo.com>
2019-07-04 09:23:27 +0700

    build.190706

--- config/app.php
+++ config/app.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'version'     => 'build.190630',
+    'version'     => 'build.190706',
     'name'        => env('APP_NAME', 'UrlHub'),
     'description' => env('APP_DESCRIPTION', 'Simplify your links'),
 
