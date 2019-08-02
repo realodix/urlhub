c37f0d4c9ff7ef078266d659e2d3c4bfb5f9ca05
Kei <realodix@yahoo.com>
2019-08-02 11:18:36 +0700

    Using environment variable to set redis prefix
    https://github.com/laravel/laravel/pull/5062

--- config/database.php
+++ config/database.php
@@ -143,7 +143,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'predis'),
-            'prefix'  => Str::slug(env('APP_NAME', 'laravel'), '_').'_database_',
+            'prefix'  => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
         ],
 
         'default' => [
