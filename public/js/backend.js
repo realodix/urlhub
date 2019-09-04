e42a6501b28cae15a8e5776b42768b86e6cf9438
Kei <realodix@yahoo.com>
2019-09-04 21:46:24 +0700

    update dependencies

--- public/js/backend.js
+++ public/js/backend.js
@@ -222,7 +222,7 @@ jquery__WEBPACK_IMPORTED_MODULE_0___default()("body").tooltip({
  * for events that are broadcast by Laravel. Echo and event broadcasting
  * allows your team to easily build robust real-time web applications.
  */
-// import Echo from 'laravel-echo'
+// import Echo from 'laravel-echo';
 // window.Pusher = require('pusher-js');
 // window.Echo = new Echo({
 //     broadcaster: 'pusher',
