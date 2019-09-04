e8446418ccacd6be0f7df9114c0d71f0d2f379e6
Kei <realodix@yahoo.com>
2019-09-04 14:17:43 +0700

    Revert "Merge branch 'master' into Upgrading-To-6.0-From-5.8"
    This reverts commit 103a921c87606c578ac4dc04a2527f5939e129e0, reversing
    changes made to 67d4decff081744d911ba5f69415c952c0c14894.

--- config/database.php
+++ config/database.php
@@ -90,6 +90,27 @@ return [
             'prefix'         => '',
             'prefix_indexes' => true,
         ],
+
+        'testing' => [
+            'driver'         => 'mysql',
+            'url'            => env('DATABASE_URL'),
+            'host'           => env('DB_TEST_HOST', '127.0.0.1'),
+            'port'           => env('DB_TEST_PORT', '3306'),
+            'database'       => env('DB_TEST_DATABASE', 'urlhub_test'),
+            'username'       => env('DB_TEST_USERNAME', 'root'),
+            'password'       => env('DB_TEST_PASSWORD', ''),
+            'unix_socket'    => env('DB_TEST_SOCKET', ''),
+            'charset'        => 'utf8mb4',
+            'collation'      => 'utf8mb4_unicode_ci',
+            'prefix'         => '',
+            'prefix_indexes' => true,
+            'strict'         => true,
+            'engine'         => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
     ],
 
     /*
