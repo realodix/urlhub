7936f4dfb033f1894886d2dd3e3c9ced450b02aa
Kei <realodix@yahoo.com>
2019-09-07 22:15:24 +0700

    Delete cached config file before running tests
    https://github.com/laravel/laravel/pull/5091

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -1,29 +1,42 @@
 <?php
 
+namespace Tests;
+
 use Illuminate\Contracts\Console\Kernel;
+use PHPUnit\Runner\AfterLastTestHook;
+use PHPUnit\Runner\BeforeFirstTestHook;
 
-require_once __DIR__.'/../vendor/autoload.php';
+class Bootstrap implements BeforeFirstTestHook, AfterLastTestHook
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Bootstrap The Test Environment
+    |--------------------------------------------------------------------------
+    |
+    | You may specify console commands that execute once before your test is
+    | run. You are free to add your own additional commands or logic into
+    | this file as needed in order to help your test suite run quicker.
+    |
+    */
 
-/*
-|--------------------------------------------------------------------------
-| Bootstrap The Test Environment
-|--------------------------------------------------------------------------
-|
-| You may specify console commands that execute once before your test is
-| run. You are free to add your own additional commands or logic into
-| this file as needed in order to help your test suite run quicker.
-|
-*/
+    use CreatesApplication;
 
-$commands = [
-    'config:cache',
-    'event:cache',
-];
+    public function executeBeforeFirstTest(): void
+    {
+        $console = $this->createApplication()->make(Kernel::class);
 
-$app = require __DIR__.'/../bootstrap/app.php';
+        $commands = [
+            'config:cache',
+            'event:cache',
+        ];
 
-$console = tap($app->make(Kernel::class))->bootstrap();
+        foreach ($commands as $command) {
+            $console->call($command);
+        }
+    }
 
-foreach ($commands as $command) {
-    $console->call($command);
+    public function executeAfterLastTest(): void
+    {
+        array_map('unlink', glob('bootstrap/cache/*.phpunit.php'));
+    }
 }
