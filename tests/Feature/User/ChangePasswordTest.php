6561bf81a02899d8d31639d8bcec03b7e3e46d64
Kei <realodix@yahoo.com>
2019-09-19 03:25:28 +0700

    fix testing fail on sequential rules

--- tests/Feature/User/ChangePasswordTest.php
+++ tests/Feature/User/ChangePasswordTest.php
@@ -162,7 +162,7 @@ class ChangePasswordTest extends TestCase
             // Laravel NIST Password Rules
             ['new-awe', 'new-awe'], // min:8
             [str_repeat('a', 9), str_repeat('a', 9)], // repetitive
-            ['davidcba1234', 'davidcba1234'], // sequential
+            ['12345678', '12345678'], // sequential
         ];
     }
 }
