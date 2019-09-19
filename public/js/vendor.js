47cad6610d9b700f9b77d3d32f685f7c7dcbf5fc
Kei <realodix@yahoo.com>
2019-09-20 04:14:23 +0700

    yarn upgrade [skip ci]

--- public/js/vendor.js
+++ public/js/vendor.js
@@ -3778,7 +3778,7 @@ var p = config.measurePerformance && PERFORMANCE && PERFORMANCE.mark && PERFORMA
   mark: noop$1,
   measure: noop$1
 };
-var preamble = "FA \"5.11.0\"";
+var preamble = "FA \"5.11.1\"";
 
 var begin = function begin(name) {
   p.mark("".concat(preamble, " ").concat(name, " begins"));
