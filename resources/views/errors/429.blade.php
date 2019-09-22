0df894b62aa5c7eb37a55c66985f941c9b804c7c
Kei <budi03@gmail.com>
2019-09-22 16:03:30 +0700

    Custom http error pages (#470)

---
+++ resources/views/errors/429.blade.php
@@ -0,0 +1,9 @@
+@extends('errors::illustrated-layout')
+
+@section('title', __('Too Many Requests'))
+@section('code', '429')
+@section('message', __('Too Many Requests'))
+@section('image')
+  <div style="background-image: url({{ asset('/svg/errors/403.svg') }});" class="absolute pin bg-cover bg-no-repeat md:bg-left lg:bg-center">
+  </div>
+@endsection
---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,9 @@
+@extends('errors::illustrated-layout')
+
+@section('title', __('Server Error'))
+@section('code', '500')
+@section('message', __('Server Error'))
+@section('image')
+  <div style="background-image: url({{ asset('/svg/errors/500.svg') }});" class="absolute pin bg-cover bg-no-repeat md:bg-left lg:bg-center">
+  </div>
+@endsection
