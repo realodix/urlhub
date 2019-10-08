a021000a12ac6f5d812e9abfd5cd46feff0bd407
Kei <realodix@yahoo.com>
2019-10-09 03:04:16 +0700

    fix redirectTo

--- app/Http/Controllers/Auth/ConfirmPasswordController.php
+++ app/Http/Controllers/Auth/ConfirmPasswordController.php
@@ -25,7 +25,7 @@ class ConfirmPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/admin';
 
     /**
      * Create a new controller instance.
