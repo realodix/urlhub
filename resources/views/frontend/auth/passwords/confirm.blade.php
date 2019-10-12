655bacc53e838cd2f9e8eba632209ff64e7d5ac4
Kei <realodix@yahoo.com>
2019-10-12 21:29:23 +0700

    ubah warna [ci skip]

--- resources/views/frontend/auth/passwords/confirm.blade.php
+++ resources/views/frontend/auth/passwords/confirm.blade.php
@@ -31,7 +31,7 @@
 
             <div class="form-group row mb-0">
               <div class="col-md-8 offset-md-4">
-                <button type="submit" class="btn btn-primary">
+                <button type="submit" class="btn btn-secondary">
                   {{ __('Confirm Password') }}
                 </button>
 
--- resources/views/frontend/auth/passwords/reset.blade.php
+++ resources/views/frontend/auth/passwords/reset.blade.php
@@ -55,7 +55,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-secondary">
                                     {{ __('Reset Password') }}
                                 </button>
                             </div>
