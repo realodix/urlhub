62fb420607fcc3e04edd536e523594a054ddf4c0
Eries Trisnadi <zdumb1885@outlook.com>
2020-01-29 14:14:08 +0700

    dry up app url to $_SERVER['SERVER_NAME']

--- resources/views/backend/edit.blade.php
+++ resources/views/backend/edit.blade.php
@@ -20,36 +20,38 @@
           <hr />
 
           <div class="row mt-4 mb-4">
-          <div class="col">
-            <div class="form-group{{ $errors->has('short-url') ? ' has-error' : '' }} row">
-              <label for="short-url" class="col-sm-3 col-form-label">@lang('Short URL')</label>
+            <div class="col">
 
-              <div class="col">
-                <div class="input-group mb-3">
-                    <div class="input-group-prepend">
-                        <span class="input-group-text" id="short-addon3">urlhub.test/</span>
-                    </div>
-                    <input type="text" name="custom_url_key" class="form-control" id="short-url" aria-describedby="short-addon3" required value="{{$url->url_key}}">
+              <div class="form-group{{ $errors->has('short-url') ? ' has-error' : '' }} row">
+                <label for="short-url" class="col-sm-3 col-form-label">@lang('Short URL')</label>
+
+                <div class="col">
+                  <div class="input-group mb-3">
+                      <div class="input-group-prepend">
+                          <span class="input-group-text" id="short-addon3">{{$_SERVER['SERVER_NAME']}}/</span>
+                      </div>
+                      <input type="text" name="custom_url_key" class="form-control" id="short-url" aria-describedby="short-addon3" required value="{{$url->url_key}}">
+                  </div>
                 </div>
               </div>
-            </div>
 
-            <div class="form-group{{ $errors->has('long-url') ? ' has-error' : '' }} row">
-              <label for="long-url" class="col-sm-3 col-form-label">@lang('Long URL')</label>
+              <div class="form-group{{ $errors->has('long-url') ? ' has-error' : '' }} row">
+                <label for="long-url" class="col-sm-3 col-form-label">@lang('Long URL')</label>
 
-              <div class="col">
-                <input id="long-url" type="text" class="form-control" name="long_url" placeholder="Enter your long url" required value="{{$url->long_url}}">
+                <div class="col">
+                  <input id="long-url" type="text" class="form-control" name="long_url" placeholder="Enter your long url" required value="{{$url->long_url}}">
+                </div>
               </div>
-            </div>
 
-            <div class="row">
-              <div class="col text-right">
-                <button type="submit" class="btn btn-secondary">
-                  @lang('Save Changes')
-                </button>
+              <div class="row">
+                <div class="col text-right">
+                  <button type="submit" class="btn btn-secondary">
+                    @lang('Save Changes')
+                  </button>
+                </div>
               </div>
-            </div>
-          </div><!--col-->
+
+            </div><!--col-->
           </div><!--row-->
         </div><!--card-body-->
       </div><!--card-->
