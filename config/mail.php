deba9d41a96c680bbd90e3741fd93a958b64ca70
Kei <realodix@yahoo.com>
2020-03-16 22:32:05 +0700

    Update mail.php

--- config/mail.php
+++ config/mail.php
@@ -84,7 +84,7 @@ return [
 
     'from' => [
         'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'name'    => env('MAIL_FROM_NAME', 'Example'),
     ],
 
     /*
