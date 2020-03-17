1c8ada92788b4128dffa6517662cd764ac1b443d
Kei <realodix@yahoo.com>
2020-03-17 11:27:37 +0700

    Remove method_separation [ci skip]

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -15,6 +15,7 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     protected $namespace = 'App\Http\Controllers';
+
     protected $apiNamespace = 'App\Http\Controllers\API';
 
     /**
