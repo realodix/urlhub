d4ef259a28ccbf9bef399bd41eb749e4e2eda946
Kei <realodix@yahoo.com>
2020-03-17 09:33:10 +0700

    build.200317 [ci skip]

--- config/app.php
+++ config/app.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'version'     => 'build.200226',
+    'version'     => 'build.200317',
     'name'        => env('APP_NAME', 'UrlHub'),
     'description' => env('APP_DESCRIPTION', 'Simplify your links'),
 
