73f50c09ceaefa3e65b16ea8accdfbc08589c389
Kei <realodix@yahoo.com>
2020-03-22 12:22:08 +0700

    docs: formatting

--- config/urlhub.php
+++ config/urlhub.php
@@ -1,17 +1,37 @@
 <?php
 
 return [
-    // Number of symbols in generating unique url_key. If hash_size_1 == hash_size_2,
-    // hash_size_2 is automatically declared to be of no value.
+
+    /*
+    |--------------------------------------------------------------------------
+    | Number of symbols in generating unique url_key
+    |--------------------------------------------------------------------------
+    |
+    | If hash_size_1 is equal to hash_size_2, hash_size_2 is automatically
+    | declared to be of no value.
+    |
+    */
+
     'hash_size_1' => env('HASH_SIZE_1', 6), // >= 1
     'hash_size_2' => env('HASH_SIZE_2', 7), // >= 0
 
-    // Symbols to be used in generating unique url_key.
+    /*
+    |--------------------------------------------------------------------------
+    | Symbols to be used in generating unique url_key
+    |--------------------------------------------------------------------------
+    */
+
     'hash_alphabet' => env(
         'HASH_ALPHABET',
         '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
     ),
-    // A list of non allowed domain.
+
+    /*
+    |--------------------------------------------------------------------------
+    | A list of non allowed domain
+    |--------------------------------------------------------------------------
+    */
+
     'blacklist' => [
         config('app.url'),
         // 'bit.ly',
