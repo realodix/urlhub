1b7ec2c8788d60b7ea446402456fbb0eb0571391
Realodix <budi03@gmail.com>
2020-04-04 04:32:34 +0700

    build.200403

--- config/app.php
+++ config/app.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'version'     => 'build.200329',
+    'version'     => 'build.200403',
     'name'        => env('APP_NAME', 'UrlHub'),
     'description' => env('APP_DESCRIPTION', 'Simplify your links'),
 
