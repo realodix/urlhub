b1ab86b3f2ec1e13578dbdb2fe9d492c1ce1ae26
Realodix <budi03@gmail.com>
2020-05-11 20:48:37 +0700

    Delete unused "IF"

--- app/Url.php
+++ app/Url.php
@@ -227,8 +227,8 @@ class Url extends Model
         $pieces = parse_url($url);
         $domain = isset($pieces['host']) ? $pieces['host'] : '';
 
-        if (preg_match('/(?P<domain>[a-z0-9][a-z0-9\-]{1,63}\.[a-z\.]{2,6})$/i', $domain, $regs)) {
-            return $regs['domain'];
-        }
+        preg_match('/(?P<domain>[a-z0-9][a-z0-9\-]{1,63}\.[a-z\.]{2,6})$/i', $domain, $regs);
+
+        return $regs['domain'];
     }
 }
