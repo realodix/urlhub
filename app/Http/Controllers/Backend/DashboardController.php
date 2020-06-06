6df3d031c438ee6fc697bf1a8d5a6529c1f08251
Kei <budi03@gmail.com>
2020-06-06 23:56:14 +0700

    Update DashboardController.php

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -96,7 +96,7 @@ class DashboardController extends Controller
      */
     public function edit($url_key)
     {
-        $url = Url::with('urlStat')->whereUrlKey($url_key)->firstOrFail();
+        $url = Url::whereUrlKey($url_key)->firstOrFail();
 
         $this->authorize('updateUrl', $url);
 
