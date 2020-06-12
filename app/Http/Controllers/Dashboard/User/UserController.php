c539605c33fd952ec5cd899a65578038a0c39f88
Kei <budi03@gmail.com>
2020-06-13 00:50:40 +0700

    Remove unused variables

--- app/Http/Controllers/Dashboard/User/UserController.php
+++ app/Http/Controllers/Dashboard/User/UserController.php
@@ -90,7 +90,7 @@ class UserController extends Controller
 
         $user->email = $request->input('email');
 
-        $validatedData = $request->validate([
+        $request->validate([
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
         ]);
 
