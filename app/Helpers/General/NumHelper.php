8e1e781c81fd3fdca509fa6c74295bc2fa7854d4
Kei <budi03@gmail.com>
2020-06-14 17:31:23 +0700

    Rename numb_prec() to numbPrec()

--- app/Helpers/General/NumHelper.php
+++ app/Helpers/General/NumHelper.php
@@ -20,7 +20,7 @@ class NumHelper
             $suffix = '';
         } elseif ($n >= pow(10, 3) && $n < pow(10, 6)) {
             // 1k-999k
-            $nFormat = $this->numb_prec($n / pow(10, 3));
+            $nFormat = $this->numbPrec($n / pow(10, 3));
             $suffix = 'K+';
 
             if (($n / pow(10, 3) == 1) || ($n / pow(10, 4) == 1) || ($n / pow(10, 5) == 1)) {
@@ -28,7 +28,7 @@ class NumHelper
             }
         } elseif ($n >= pow(10, 6) && $n < pow(10, 9)) {
             // 1m-999m
-            $nFormat = $this->numb_prec($n / pow(10, 6));
+            $nFormat = $this->numbPrec($n / pow(10, 6));
             $suffix = 'M+';
 
             if (($n / pow(10, 6) == 1) || ($n / pow(10, 7) == 1) || ($n / pow(10, 8) == 1)) {
@@ -36,7 +36,7 @@ class NumHelper
             }
         } elseif ($n >= pow(10, 9) && $n < pow(10, 12)) {
             // 1b-999b
-            $nFormat = $this->numb_prec($n / pow(10, 9));
+            $nFormat = $this->numbPrec($n / pow(10, 9));
             $suffix = 'B+';
 
             if (($n / pow(10, 9) == 1) || ($n / pow(10, 10) == 1) || ($n / pow(10, 11) == 1)) {
@@ -44,7 +44,7 @@ class NumHelper
             }
         } elseif ($n >= pow(10, 12)) {
             // 1t+
-            $nFormat = $this->numb_prec($n / pow(10, 12));
+            $nFormat = $this->numbPrec($n / pow(10, 12));
             $suffix = 'T+';
 
             if (($n / pow(10, 12) == 1) || ($n / pow(10, 13) == 1) || ($n / pow(10, 14) == 1)) {
@@ -64,7 +64,7 @@ class NumHelper
      * @param int    $precision
      * @return number
      */
-    public function numb_prec($number, $precision = 2)
+    public function numbPrec($number, $precision = 2)
     {
         return floor($number * pow(10, $precision)) / pow(10, $precision);
     }
