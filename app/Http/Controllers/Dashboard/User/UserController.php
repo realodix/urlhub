47362cb83add29d42f175f13fca811688ef42df7
Kei <budi03@gmail.com>
2020-06-22 05:43:44 +0700

    Datatables user index pada services

--- app/Http/Controllers/Dashboard/User/UserController.php
+++ app/Http/Controllers/Dashboard/User/UserController.php
@@ -3,9 +3,9 @@
 namespace App\Http\Controllers\Dashboard\User;
 
 use App\Http\Controllers\Controller;
+use App\Services\Dashboard\UserService;
 use App\User;
 use Illuminate\Http\Request;
-use Yajra\Datatables\Datatables;
 
 class UserController extends Controller
 {
@@ -28,37 +28,9 @@ class UserController extends Controller
     /**
      * @codeCoverageIgnore
      */
-    public function getData()
+    public function dataTable(UserService $userService)
     {
-        $userModel = User::query();
-
-        return Datatables::of($userModel)
-            ->editColumn('name', function ($user) {
-                return '<a href="'.route('user.edit', $user->name).'">'.$user->name.'</a>';
-            })
-            ->editColumn('created_at', function ($user) {
-                return [
-                    'display'   => '<span title="'.$user->created_at->toDayDateTimeString().'" data-toggle="tooltip" style="cursor: default;">'.$user->created_at->diffForHumans().'</span>',
-                    'timestamp' => $user->created_at->timestamp,
-                ];
-            })
-            ->editColumn('updated_at', function ($user) {
-                return [
-                    'display'   => '<span title="'.$user->updated_at->toDayDateTimeString().'" data-toggle="tooltip" style="cursor: default;">'.$user->updated_at->diffForHumans().'</span>',
-                    'timestamp' => $user->updated_at->timestamp,
-                ];
-            })
-            ->addColumn('action', function ($user) {
-                return
-                    '<div class="btn-group" role="group" aria-label="Basic example">
-                        <div class="btn-group" role="group" aria-label="Basic example">
-                        <a role="button" class="btn" href="'.route('user.edit', $user->name).'" title="'.__('Details').'" data-toggle="tooltip"><i class="fas fa-user-edit"></i></a>
-                        <a role="button" class="btn" href="'.route('user.change-password', $user->name).'" title="'.__('Change Password').'" data-toggle="tooltip"><i class="fas fa-key"></i></a>
-                        </div>
-                    </div>';
-            })
-            ->rawColumns(['name', 'created_at.display', 'updated_at.display', 'action'])
-            ->toJson();
+        return $userService->dataTable();
     }
 
     /**
---
+++ app/Services/Dashboard/UserService.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Services\Dashboard;
+
+use App\User;
+
+class UserService
+{
+    /**
+     * @codeCoverageIgnore
+     */
+    public function dataTable()
+    {
+        $user = User::query();
+
+        return datatables($user)
+            ->editColumn('name', function ($user) {
+                return '<a href="'.route('user.edit', $user->name).'">'.$user->name.'</a>';
+            })
+            ->editColumn('created_at', function ($user) {
+                return [
+                    'display'   => '<span title="'.$user->created_at->toDayDateTimeString().'" data-toggle="tooltip" style="cursor: default;">'.$user->created_at->diffForHumans().'</span>',
+                    'timestamp' => $user->created_at->timestamp,
+                ];
+            })
+            ->editColumn('updated_at', function ($user) {
+                return [
+                    'display'   => '<span title="'.$user->updated_at->toDayDateTimeString().'" data-toggle="tooltip" style="cursor: default;">'.$user->updated_at->diffForHumans().'</span>',
+                    'timestamp' => $user->updated_at->timestamp,
+                ];
+            })
+            ->addColumn('action', function ($user) {
+                return
+                    '<div class="btn-group" role="group" aria-label="Basic example">
+                        <div class="btn-group" role="group" aria-label="Basic example">
+                        <a role="button" class="btn" href="'.route('user.edit', $user->name).'" title="'.__('Details').'" data-toggle="tooltip"><i class="fas fa-user-edit"></i></a>
+                        <a role="button" class="btn" href="'.route('user.change-password', $user->name).'" title="'.__('Change Password').'" data-toggle="tooltip"><i class="fas fa-key"></i></a>
+                        </div>
+                    </div>';
+            })
+            ->rawColumns(['name', 'created_at.display', 'updated_at.display', 'action'])
+            ->toJson();
+    }
+}
