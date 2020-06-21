4d5a3e23aa03e34ed8b5e510205bccf4ed12dd50
Kei <budi03@gmail.com>
2020-06-22 06:09:10 +0700

    fix yajra datatables syntax

--- app/DataTables/DashboardDataTables.php
+++ app/DataTables/DashboardDataTables.php
@@ -13,28 +13,28 @@ class DashboardDataTables
      */
     public function dataTable()
     {
-        $url = Url::whereUserId(Auth::id());
+        $urlModel = Url::whereUserId(Auth::id());
 
-        return datatables($url)
-            ->editColumn('keyword', function ($url) {
+        return datatables($urlModel)
+            ->editColumn('keyword', function (Url $url) {
                 return '<span class="short_url" data-clipboard-text="'.$url->short_url.'" title="'.__('Copy to clipboard').'" data-toggle="tooltip">'.urlRemoveScheme($url->short_url).'</span>';
             })
-            ->editColumn('long_url', function ($url) {
+            ->editColumn('long_url', function (Url $url) {
                 return '
                     <span title="'.$url->meta_title.'" data-toggle="tooltip">'.Str::limit($url->meta_title, 90).'</span>
                     <br>
                     <a href="'.$url->long_url.'" target="_blank" title="'.$url->long_url.'" data-toggle="tooltip" class="text-muted">'.urlLimit($url->long_url, 70).'</a>';
             })
-            ->editColumn('clicks', function ($url) {
+            ->editColumn('clicks', function (Url $url) {
                 return '<span title="'.number_format($url->clicks).' clicks" data-toggle="tooltip">'.numberFormatShort($url->clicks).'</span>';
             })
-            ->editColumn('created_at', function ($url) {
+            ->editColumn('created_at', function (Url $url) {
                 return [
                     'display'   => '<span title="'.$url->created_at->toDayDateTimeString().'" data-toggle="tooltip">'.$url->created_at->diffForHumans().'</span>',
                     'timestamp' => $url->created_at->timestamp,
                 ];
             })
-            ->addColumn('action', function ($url) {
+            ->addColumn('action', function (Url $url) {
                 return
                     '<div class="btn-group btn-group-sm" role="group" aria-label="Basic example">
                         <a role="button" class="btn" href="'.route('short_url.stats', $url->keyword).'" target="_blank" title="'.__('Details').'" data-toggle="tooltip"><i class="fa fa-eye"></i></a>
