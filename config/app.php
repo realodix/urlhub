6fc75f47d6e5578302491c52ab7514984c9d88e4
Kei <budi03@gmail.com>
2020-07-09 18:13:17 +0700

    v1.0.0-alpha.4.1 [ci skip]

--- config/app.php
+++ config/app.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'version'     => 'v1.0.0-alpha.5 (master)',
+    'version'     => 'v1.0.0-alpha.4.1',
     'name'        => env('APP_NAME', 'UrlHub'),
     'description' => env('APP_DESCRIPTION', 'Simplify your links'),
 
