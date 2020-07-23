1db0866d1f2f6b7ae1c95500eff96493764f3a1f
Kei <budi03@gmail.com>
2020-07-23 22:04:14 +0700

    Set default value is true

--- app/Services/ConfigService.php
+++ app/Services/ConfigService.php
@@ -7,6 +7,7 @@ namespace App\Services;
  */
 class ConfigService
 {
+    private const DEFAULT_TRUE = true;
     /**
      * Validate all of the config related to the library.
      *
@@ -20,36 +21,30 @@ class ConfigService
             && $this->guest_show_stat()
             && $this->hash_char()
             && $this->hash_length()
+            && $this->anonymize_ip_addr()
             && $this->redirect_status_code()
-            && $this->redirect_cache_lifetime()
-            && $this->anonymize_ip_addr();
+            && $this->redirect_cache_lifetime();
     }
 
     private function guest()
     {
         if (! is_bool(config('urlhub.guest'))) {
-            throw new \Exception('The "guest" config variable must be a boolean.');
+            return config(['urlhub.guest' => self::DEFAULT_TRUE]);
         }
-
-        return true;
     }
 
     private function guest_register()
     {
         if (! is_bool(config('urlhub.guest_register'))) {
-            throw new \Exception('The "guest_register" config variable must be a boolean.');
+            return config(['urlhub.guest_register' => self::DEFAULT_TRUE]);
         }
-
-        return true;
     }
 
     private function guest_show_stat()
     {
         if (! is_bool(config('urlhub.guest_show_stat'))) {
-            throw new \Exception('The "guest_show_stat" config variable must be a boolean.');
+            return config(['urlhub.guest_show_stat' => self::DEFAULT_TRUE]);
         }
-
-        return true;
     }
 
     private function hash_char()
@@ -101,7 +96,7 @@ class ConfigService
     private function anonymize_ip_addr()
     {
         if (! is_bool(config('urlhub.anonymize_ip_addr'))) {
-            throw new \Exception('The "urlhub.anonymize_ip_addr" config variable must be a boolean.');
+            return config(['urlhub.anonymize_ip_addr' => self::DEFAULT_TRUE]);
         }
 
         return true;
