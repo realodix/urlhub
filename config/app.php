bbe404e152bd89365d9c034f9672004125e218d1
Kei <budi03@gmail.com>
2020-08-02 22:38:46 +0700

    v1.0.0-alpha.6 (master)

--- config/app.php
+++ config/app.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'version'     => 'v1.0.0-alpha.5',
+    'version'     => 'v1.0.0-alpha.6 (master)',
     'name'        => env('APP_NAME', 'UrlHub'),
     'description' => env('APP_DESCRIPTION', 'Simplify your links'),
 
