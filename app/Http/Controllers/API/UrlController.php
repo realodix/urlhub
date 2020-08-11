90bb383968cf70cec40db343dad8ce0271f7912a
Kei <realodix@users.noreply.github.com>
2020-08-11 12:33:25 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/API/UrlController.php
+++ app/Http/Controllers/API/UrlController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreUrl;
 use App\Services\UrlService;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
