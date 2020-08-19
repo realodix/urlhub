abd3b952af25c4d1915c005a61b24ae318eb345c
Kei <budi03@gmail.com>
2020-08-20 06:56:30 +0700

    Update GeneralHelper.php

--- app/Helpers/General/GeneralHelper.php
+++ app/Helpers/General/GeneralHelper.php
@@ -46,7 +46,7 @@ class GeneralHelper
             $hostLen = strlen($sUrl->getHost());
         }
 
-        if ($url - $hostLen === 1) {
+        if ($urlLen - $hostLen === 1) {
             $url = rtrim($url, '/').'';
         }
 
@@ -61,7 +61,7 @@ class GeneralHelper
         $firstSide = intval($length * 0.6); // use intval to prevent float
         $lastSide = -abs($length - $firstSide - 3); // 3 dots from Str::limit()
 
-        if ($urlLen > $length && $length > 0) {
+        if (($urlLen > $length) && ($length > 0)) {
             if ($trimPathLen == 10) {
                 $firstSide = $hostLen + 4;
                 $lastSide = -abs($length - $firstSide - 3); // 3 dots from Str::limit()
