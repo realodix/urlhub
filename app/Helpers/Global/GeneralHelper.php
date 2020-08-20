a923cfcba18d6d6bfd4cd22df15461670e456429
realodix <budi03@gmail.com>
2020-08-20 19:00:02 +0700

    Refactor GeneralHelper

--- app/Helpers/Global/GeneralHelper.php
+++ app/Helpers/Global/GeneralHelper.php
@@ -1,18 +1,26 @@
 <?php
 
-use App\Helpers\General\GeneralHelper;
 use CodeItNow\BarcodeBundle\Utils\QrCode;
+use Illuminate\Support\Str;
+use Spatie\Url\Url as SpatieUrl;
 
 if (! function_exists('uHub')) {
     /**
-     * uHub('option') is equal to config('urlhub.option').
+     * Helper that makes the way to access the configuration value in
+     * '/config/urlhub.php' becomes easier.
+     * @codeCoverageIgnore
+     *
+     * Example:
+     * - uHub('option') is equal to config('urlhub.option').
      *
      * @param string $value
      * @return mixed
      */
     function uHub($value)
     {
-        return resolve(GeneralHelper::class)->uHub($value);
+        // Validation of character types allowed in the `urlhub.hash_char`
+        // configuration option
+        return config('urlhub.'.$value);
     }
 }
 
@@ -29,16 +37,58 @@ if (! function_exists('appName')) {
 }
 
 if (! function_exists('urlDisplay')) {
-    function urlDisplay($url, $scheme = true, $length = 0)
+    /**
+     * Display links or URLs as needed.
+     *
+     * @param string $url    URL or Link
+     * @param bool   $scheme Show scheme or not
+     * @param int    $length Truncates the given string at the specified length.
+     *                       Set to 0 to display all of it.
+     * @return string
+     */
+    function urlDisplay(string $url, bool $scheme = true, int $length = 0)
     {
-        return resolve(GeneralHelper::class)->urlDisplay($url, $scheme, $length);
+        $urlFS = SpatieUrl::fromString($url);
+        $hostLen = strlen($urlFS->getScheme().'://'.$urlFS->getHost());
+
+        if ($scheme == false) {
+            $url = urlRemoveScheme($url);
+            $hostLen = strlen($urlFS->getHost());
+        }
+
+        if ($length <= 0) {
+            return $url;
+        }
+
+        if ($hostLen >= 30 || (($hostLen <= 27) && ($length <= 30))) {
+            $length -= 3;
+
+            return Str::limit($url, $length);
+        }
+
+        $firstSide = intval($length * 0.6); // use intval to prevent float
+        $lastSide = (($length - $firstSide) * -1) + 3; // + 3 dots from Str::limit()
+
+        if (strlen($url) > $length) {
+            return Str::limit($url, $firstSide).substr($url, $lastSide);
+        }
+
+        return $url;
     }
 }
 
 if (! function_exists('urlRemoveScheme')) {
+    /**
+     * @param string $value
+     * @return string
+     */
     function urlRemoveScheme($value)
     {
-        return resolve(GeneralHelper::class)->urlRemoveScheme($value);
+        return str_replace([
+            'http://',
+            'https://',
+            'www.',
+        ], '', $value);
     }
 }
 
