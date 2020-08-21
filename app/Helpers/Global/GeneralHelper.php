24e677a664bc49010c4ad07030c81989c530dd4e
realodix <budi03@gmail.com>
2020-08-22 03:43:02 +0700

    Update GeneralHelper.php

--- app/Helpers/Global/GeneralHelper.php
+++ app/Helpers/Global/GeneralHelper.php
@@ -116,14 +116,14 @@ if (! function_exists('qrCode')) {
     {
         $qrCode = new QrCode();
         $qrCode->setText($string)
-                ->setSize(150)
-                ->setPadding(10)
-                ->setErrorCorrection('high')
-                ->setForegroundColor(['r' => 0, 'g' => 0, 'b' => 0, 'a' => 0])
-                ->setBackgroundColor(['r' => 255, 'g' => 255, 'b' => 255, 'a' => 0])
-                ->setLabel('Scan QR Code')
-                ->setLabelFontSize(12)
-                ->setImageType(QrCode::IMAGE_TYPE_PNG);
+               ->setSize(150)
+               ->setPadding(10)
+               ->setErrorCorrection('high')
+               ->setForegroundColor(['r' => 0, 'g' => 0, 'b' => 0, 'a' => 0])
+               ->setBackgroundColor(['r' => 255, 'g' => 255, 'b' => 255, 'a' => 0])
+               ->setLabel('Scan QR Code')
+               ->setLabelFontSize(12)
+               ->setImageType(QrCode::IMAGE_TYPE_PNG);
 
         return $qrCode;
     }
