ad138a10a2ba85578e275017ee66bc012f90728f
realodix <budi03@gmail.com>
2020-08-21 22:53:52 +0700

    Update GeneralHelper.php

--- app/Helpers/Global/GeneralHelper.php
+++ app/Helpers/Global/GeneralHelper.php
@@ -42,7 +42,7 @@ if (! function_exists('urlDisplay')) {
      * Display the link according to what You need.
      *
      * @param string $url    URL or Link.
-     * @param bool   $scheme Show or remove URL schema.
+     * @param bool   $scheme Show or remove URL schemes.
      * @param int    $limit  Length string will be truncated to, including
      *                       suffix.
      * @return string
