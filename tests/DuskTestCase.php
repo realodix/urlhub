f24b091e0ddb528d7a0fe07305fee2ec571908bd
Kei <realodix@users.noreply.github.com>
2020-09-14 03:39:39 +0000

    Apply fixes from StyleCI

--- tests/DuskTestCase.php
+++ tests/DuskTestCase.php
@@ -34,7 +34,7 @@ abstract class DuskTestCase extends BaseTestCase
             '--disable-gpu',
             '--headless',
             '--window-size=1920,1080',
-            '--no-sandbox'
+            '--no-sandbox',
         ]);
 
         return RemoteWebDriver::create(
