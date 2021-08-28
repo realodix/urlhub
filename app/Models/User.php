d3e137a3853c4409422ce029d154d65eb6d1bd1a
Kei <budi03@gmail.com>
2021-08-28 21:55:19 +0700

    Use PHPDoc comments from base class in User model
    https://github.com/laravel/laravel/commit/7c993468018d950d549fae77241a53cb4f68e2ad

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,14 +22,14 @@ class User extends Authenticatable
     /**
      * The attributes that are mass assignable.
      *
-     * @var array
+     * @var string[]
      */
     protected $fillable = [
         'name', 'email', 'password',
     ];
 
     /**
-     * The attributes that should be hidden for arrays.
+     * The attributes that should be hidden for serialization.
      *
      * @var array
      */
@@ -38,7 +38,7 @@ class User extends Authenticatable
     ];
 
     /**
-     * The attributes that should be cast to native types.
+     * The attributes that should be cast.
      *
      * @var array
      */
