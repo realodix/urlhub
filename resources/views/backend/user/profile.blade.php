f1af492f566f723fb6053e4805a03078aa1d5394
Kei <budi03@gmail.com>
2022-01-31 12:28:13 +0700

    masukkan style button ke dalam css

--- resources/views/backend/user/profile.blade.php
+++ resources/views/backend/user/profile.blade.php
@@ -33,7 +33,7 @@
         </div>
       </div>
       <div class="flex items-center justify-end px-4 py-3 bg-gray-50 text-right sm:px-6 shadow sm:rounded-bl-md sm:rounded-br-md">
-        <button type="submit" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition">
+        <button type="submit" class="button">
           @lang('Save')
         </button>
       </div>
