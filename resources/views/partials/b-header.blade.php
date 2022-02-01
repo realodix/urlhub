e54d09f42166e33e63e0bcda762f5d13a9152a4d
Kei <budi03@gmail.com>
2022-02-01 15:24:56 +0700

    Ubah sedikit tampilan navbar header

--- resources/views/partials/b-header.blade.php
+++ resources/views/partials/b-header.blade.php
@@ -1,17 +1,17 @@
 @include('partials/nav')
 
 <header class="bg-white shadow">
-  <div class="hidden sm:flex max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 croll-smooth hover:scroll-auto">
-    <a href="{{route('dashboard')}}" class="text-xl font-light text-nord0 hover:text-nord10 leading-tight mr-4">
+  <div class="hidden sm:flex max-w-7xl mx-auto p-4 sm:px-6 lg:px-8 croll-smooth hover:scroll-auto">
+    <a href="{{route('dashboard')}}" class="text-lg font-light text-nord0 hover:text-sky-700 leading-tight mr-4">
       <i class="fas fa-tachometer-alt"></i>
       @lang('Dashboard')
     </a>
     @role('admin')
-      <a href="{{route('dashboard.allurl')}}" class="text-xl font-light text-nord0 hover:text-nord10 leading-tight mr-4">
+      <a href="{{route('dashboard.allurl')}}" class="text-lg font-light text-nord0 hover:text-sky-700 leading-tight mr-4">
         <i class="nav-icon fas fa-link"></i>
         @lang('All URLs')
       </a>
-      <a href="{{route('user.index')}}" class="text-xl font-light text-nord0 hover:text-nord10 leading-tight">
+      <a href="{{route('user.index')}}" class="text-lg font-light text-nord0 hover:text-sky-700 leading-tight">
         <i class="nav-icon fas fa-users"></i>
         @lang('All Users')
       </a>
