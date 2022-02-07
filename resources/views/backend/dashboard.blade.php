7fe3e499bcf80f44218f64595fbe13f4c035b5ad
Kei <budi03@gmail.com>
2022-02-07 19:50:43 +0700

    Satukan statistik
    Statistik tidak menjadi partial lagi

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -4,7 +4,54 @@
 
 @section('content')
 <main>
-  @include('partials.b-stat')
+  <div class="bg-white p-4 shadow sm:rounded-md mb-4">
+  @role('admin')
+    <div class="flex flex-wrap">
+      <div class="w-full sm:w-1/4">
+        <span class="text-cyan-600"><i class="fas fa-square mr-2"></i>@lang('All')</span>
+        <span class="text-teal-600 ml-5"><i class="fas fa-square mr-2"></i>@lang('Me')</span>
+        <span class="text-orange-600 ml-5"><i class="fas fa-square mr-2"></i>@lang('Guest')</span>
+      </div>
+      <div class=" mt-8 sm:mt-0">
+        <b>@lang('Free Space'):</b> <span class="font-light">{{numberToAmountShort($keyRemaining)}} of {{numberToAmountShort($keyCapacity)}} ({{$remainingPercentage}})</span>
+      </div>
+    </div>
+
+    <div class="flex flex-wrap sm:mt-8">
+      <div class="w-full sm:w-1/4">
+        <div class="block">
+          <b>@lang('Urls Shortened'):</b>
+          <span class="text-cyan-600">{{numberToAmountShort($shortUrlCount)}}</span> -
+          <span class="text-teal-600">{{numberToAmountShort($shortUrlCountByMe)}}</span> -
+          <span class="text-orange-600">{{numberToAmountShort($shortUrlCountByGuest)}}</span>
+        </div>
+        <div class="block">
+          <b>@lang('Clicks & Redirects'):</b>
+          <span class="text-cyan-600">{{numberToAmountShort($clickCount)}}</span> -
+          <span class="text-teal-600">{{numberToAmountShort($clickCountFromMe)}}</span> -
+          <span class="text-orange-600">{{numberToAmountShort($clickCountFromGuest)}}</span>
+        </div>
+      </div>
+      <div class="w-full sm:w-1/4 mt-4 sm:mt-0">
+        <div class="block">
+          <b>@lang('Registered Users'):</b> <span class="font-light">{{numberToAmountShort($userCount)}}</span>
+        </div>
+        <div class="block">
+          <b cl>@lang('Guest'):</b> <span class="font-light">{{numberToAmountShort($guestCount)}}</span>
+        </div>
+      </div>
+    </div>
+  @else
+    <div class="flex flex-wrap">
+      <div class="w-full sm:w-1/4">
+        <span class="text-lg sm:text-2xl font-light">@lang('Urls Shortened'):</span> <span class="text-lg sm:text-2xl font-light">{{numberToAmountShort($shortUrlCountByMe)}}</span>
+      </div>
+      <div class="w-full sm:w-1/4">
+        <span class="text-lg sm:text-2xl font-light">@lang('Clicks & Redirects'):</span> <span class="text-lg sm:text-2xl font-light">{{numberToAmountShort($clickCountFromMe)}}</span>
+      </div>
+    </div>
+  @endrole
+  </div>
 
   <div class="bg-white p-4 shadow sm:rounded-md">
     <div class="flex mb-8">
