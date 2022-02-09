dfbcd3b5de721d090dd4c1457a371e8557b17154
Kei <budi03@gmail.com>
2022-02-09 11:59:34 +0700

    remove nord color pallete

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,26 +10,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'uh-blue': '#3d5b99',
-        nord: {
-          pn1: '#2E3440', //
-          'pn1-bold': 'rgb(41, 46, 57)',
-          pn2: '#3B4252',
-          pn3: '#434C5E',
-          pn4: '#4C566A',
-          ss1: '#D8DEE9',
-          ss2: '#E5E9F0',
-          ss3: '#ECEFF4',
-          f1: '#8FBCBB',
-          f2: '#88C0D0',
-          f3: '#81A1C1',
-          f4: '#5E81AC',
-          a1: '#BF616A',
-          a2: '#D08770',
-          a3: '#EBCB8B',
-          a4: '#A3BE8C',
-          a5: '#B48EAD',
-        },
+        'uh-blue': '#3d5b99'
       }
     }
   },
