b9c7d40af694bf0b82653f17056c12256e1756e2
Kei <budi03@gmail.com>
2022-02-17 13:39:10 +0700

    Update Admin Theme Color #799

---
+++ resources/views/vendor/livewire-powergrid/components/frameworks/tailwind/footer.blade.php
@@ -0,0 +1,36 @@
+<div class="md:flex md:flex-row w-full justify-between pt-3 bg-white overflow-y-auto pl-2 pr-2 pb-1 relative dark:bg-gray-700">
+
+    @if($perPageInput)
+        <div class="flex flex-row justify-center md:justify-start mb-2 md:mb-0">
+            <div class="relative h-10">
+
+                <select wire:model.lazy="perPage"
+                        class="block appearance-none bg-gray-50 border border-gray-300 text-gray-700 py-2 px-3 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500  dark:bg-gray-500 dark:text-gray-200 dark:placeholder-gray-200 dark:border-gray-500">
+                    @foreach($perPageValues as $value)
+                        <option value="{{$value}}">
+                            @if($value == 0)
+                                {{ trans('livewire-powergrid::datatable.labels.all') }}
+                            @else
+                                {{ $value }}
+                            @endif
+                        </option>
+                    @endforeach
+                </select>
+
+                <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                    <x-livewire-powergrid::icons.down class="w-4 h-4"/>
+                </div>
+            </div>
+            <div class="pl-4 hidden sm:block md:block lg:block w-full" style="padding-top: 6px;">
+            </div>
+        </div>
+    @endif
+
+    @if(filled($data))
+        <div>
+            @if(method_exists($data, 'links'))
+                {!! $data->links(powerGridThemeRoot().'.pagination', ['recordCount' => $recordCount]) !!}
+            @endif
+        </div>
+    @endif
+</div>
---
+++ resources/views/vendor/livewire-powergrid/components/frameworks/tailwind/table-base.blade.php
@@ -0,0 +1,34 @@
+<div class="power-grid flex flex-col">
+    <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div class="py-2 align-middle inline-block min-w-full w-full sm:px-6 lg:px-8">
+
+            @include($theme->layout->header, [
+                'enabledFilters' => $enabledFilters
+            ])
+
+            @if(config('livewire-powergrid.filter') === 'outside')
+                @if(count($makeFilters) > 0)
+                    <div>
+                        <x-livewire-powergrid::frameworks.tailwind.filter
+                            :makeFilters="$makeFilters"
+                            :inputTextOptions="$inputTextOptions"
+                            :tableName="$tableName"
+                            :filters="$filters"
+                            :theme="$theme"
+                        />
+                    </div>
+                @endif
+            @endif
+
+            <div class="my-3">
+                @include($theme->layout->message)
+            </div>
+
+            <div class="overflow-hidden overflow-x-auto bg-white shadow overflow-y-auto relative">
+                @include($table)
+            </div>
+
+            @include($theme->footer->view)
+        </div>
+    </div>
+</div>
