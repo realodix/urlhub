1321d9478aa93ab045d92b05309229f7cf1b1f05
Kei <budi03@gmail.com>
2022-02-19 20:14:19 +0700

    Hilangkan menu dashboard pada halaman admin

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -21,9 +21,11 @@
               <div x-show="open" x-transition:enter="transition ease-out duration-200" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95" class="absolute z-50 mt-2 w-48 rounded-md shadow-lg origin-top-right right-0" @click="open = false" style="display: none;">
                 <div class="rounded-md ring-1 ring-black ring-opacity-5 py-1 bg-white">
 
-                  <a class="nav-item" href="{{route('dashboard')}}">@lang('Dashboard')</a>
+                  @if (Route::currentRouteName() != 'dashboard')
+                    <a class="nav-item" href="{{route('dashboard')}}">@lang('Dashboard')</a>
 
-                  <div class="border-t border-gray-100"></div>
+                    <div class="border-t border-gray-100"></div>
+                  @endif
 
                   <!-- Account Management -->
                   <div class="block px-4 py-2 text-xs text-gray-400">
@@ -69,9 +71,11 @@
   <div :class="{'block': open, 'hidden': ! open}" x-show="open" x-transition class="navbar-mobile sm:hidden block">
     @auth
       <div class="pt-2 pb-3 space-y-1">
-        <a class="nav-item" href="{{route('dashboard')}}">
-          <i class="fas fa-tachometer-alt"></i> @lang('Dashboard')
-        </a>
+        @if (Route::currentRouteName() != 'dashboard')
+          <a class="nav-item" href="{{route('dashboard')}}">
+            <i class="fas fa-tachometer-alt"></i> @lang('Dashboard')
+          </a>
+        @endif
         <a class="nav-item" href="{{route('dashboard.allurl')}}">
           <i class="nav-icon fas fa-link"></i> @lang('All URLs')
         </a>
