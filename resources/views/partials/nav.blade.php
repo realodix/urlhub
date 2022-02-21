a2bbf4f9121ec23b4a340e6ce47582d7b8c2d9e9
Kei <budi03@gmail.com>
2022-02-21 18:56:00 +0700

    Update nav.blade.php

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -10,7 +10,7 @@
               <div @click="open = ! open">
                 <span class="inline-flex rounded-md">
                   <button type="button" class="navbar-toggler inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 rounded-md focus:outline-none transition">
-                    {{ Str::title(Auth::user()->name) }}
+                    <div class="text-base font-semibold">{{Str::title(Auth::user()->name)}}</div>
 
                     <svg class="navbar-toggler-icon ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                       <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path>
