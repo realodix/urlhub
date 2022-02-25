b4083e2fe5b750ea00f733a8efba7a54e2df1ea7
Kei <budi03@gmail.com>
2022-02-26 03:36:00 +0700

    Ubah warna tombol `copy`

--- resources/views/frontend/short.blade.php
+++ resources/views/frontend/short.blade.php
@@ -28,10 +28,10 @@
       <img class="qrcode" src="data:{{$qrCode->getContentType()}};base64,{{$qrCode->generate()}}" alt="QR Code">
     </div>
     <div class="w-full md:w-3/4 mt-8 sm:mt-0">
-      <b>@lang('Shortened URL')</b>
+      <b class="text-indigo-700">@lang('Shortened URL')</b>
       <button title="@lang('Copy to clipboard')" data-clipboard-text="{{urlDisplay($url->short_url, false)}}"
         class="btn-clipboard ml-4 py-0.5 px-1
-          text-xs text-indigo-500 hover:text-white hover:bg-indigo-500 focus:bg-indigo-600 border-indigo-500"
+          text-xs text-white bg-[#b858a9] hover:bg-[#9c3d8a] focus:bg-[#b858a9]"
       >
         @lang('Copy')
       </button>
@@ -43,10 +43,10 @@
 
       <br> <br>
 
-      <b>@lang('Destination URL')</b>
+      <b class="text-indigo-700">@lang('Destination URL')</b>
       <button title="@lang('Copy to clipboard')" data-clipboard-text="{{ $url->long_url }}"
         class="btn-clipboard ml-4 py-0.5 px-1
-          text-xs text-indigo-500 hover:text-white hover:bg-indigo-500 focus:bg-indigo-600 border-indigo-500"
+          text-xs text-white bg-[#b858a9] hover:bg-[#9c3d8a] focus:bg-[#b858a9]"
       >
         @lang('Copy')
       </button>
