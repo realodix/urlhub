4965380e9b463ec2b3abb3e669c99f3ec4b66ba6
Kei <budi03@gmail.com>
2022-02-27 14:55:29 +0700

    cleanup

--- resources/views/partials/b-footer.blade.php
+++ resources/views/partials/b-footer.blade.php
@@ -1,18 +1,18 @@
 <footer class="mt-4 sm:mt-16 py-4 text-slate-700">
   <hr class="mb-8">
-  <div class="main max-w-7xl mx-auto px-6 lg:px-8">
+  <div class="max-w-7xl mx-auto px-6 lg:px-8">
     <div class="flex flex-wrap-reverse">
       <div class="w-full sm:w-1/2">
-        <a class="" href="https://github.com/realodix/urlhub">{{appName()}}</a> <small>{{config('app.version')}}</small>
+        <a class="" href="https://github.com/realodix/urlhub">{{appName()}}</a> {{config('app.version')}}
         <span>
           &copy; 2018-present
-          <a href="https://github.com/realodix" class="text-decoration-none text-body">Budi Hermawan</a>
+          <a href="https://github.com/realodix">Budi Hermawan</a>
         </span>
       </div>
       <div class="w-full sm:w-1/2 sm:text-right">
         <span>@lang('Powered by')</span>
-        <a href="https://github.com/laravel/framework/releases/tag/v{{ App::VERSION() }}" target="_blank" title="@lang('Laravel v'.App::VERSION().' (release notes)')"
-          class="text-decoration-none text-body text-[#F9322C]"
+        <a href="https://github.com/laravel/framework/releases/tag/v{{App::VERSION()}}" target="_blank" title="@lang('Laravel v'.App::VERSION().' (release notes)')"
+          class="text-[#F9322C]"
         >
           Laravel v{{App::VERSION()}}
         </a>
