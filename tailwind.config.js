5f28a393a23de1c91b7cb18ada238e51b195b374
Kei <budi03@gmail.com>
2022-03-06 23:12:56 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,38 +3,39 @@ module.exports = {
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
     "./resources/**/*.vue",
-    './app/Http/Livewire/**/*Table.php',
-    './vendor/power-components/livewire-powergrid/resources/views/**/*.php',
-    './vendor/power-components/livewire-powergrid/src/Themes/Tailwind.php',
+    "./app/Http/Livewire/**/*Table.php",
+    "./vendor/power-components/livewire-powergrid/resources/views/**/*.php",
+    "./vendor/power-components/livewire-powergrid/src/Themes/Tailwind.php",
   ],
   theme: {
     extend: {
       colors: {
-        'uh-blue': '#3d5b99',
+        "uh-blue": "#3d5b99",
+        "bg-primary": "#f1f5f9",
         uh: {
-          1: '#344767',
-          '2a': 'rgb(63 152 222)',
-          '2b': 'rgb(53 140 208)',
-          '2c': 'rgb(43 128 194)',
-          'indigo': {
-            50: 'rgb(247 246 253)',
-            100: 'rgb(238 238 251)',
-            200: 'rgb(213 212 245)',
-            300: 'rgb(188 185 239)',
-            400: 'rgb(138 133 228)',
-            500: 'rgb(88 81 216)',
-            600: 'rgb(79 73 194)',
-            700: 'rgb(53 49 130)',
-            800: 'rgb(40 36 97)',
-            900: 'rgb(26 24 65)',
-          }
-        }
-      }
-    }
+          1: "#344767",
+          "2a": "rgb(63 152 222)",
+          "2b": "rgb(53 140 208)",
+          "2c": "rgb(43 128 194)",
+          indigo: {
+            50: "rgb(247 246 253)",
+            100: "rgb(238 238 251)",
+            200: "rgb(213 212 245)",
+            300: "rgb(188 185 239)",
+            400: "rgb(138 133 228)",
+            500: "rgb(88 81 216)",
+            600: "rgb(79 73 194)",
+            700: "rgb(53 49 130)",
+            800: "rgb(40 36 97)",
+            900: "rgb(26 24 65)",
+          },
+        },
+      },
+    },
   },
   plugins: [
     require("@tailwindcss/forms")({
-      strategy: 'class',
+      strategy: "class",
     }),
   ],
-}
+};
