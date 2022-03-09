01dabeb72da08fdab2f2cea614900857b35056a4
Kei <budi03@gmail.com>
2022-03-10 02:57:44 +0700

    cleanup

--- resources/js/backend.js
+++ resources/js/backend.js
@@ -1,16 +1,1 @@
 import './bootstrap';
-
-$(function() {
-    /**
-     * Copy short url to clipboard
-     *
-     * https://github.com/zenorocha/clipboard.js
-     */
-    var ClipboardJS = require('clipboard');
-
-    new ClipboardJS('[data-clipboard-text]').on('success', function(e) {
-        $(e.trigger)
-            .attr('data-original-title','Copied!').tooltip("_fixTitle").tooltip("show")
-            .attr("title", "Copy to clipboard").tooltip("_fixTitle");
-    });
-});
