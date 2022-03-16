74b36e8da0d6ec30ec5b4219484bc1691a8f9857
Kei <budi03@gmail.com>
2022-03-16 17:03:14 +0700

    Selaraskan warna untuk button

--- resources/views/frontend/auth/passwords/confirm.blade.php
+++ resources/views/frontend/auth/passwords/confirm.blade.php
@@ -31,12 +31,12 @@
 
             <div class="form-group row mb-0">
               <div class="col-md-8 offset-md-4">
-                <button type="submit" class="btn btn-secondary">
+                <button type="submit" class="btn btn-primary">
                   {{ __('Confirm Password') }}
                 </button>
 
                 @if (Route::has('password.request'))
-                <a class="btn btn-link" href="{{ route('password.request') }}">
+                <a class="" href="{{ route('password.request') }}">
                   {{ __('Forgot Your Password?') }}
                 </a>
                 @endif
