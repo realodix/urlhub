3bb715b59c66ae85e6c6523297973c8d67457e20
Realodix <budi03@gmail.com>
2022-05-14 02:13:05 +0700

    Pecah ke dalam 2 file

--- tests/Feature/ShortenUrlWithLongUrlAlreadyExist.php
+++ tests/Feature/ShortenUrlWithLongUrlAlreadyExist.php
@@ -5,52 +5,8 @@ namespace Tests\Feature;
 use App\Models\Url;
 use Tests\TestCase;
 
-/**
- * Front-End Test.
- */
-class UrlFeTest extends TestCase
+class ShortenUrlWithLongUrlAlreadyExist extends TestCase
 {
-    /**
-     * Users shorten the URLs, they don't fill in the custom keyword field. The
-     * is_custom column (Urls table) must be filled with 0 / false.
-     *
-     * @test
-     */
-    public function shortenUrl()
-    {
-        $longUrl = 'https://laravel.com';
-        $response = $this->post(route('createshortlink'), [
-            'long_url' => $longUrl,
-        ]);
-
-        $url = Url::whereLongUrl($longUrl)->first();
-
-        $response->assertRedirect(route('short_url.stats', $url->keyword));
-        $this->assertFalse($url->is_custom);
-    }
-
-    /**
-     * The user shortens the URL and they fill in the custom keyword field. The
-     * keyword column (Urls table) must be filled with the keywords requested
-     * by the user and the is_custom column must be filled with 1 / true.
-     *
-     * @test
-     */
-    public function shortenUrlWithCustomKeyword()
-    {
-        $longUrl = 'https://laravel.com';
-        $customKey = 'laravel';
-
-        $response = $this->post(route('createshortlink'), [
-            'long_url'   => $longUrl,
-            'custom_key' => $customKey,
-        ]);
-        $response->assertRedirect(route('short_url.stats', $customKey));
-
-        $url = Url::whereLongUrl($longUrl)->first();
-        $this->assertTrue($url->is_custom);
-    }
-
     /**
      * Guest A and guest B.
      *
@@ -168,25 +124,6 @@ class UrlFeTest extends TestCase
         $url = Url::whereUserId($user->id)->first();
 
         $response->assertRedirect(route('short_url.stats', $url->keyword));
-
-        $this->assertCount(2, Url::all());
-    }
-
-    /** @test */
-    public function duplicate()
-    {
-        $this->loginAsAdmin();
-
-        $url = Url::factory()->create([
-            'user_id' => $this->admin()->id,
-        ]);
-
-        $this->post(route('createshortlink'), [
-            'long_url' => $url->long_url,
-        ]);
-
-        $this->from(route('short_url.stats', $url->keyword))
-             ->get(route('duplicate', $url->keyword));
 
         $this->assertCount(2, Url::all());
     }
