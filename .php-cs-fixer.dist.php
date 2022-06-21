13ba6855baa0918ecd9b6d355e3ac2b1bd64d3a5
Realodix <budi03@gmail.com>
2022-06-22 01:49:09 +0700

    Update .php-cs-fixer.dist.php

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -18,7 +18,6 @@ $addOrOverrideRules = [
     'new_with_braces'  => ['named_class' => false, 'anonymous_class' => false],
     'no_empty_comment' => false,
     'phpdoc_align'     => false,
-    'phpdoc_annotation_without_dot' => false,
 ];
 
 return Config::create(new Realodix($addOrOverrideRules))
