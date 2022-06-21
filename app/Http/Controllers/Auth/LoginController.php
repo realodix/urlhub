11b99ef05e0ad76f5ddc541f11e3a9ad7d5c2847
Realodix <budi03@gmail.com>
2022-06-21 23:45:36 +0700

    Update LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -83,9 +83,8 @@ class LoginController extends Controller
      * Get the failed login response instance.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Symfony\Component\HttpFoundation\Response
-     *
      * @throws \Illuminate\Validation\ValidationException
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     protected function sendFailedLoginResponse(Request $request)
     {
