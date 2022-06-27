36f23ee80b3feb257e52a7b094c715dc106419ee
Realodix <budi03@gmail.com>
2022-06-27 15:17:49 +0700

    U-220627151749

--- app/Http/Traits/Hashidable.php
+++ app/Http/Traits/Hashidable.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Traits;
 
+use Vinkla\Hashids\Facades\Hashids;
+
 trait Hashidable
 {
     public function getRouteKey()
     {
-        return \Hashids::connection(get_called_class())->encode($this->getKey());
+        return Hashids::connection(get_called_class())->encode($this->getKey());
     }
 }
