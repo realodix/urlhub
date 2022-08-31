81e680f6b8c8e8ad1d9c39e1a6fd2f8d808cf5bf
Realodix <budi03@gmail.com>
2022-09-01 04:00:21 +0700

    app\Helpers\Global\NumHelper

--- app/Helpers/Global/NumHelper.php
+++ app/Helpers/Global/NumHelper.php
@@ -3,6 +3,11 @@
 use App\Helpers\General\NumHelper;
 
 if (! function_exists('numberToAmountShort')) {
+    /**
+     * \App\Helpers\General\NumHelper::numberToAmountShort()
+     *
+     * @return int|string
+     */
     function numberToAmountShort($value)
     {
         return resolve(NumHelper::class)->numberToAmountShort($value);
