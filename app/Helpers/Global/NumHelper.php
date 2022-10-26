db674a18124d74f665f56baf14bab5ca0d8d4f0d
Realodix <budi03@gmail.com>
2022-10-27 02:07:36 +0700

    Change some helper functions to static

--- app/Helpers/Global/NumHelper.php
+++ app/Helpers/Global/NumHelper.php
@@ -6,10 +6,11 @@ if (! function_exists('numberToAmountShort')) {
     /**
      * \App\Helpers\General\NumHelper::numberToAmountShort()
      *
+     * @param int $value
      * @return int|string
      */
     function numberToAmountShort($value)
     {
-        return resolve(NumHelper::class)->numberToAmountShort($value);
+        return NumHelper::numberToAmountShort($value);
     }
 }
