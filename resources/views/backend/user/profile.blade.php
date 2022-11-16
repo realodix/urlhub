346bc3f871590a3d83d7442c61620ed3996a8463
Realodix <budi03@gmail.com>
2022-11-16 20:15:30 +0700

    Use the default value of indent_size in the blade file

--- resources/views/backend/user/profile.blade.php
+++ resources/views/backend/user/profile.blade.php
@@ -7,40 +7,41 @@
 @include('partials/messages')
 
 <main class="flex flex-wrap">
-  <div class="md:w-3/12 flex justify-between">
-    <div class="px-4 sm:px-0">
-      <h3 class="text-lg font-medium text-slate-900">{{__('Profile Information')}}</h3>
+    <div class="md:w-3/12 flex justify-between">
+        <div class="px-4 sm:px-0">
+            <h3 class="text-lg font-medium text-slate-900">{{__('Profile Information')}}</h3>
 
-      <p class="mt-1 text-sm text-slate-600">
-        {{__("Update your account's profile information and email address.")}}
-      </p>
-    </div>
-  </div>
-  <div class="w-full md:w-8/12 lg:w-6/12 mt-5 md:mt-0 md:ml-4">
-    <form method="post" action="{{route('user.update', $user->getRouteKey())}}">
-    @csrf
-      <div class="px-4 py-5 sm:p-6 bg-white shadow sm:rounded-tl-md sm:rounded-tr-md">
-        <div class="grid grid-cols-6 gap-6">
-          <div class="col-span-6 lg:col-span-4">
-            <label for="name" class="block font-medium text-sm text-slate-700">{{__('Username')}}</label>
-            <input value="{{$user->name}}" id="name" type="text" name="name" class="form-input bg-slate-100 text-slate-700 mt-1" disabled>
-            <small class="block text-red-400"><i>{{__('Usernames cannot be changed.')}}</i></small>
-          </div>
-          <div class="col-span-6 lg:col-span-4">
-            <label for="email" class="block font-medium text-sm text-slate-700">{{__('E-mail Address')}}</label>
-            <input value="{{$user->email}}" id="email" type="email" name="email" class="form-input mt-1">
-          </div>
+            <p class="mt-1 text-sm text-slate-600">
+                {{__("Update your account's profile information and email address.")}}
+            </p>
         </div>
-      </div>
-      <div class="flex items-center justify-end px-4 py-3 sm:px-6
-            text-right bg-bg-primary sm:bg-slate-50
-            border-t sm:rounded-bl-md sm:rounded-br-md sm:shadow"
-      >
-        <button type="submit" class="btn btn-primary btn-sm">
-          {{__('Save')}}
-        </button>
-      </div>
-    </form>
-  </div>
+    </div>
+    <div class="w-full md:w-8/12 lg:w-6/12 mt-5 md:mt-0 md:ml-4">
+        <form method="post" action="{{route('user.update', $user->getRouteKey())}}">
+        @csrf
+            <div class="px-4 py-5 sm:p-6 bg-white shadow sm:rounded-tl-md sm:rounded-tr-md">
+                <div class="grid grid-cols-6 gap-6">
+                    <div class="col-span-6 lg:col-span-4">
+                        <label for="name" class="block font-medium text-sm text-slate-700">{{__('Username')}}</label>
+                        <input value="{{$user->name}}" id="name" type="text" name="name"
+                            class="form-input bg-slate-100 text-slate-700 mt-1" disabled>
+                        <small class="block text-red-400"><i>{{__('Usernames cannot be changed.')}}</i></small>
+                    </div>
+                    <div class="col-span-6 lg:col-span-4">
+                        <label for="email" class="block font-medium text-sm text-slate-700">{{__('E-mail Address')}}</label>
+                        <input value="{{$user->email}}" id="email" type="email" name="email" class="form-input mt-1">
+                    </div>
+                </div>
+            </div>
+            <div class="flex items-center justify-end px-4 py-3 sm:px-6
+                text-right bg-bg-primary sm:bg-slate-50
+                border-t sm:rounded-bl-md sm:rounded-br-md sm:shadow"
+            >
+                <button type="submit" class="btn btn-primary btn-sm">
+                    {{__('Save')}}
+                </button>
+            </div>
+        </form>
+    </div>
 </main>
 @endsection
--- resources/views/partials/b-footer.blade.php
+++ resources/views/partials/b-footer.blade.php
@@ -1,22 +1,22 @@
 <footer class="mt-4 sm:mt-16 py-4 text-slate-700">
-  <hr class="mb-8">
-  <div class="max-w-7xl mx-auto px-6 lg:px-8">
-    <div class="flex flex-wrap-reverse">
-      <div class="w-full sm:w-1/2">
-        <a class="" href="https://github.com/realodix/urlhub">{{appName()}}</a> {{config('app.version')}}
-        <span>
-          &copy; 2018-present
-          <a href="https://github.com/realodix">Budi Hermawan</a>
-        </span>
-      </div>
-      <div class="w-full sm:w-1/2 sm:text-right">
-        <span>{{__('Powered by')}}</span>
-        <a href="https://github.com/laravel/framework/releases/tag/v{{App::VERSION()}}" target="_blank" title="Laravel v{{App::VERSION()}} (release notes)"
-          class="text-[#F9322C]"
-        >
-          @svg('icon-brand-laravel') v{{App::VERSION()}}
-        </a>
-      </div>
+    <hr class="mb-8">
+    <div class="max-w-7xl mx-auto px-6 lg:px-8">
+        <div class="flex flex-wrap-reverse">
+            <div class="w-full sm:w-1/2">
+                <a class="" href="https://github.com/realodix/urlhub">{{appName()}}</a> {{config('app.version')}}
+                <span>
+                    &copy; 2018-present
+                    <a href="https://github.com/realodix">Budi Hermawan</a>
+                </span>
+            </div>
+            <div class="w-full sm:w-1/2 sm:text-right">
+                <span>{{__('Powered by')}}</span>
+                <a href="https://github.com/laravel/framework/releases/tag/v{{App::VERSION()}}" target="_blank"
+                    title="Laravel v{{App::VERSION()}} (release notes)" class="text-[#F9322C]"
+                >
+                    @svg('icon-brand-laravel') v{{App::VERSION()}}
+                </a>
+            </div>
+        </div>
     </div>
-  </div>
 </footer>
