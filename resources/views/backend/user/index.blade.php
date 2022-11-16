5f08f36b8b243943322f5e4eccb4359d1e90dd97
Realodix <budi03@gmail.com>
2022-11-16 20:15:30 +0700

    Use the default value of indent_size in the blade file

--- resources/views/backend/user/index.blade.php
+++ resources/views/backend/user/index.blade.php
@@ -4,12 +4,12 @@
 
 @section('content')
 <main>
-  <div class="bg-white p-4 sm:rounded-md shadow">
-    <div class="text-2xl text-uh-1 mb-8">
-      <span>{{__('All Users')}}</span>
-    </div>
+    <div class="bg-white p-4 sm:rounded-md shadow">
+        <div class="text-2xl text-uh-1 mb-8">
+            <span>{{__('All Users')}}</span>
+        </div>
 
-    @livewire('user-table')
-  </div>
+        @livewire('user-table')
+    </div>
 </main>
 @endsection
