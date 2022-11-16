d02cb68cff5f857d4755424f7646206554e8f452
Realodix <budi03@gmail.com>
2022-11-16 20:15:30 +0700

    Use the default value of indent_size in the blade file

--- resources/views/errors/429.blade.php
+++ resources/views/errors/429.blade.php
@@ -4,6 +4,7 @@
 @section('code', '429')
 @section('message', __('Too Many Requests'))
 @section('image')
-  <div style="background-image: url({{ asset('/svg/errors/403.svg') }});" class="absolute pin bg-cover bg-no-repeat md:bg-left lg:bg-center">
-  </div>
+<div style="background-image: url({{ asset('/svg/errors/403.svg') }});"
+    class="absolute pin bg-cover bg-no-repeat md:bg-left lg:bg-center">
+</div>
 @endsection
--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -1,23 +1,25 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-  <title>{{appName().' - '.config('app.description')}}</title>
+    <title>{{appName().' - '.config('app.description')}}</title>
 
-  {!! style(mix('css/main.css')) !!}
-  {!! style(mix('css/frontend.css')) !!}
+    {!! style(mix('css/main.css')) !!}
+    {!! style(mix('css/frontend.css')) !!}
 </head>
 
 <body class="@yield('css_class')"></body>
-@include('partials.nav-header')
+    @include('partials.nav-header')
 
-@yield('content')
+    @yield('content')
 
-{!! script(mix('js/manifest.js')) !!}
-{!! script(mix('js/vendor.js')) !!}
-{!! script(mix('js/frontend.js')) !!}
+    {!! script(mix('js/manifest.js')) !!}
+    {!! script(mix('js/vendor.js')) !!}
+    {!! script(mix('js/frontend.js')) !!}
 </body>
+
 </html>
