8702aebd43f8b303c1163ce156c93381cdac18d5
Realodix <budi03@gmail.com>
2022-11-16 20:15:30 +0700

    Use the default value of indent_size in the blade file

--- resources/views/errors/401.blade.php
+++ resources/views/errors/401.blade.php
@@ -4,6 +4,7 @@
 @section('code', '401')
 @section('message', __('Unauthorized'))
 @section('image')
-  <div style="background-image: url({{ asset('/svg/errors/403.svg') }});" class="absolute pin bg-cover bg-no-repeat md:bg-left lg:bg-center">
-  </div>
+<div style="background-image: url({{ asset('/svg/errors/403.svg') }});"
+    class="absolute pin bg-cover bg-no-repeat md:bg-left lg:bg-center">
+</div>
 @endsection
