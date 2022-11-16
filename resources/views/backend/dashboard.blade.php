efd46702259ea0dc02e62e9a6617baa1915d0b27
Realodix <budi03@gmail.com>
2022-11-16 20:15:30 +0700

    Use the default value of indent_size in the blade file

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -4,78 +4,77 @@
 
 @section('content')
 <main>
-  <div class="mb-4 bg-white p-4 shadow sm:rounded-md">
-  @role('admin')
-    <div class="flex flex-wrap">
-      <div class="w-full sm:w-1/4">
-        <span class="text-cyan-600"> @svg('icon-square', 'mr-2') {{__('All')}}</span>
-        <span class="text-teal-600 ml-5"> @svg('icon-square', 'mr-2') {{__('Me')}}</span>
-        <span class="text-orange-600 ml-5"> @svg('icon-square', 'mr-2') {{__('Guests')}}</span>
-      </div>
-      <div class="mt-8 sm:mt-0 text-uh-1 ">
-        <b>@svg('icon-storage', 'mr-1.5') {{__('Free Space')}}:</b>
-        <span class="font-light">{{numberToAmountShort($keyRemaining)}} {{__('of')}} {{numberToAmountShort($keyCapacity)}} ({{$keyRemaining_Percent}})</span>
-      </div>
+    <div class="mb-4 bg-white p-4 shadow sm:rounded-md">
+        @role('admin')
+            <div class="flex flex-wrap">
+                <div class="w-full sm:w-1/4">
+                    <span class="text-cyan-600"> @svg('icon-square', 'mr-2') {{__('All')}}</span>
+                    <span class="text-teal-600 ml-5"> @svg('icon-square', 'mr-2') {{__('Me')}}</span>
+                    <span class="text-orange-600 ml-5"> @svg('icon-square', 'mr-2') {{__('Guests')}}</span>
+                </div>
+                <div class="mt-8 sm:mt-0 text-uh-1 ">
+                    <b>@svg('icon-storage', 'mr-1.5') {{__('Free Space')}}:</b>
+                    <span class="font-light">{{numberToAmountShort($keyRemaining)}} {{__('of')}}
+                        {{numberToAmountShort($keyCapacity)}} ({{$keyRemaining_Percent}})</span>
+                </div>
+            </div>
+
+            <div class="flex flex-wrap sm:mt-8">
+                <div class="w-full sm:w-1/4">
+                    <div class="block">
+                        <b class="text-uh-1">@svg('icon-link', 'mr-1.5') {{__('URLs')}}:</b>
+                        <span class="text-cyan-600">{{numberToAmountShort($totalUrl)}}</span> -
+                        <span class="text-teal-600">{{numberToAmountShort($urlCount_Me)}}</span> -
+                        <span class="text-orange-600">{{numberToAmountShort($urlCount_Guest)}}</span>
+                    </div>
+                    <div class="block">
+                        <b class="text-uh-1">@svg('icon-bar-chart', 'mr-1.5') {{__('Clicks')}}:</b>
+                        <span class="text-cyan-600">{{numberToAmountShort($totalClick)}}</span> -
+                        <span class="text-teal-600">{{numberToAmountShort($clickCount_Me)}}</span> -
+                        <span class="text-orange-600">{{numberToAmountShort($clickCount_Guest)}}</span>
+                    </div>
+                </div>
+                <div class="text-uh-1 w-full sm:w-1/4 mt-4 sm:mt-0">
+                    <div class="block">
+                        <b>@svg('icon-user', 'mr-1.5') {{__('Users')}}:</b>
+                        <span class="font-light">{{numberToAmountShort($userCount)}}</span>
+                    </div>
+                    <div class="block">
+                        <b>@svg('icon-user', 'mr-1.5') {{__('Guests')}}:</b>
+                        <span class="font-light">{{numberToAmountShort($guestCount)}}</span>
+                    </div>
+                </div>
+            </div>
+        @else
+            <div class="flex flex-wrap">
+                <div class="w-full sm:w-1/4">
+                    <span class="font-semibold text-md sm:text-2xl">@svg('icon-link', 'mr-1.5') {{__('URLs')}}:</span>
+                    <span class="font-light text-lg sm:text-2xl">{{numberToAmountShort($urlCount_Me)}}</span>
+                </div>
+                <div class="w-full sm:w-1/4">
+                    <span class="font-semibold text-lg sm:text-2xl">@svg('icon-eye', 'mr-1.5') {{__('Clicks')}}:</span>
+                    <span class="font-light text-lg sm:text-2xl">{{numberToAmountShort($clickCount_Me)}}</span>
+                </div>
+            </div>
+        @endrole
     </div>
 
-    <div class="flex flex-wrap sm:mt-8">
-      <div class="w-full sm:w-1/4">
-        <div class="block">
-          <b class="text-uh-1">@svg('icon-link', 'mr-1.5') {{__('URLs')}}:</b>
-          <span class="text-cyan-600">{{numberToAmountShort($totalUrl)}}</span> -
-          <span class="text-teal-600">{{numberToAmountShort($urlCount_Me)}}</span> -
-          <span class="text-orange-600">{{numberToAmountShort($urlCount_Guest)}}</span>
+    <div class="bg-white p-4 shadow sm:rounded-md">
+        <div class="flex mb-8">
+            <div class="w-1/2">
+                <span class="text-2xl text-uh-1">{{__('My URLs')}}</span>
+            </div>
+            <div class="w-1/2 text-right">
+                <a href="{{ url('./') }}" target="_blank" title="{{__('Add URL')}}" class="btn">
+                    @svg('icon-add-link', '!h-[1.5em] mr-1')
+                    {{__('Add URL')}}
+                </a>
+            </div>
         </div>
-        <div class="block">
-          <b class="text-uh-1">@svg('icon-bar-chart', 'mr-1.5') {{__('Clicks')}}:</b>
-          <span class="text-cyan-600">{{numberToAmountShort($totalClick)}}</span> -
-          <span class="text-teal-600">{{numberToAmountShort($clickCount_Me)}}</span> -
-          <span class="text-orange-600">{{numberToAmountShort($clickCount_Guest)}}</span>
-        </div>
-      </div>
-      <div class="text-uh-1 w-full sm:w-1/4 mt-4 sm:mt-0">
-        <div class="block">
-          <b>@svg('icon-user', 'mr-1.5') {{__('Users')}}:</b>
-          <span class="font-light">{{numberToAmountShort($userCount)}}</span>
-        </div>
-        <div class="block">
-          <b>@svg('icon-user', 'mr-1.5') {{__('Guests')}}:</b>
-          <span class="font-light">{{numberToAmountShort($guestCount)}}</span>
-        </div>
-      </div>
-    </div>
-  @else
-    <div class="flex flex-wrap">
-      <div class="w-full sm:w-1/4">
-        <span class="font-semibold text-md sm:text-2xl">@svg('icon-link', 'mr-1.5') {{__('URLs')}}:</span>
-        <span class="font-light text-lg sm:text-2xl">{{numberToAmountShort($urlCount_Me)}}</span>
-      </div>
-      <div class="w-full sm:w-1/4">
-        <span class="font-semibold text-lg sm:text-2xl">@svg('icon-eye', 'mr-1.5') {{__('Clicks')}}:</span>
-        <span class="font-light text-lg sm:text-2xl">{{numberToAmountShort($clickCount_Me)}}</span>
-      </div>
-    </div>
-  @endrole
-  </div>
 
-  <div class="bg-white p-4 shadow sm:rounded-md">
-    <div class="flex mb-8">
-      <div class="w-1/2">
-        <span class="text-2xl text-uh-1">{{__('My URLs')}}</span>
-      </div>
-      <div class="w-1/2 text-right">
-        <a href="{{ url('./') }}" target="_blank" title="{{__('Add URL')}}"
-          class="btn"
-        >
-          @svg('icon-add-link', '!h-[1.5em] mr-1')
-          {{__('Add URL')}}
-        </a>
-      </div>
+        @include('partials/messages')
+
+        @livewire('my-url-table')
     </div>
-
-    @include('partials/messages')
-
-    @livewire('my-url-table')
-  </div>
 </main>
 @endsection
--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -4,6 +4,7 @@
 @section('code', '403')
 @section('message', __($exception->getMessage() ?: 'Forbidden'))
 @section('image')
-  <div style="background-image: url({{ asset('/svg/errors/403.svg') }});" class="absolute pin bg-cover bg-no-repeat md:bg-left lg:bg-center">
-  </div>
+<div style="background-image: url({{ asset('/svg/errors/403.svg') }});"
+    class="absolute pin bg-cover bg-no-repeat md:bg-left lg:bg-center">
+</div>
 @endsection
