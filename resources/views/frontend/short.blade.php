b1c4813b7205e9ec4700fd5023107f2d9e22cc36
Realodix <budi03@gmail.com>
2022-11-16 20:15:30 +0700

    Use the default value of indent_size in the blade file

--- resources/views/frontend/short.blade.php
+++ resources/views/frontend/short.blade.php
@@ -4,73 +4,73 @@
 
 @section('content')
 <div class="max-w-7xl mx-auto mb-12">
-  <div class="flex flex-wrap mt-6 lg:mt-12 px-4 sm:p-6">
-    <div class="md:w-9/12">
+    <div class="flex flex-wrap mt-6 lg:mt-12 px-4 sm:p-6">
+        <div class="md:w-9/12">
 
-      @include('partials/messages')
+            @include('partials/messages')
 
-      <ul>
-        <li class="inline-block pr-4">
-          @svg('icon-calendar')
-          <i>{{$url->created_at->toDayDateTimeString()}}</i>
-        </li>
-        <li class="inline-block pr-4">
-          @svg('icon-bar-chart')
-          <i><span title="{{number_format($url->clicks)}}">{{numberToAmountShort($url->clicks)}}</span></i>
-        </li>
-        @auth
-          @if (Auth::user()->hasRole('admin') || (Auth::user()->id === $url->user_id))
-            <li class="inline-block pr-2">
-              <a href="{{route('short_url.edit', $url->keyword)}}" title="{{__('Edit')}}"
-                class="btn-icon text-xs"
-              >
-                @svg('icon-edit')
-              </a>
-            </li>
-            <li class="inline-block">
-              <a href="{{route('dashboard.delete', $url->getRouteKey())}}" title="{{__('Delete')}}"
-                class="btn-icon text-xs hover:text-red-700 active:text-red-600"
-              >
-                @svg('icon-trash')
-              </a>
-            </li>
-          @endif
-        @endauth
-      </ul>
-      <div class="text-xl sm:text-2xl lg:text-3xl mt-2 font-light">{!! $url->meta_title !!}</div>
+            <ul>
+                <li class="inline-block pr-4">
+                    @svg('icon-calendar')
+                    <i>{{$url->created_at->toDayDateTimeString()}}</i>
+                </li>
+                <li class="inline-block pr-4">
+                    @svg('icon-bar-chart')
+                    <i><span title="{{number_format($url->clicks)}}">{{numberToAmountShort($url->clicks)}}</span></i>
+                </li>
+                @auth
+                    @if (Auth::user()->hasRole('admin') || (Auth::user()->id === $url->user_id))
+                        <li class="inline-block pr-2">
+                            <a href="{{route('short_url.edit', $url->keyword)}}" title="{{__('Edit')}}"
+                                class="btn-icon text-xs">
+                                @svg('icon-edit')
+                            </a>
+                        </li>
+                        <li class="inline-block">
+                            <a href="{{route('dashboard.delete', $url->getRouteKey())}}" title="{{__('Delete')}}"
+                                class="btn-icon text-xs hover:text-red-700 active:text-red-600">
+                                @svg('icon-trash')
+                            </a>
+                        </li>
+                    @endif
+                @endauth
+            </ul>
+            <div class="text-xl sm:text-2xl lg:text-3xl mt-2 font-light">{!! $url->meta_title !!}</div>
+        </div>
     </div>
-  </div>
 
-  <div class="flex flex-wrap mt-6 sm:mt-0 px-4 py-5 sm:p-6 bg-white shadow sm:rounded-md">
-    @if (config('urlhub.qrcode'))
-    <div class="w-full md:w-1/4 flex justify-center">
-      <img class="qrcode" src="{{$qrCode->getDataUri()}}" alt="QR Code">
+    <div class="flex flex-wrap mt-6 sm:mt-0 px-4 py-5 sm:p-6 bg-white shadow sm:rounded-md">
+        @if (config('urlhub.qrcode'))
+            <div class="w-full md:w-1/4 flex justify-center">
+                <img class="qrcode" src="{{$qrCode->getDataUri()}}" alt="QR Code">
+            </div>
+        @endif
+        <div class="w-full md:w-3/4 mt-8 sm:mt-0">
+            <b class="text-indigo-700">{{__('Shortened URL')}}</b>
+            <button title="{{__('Copy the shortened URL to clipboard')}}"
+                data-clipboard-text="{{urlDisplay($url->short_url, false)}}"
+                class="btn-clipboard btn-icon text-xs ml-4">
+                @svg('icon-clone')
+            </button>
+
+            <br>
+
+            <span class="font-light">
+                <a href="{{ $url->short_url }}" target="_blank" id="copy">
+                    {{urlDisplay($url->short_url, false)}}
+                </a>
+            </span>
+
+            <br> <br>
+
+            <b class="text-indigo-700">{{__('Destination URL')}}</b>
+            <button title="{{__('Copy the destination URL to clipboard')}}" data-clipboard-text="{{ $url->long_url }}"
+                class="btn-clipboard btn-icon text-xs ml-4">
+                @svg('icon-clone')
+            </button>
+
+            <div class="font-light break-all">{{ $url->long_url }}</div>
+        </div>
     </div>
-    @endif
-    <div class="w-full md:w-3/4 mt-8 sm:mt-0">
-      <b class="text-indigo-700">{{__('Shortened URL')}}</b>
-      <button title="{{__('Copy the shortened URL to clipboard')}}" data-clipboard-text="{{urlDisplay($url->short_url, false)}}"
-        class="btn-clipboard btn-icon text-xs ml-4"
-      >
-        @svg('icon-clone')
-      </button>
-
-      <br>
-
-      <span class="font-light"><a href="{{ $url->short_url }}" target="_blank"
-        id="copy">{{urlDisplay($url->short_url, false)}}</a></span>
-
-      <br> <br>
-
-      <b class="text-indigo-700">{{__('Destination URL')}}</b>
-      <button title="{{__('Copy the destination URL to clipboard')}}" data-clipboard-text="{{ $url->long_url }}"
-        class="btn-clipboard btn-icon text-xs ml-4"
-      >
-        @svg('icon-clone')
-      </button>
-
-      <div class="font-light break-all">{{ $url->long_url }}</div>
-    </div>
-  </div>
 </div>
 @endsection
--- resources/views/partials/messages.blade.php
+++ resources/views/partials/messages.blade.php
@@ -1,23 +1,23 @@
 @if($errors->any())
-  <div class="block mb-4 pl-3 pr-4 py-2 font-medium text-base text-orange-700 bg-orange-50 border-l-4 border-orange-400" role="alert">
-    @foreach($errors->all() as $error)
-      {{ $error }}<br/>
-    @endforeach
-  </div>
+    <div class="block mb-4 pl-3 pr-4 py-2 font-medium text-base text-orange-700 bg-orange-50 border-l-4 border-orange-400" role="alert">
+        @foreach($errors->all() as $error)
+            {{ $error }} <br>
+        @endforeach
+    </div>
 @elseif (session('flash_success'))
-  <div class="block mb-4 pl-3 pr-4 py-2 font-medium text-base text-emerald-700 bg-emerald-50 border-l-4 border-emerald-400" role="alert">
-    {{ session('flash_success') }}
-  </div>
+    <div class="block mb-4 pl-3 pr-4 py-2 font-medium text-base text-emerald-700 bg-emerald-50 border-l-4 border-emerald-400" role="alert">
+        {{ session('flash_success') }}
+    </div>
 @elseif (session('flash_error'))
-  <div class="block mb-4 pl-3 pr-4 py-2 font-medium text-base text-red-700 bg-red-50 border-l-4 border-red-400" role="alert">
-    {{ session('flash_error') }}
-    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-      <span aria-hidden="true">&times;</span>
-    </button>
-  </div>
+    <div class="block mb-4 pl-3 pr-4 py-2 font-medium text-base text-red-700 bg-red-50 border-l-4 border-red-400" role="alert">
+        {{ session('flash_error') }}
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
 @elseif (session('msgLinkAlreadyExists'))
-  <div class="block mb-4 pl-3 pr-4 py-2 font-medium text-base text-emerald-700 bg-emerald-50 border-l-4 border-emerald-400">
-    {{ session('msgLinkAlreadyExists') }}
-    @auth<a href="{{route('duplicate', $url->keyword)}}">{{__('Duplicate this')}}<a>@endauth
-  </div>
+    <div class="block mb-4 pl-3 pr-4 py-2 font-medium text-base text-emerald-700 bg-emerald-50 border-l-4 border-emerald-400">
+        {{ session('msgLinkAlreadyExists') }}
+        @auth<a href="{{route('duplicate', $url->keyword)}}">{{__('Duplicate this')}}<a>@endauth
+    </div>
 @endif
