088c04badd9dae4f7eaafa40b773ea6def379b46
Realodix <budi03@gmail.com>
2022-11-19 05:10:53 +0700

    Update UrlCheck.php

--- app/Http/Livewire/UrlCheck.php
+++ app/Http/Livewire/UrlCheck.php
@@ -6,8 +6,16 @@ use Livewire\Component;
 use App\Rules\StrAlphaUnderscore;
 class UrlCheck extends Component
 {
+    /**
+     * @var string
+     */
     public $keyword;
 
+    /**
+     * https://github.com/livewire/livewire/blob/6aaa3ec8567e607a25281a85ec9d3404d146f20f/src/ComponentConcerns/ValidatesInput.php#L80
+     *
+     * @return array
+     */
     public function rules()
     {
         return [
@@ -17,11 +25,18 @@ class UrlCheck extends Component
         ];
     }
 
+    /**
+     * @param string $propertyName
+     * @return void
+     */
     public function updated($propertyName)
     {
         $this->validateOnly($propertyName);
     }
 
+    /**
+     * @return \Illuminate\View\View
+     */
     public function render()
     {
         return view('livewire.url-check');
