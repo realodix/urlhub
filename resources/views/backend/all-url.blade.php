0837450ebd05bbd0fae0076e072438b3260b4d00
Kei <budi03@gmail.com>
2022-12-01 07:32:07 +0700

    formatting

--- resources/views/backend/all-url.blade.php
+++ resources/views/backend/all-url.blade.php
@@ -3,25 +3,25 @@
 @section('title', __('All URLs'))
 
 @section('content')
-<main>
-    <div class="common-card-style p-4">
-        <div class="flex mb-8">
-            <div class="w-1/2">
-                <span class="text-2xl text-uh-1">
-                    {{__('All URLs')}}
-                </span>
+    <main>
+        <div class="common-card-style p-4">
+            <div class="flex mb-8">
+                <div class="w-1/2">
+                    <span class="text-2xl text-uh-1">
+                        {{__('All URLs')}}
+                    </span>
+                </div>
+                <div class="w-1/2 text-right">
+                    <a href="{{ url('./') }}" target="_blank" title="{{__('Add URL')}}" class="btn">
+                        @svg('icon-add-link', '!h-[1.5em] mr-1')
+                        {{__('Add URL')}}
+                    </a>
+                </div>
             </div>
-            <div class="w-1/2 text-right">
-                <a href="{{ url('./') }}" target="_blank" title="{{__('Add URL')}}" class="btn">
-                    @svg('icon-add-link', '!h-[1.5em] mr-1')
-                    {{__('Add URL')}}
-                </a>
-            </div>
+
+            @include('partials/messages')
+
+            @livewire('table.all-ulr-table')
         </div>
-
-        @include('partials/messages')
-
-        @livewire('table.all-ulr-table')
-    </div>
-</main>
+    </main>
 @endsection
