c26d95d65f641748d881695ee3ab1e7ac5d10003
Kei <budi03@gmail.com>
2022-12-01 07:32:07 +0700

    formatting

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,53 +5,53 @@
 @section('css_class', 'auth')
 
 @section('content')
-<div class="flex flex-col min-h-screen sm:justify-center items-center pt-6 sm:pt-0">
+    <div class="flex flex-col min-h-screen sm:justify-center items-center pt-6 sm:pt-0">
 
-    <div class="font-bold text-uh-blue text-4xl sm:text-6xl">{{config('app.name')}}</div>
+        <div class="font-bold text-uh-blue text-4xl sm:text-6xl">{{config('app.name')}}</div>
 
-    @if(session()->has('login_error'))
-        <div class="alert alert-danger alert-dismissible fade show" role="alert">
-            {{ session()->get('login_error') }}
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-        </div>
-        {{ session()->forget('login_error') }}
-    @endif
-
-    <div class="common-card-style w-full sm:max-w-md mt-6 px-12 py-8 overflow-hidden">
-        <form method="POST" action="{{ route('login') }}" aria-label="{{__('Login')}}">
-        @csrf
-            <div>
-                @if (Request::has('previous'))
-                    <input type="hidden" name="previous" value="{{ Request::get('previous') }}">
-                @else
-                    <input type="hidden" name="previous" value="{{ URL::previous() }}">
-                @endif
-                <label for="email" class="block font-medium text-sm text-slate-700">
-                    {{__('E-Mail / Username')}}
-                </label>
-                <input type="text" name="identity" class="form-input mt-1" value="{{ old('identity') }}" required autofocus>
-            </div>
-
-            <div class="mt-4">
-                <label for="password" class="block font-medium text-sm text-slate-700">
-                    {{__('Password')}}
-                </label>
-                <input type="password" name="password" class="form-input mt-1" autocomplete="current-password" required>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <a href="{{ route('password.request') }}" class="text-sm text-slate-600 hover:text-slate-900 underline">
-                    {{__('Forgot your password?')}}
-                </a>
-
-                <button type="submit" class="btn btn-primary ml-4">
-                    {{__('Log in')}}
+        @if(session()->has('login_error'))
+            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                {{ session()->get('login_error') }}
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
                 </button>
             </div>
-        </form>
-    </div>
+            {{ session()->forget('login_error') }}
+        @endif
 
-</div>
+        <div class="common-card-style w-full sm:max-w-md mt-6 px-12 py-8 overflow-hidden">
+            <form method="POST" action="{{ route('login') }}" aria-label="{{__('Login')}}">
+            @csrf
+                <div>
+                    @if (Request::has('previous'))
+                        <input type="hidden" name="previous" value="{{ Request::get('previous') }}">
+                    @else
+                        <input type="hidden" name="previous" value="{{ URL::previous() }}">
+                    @endif
+                    <label for="email" class="block font-medium text-sm text-slate-700">
+                        {{__('E-Mail / Username')}}
+                    </label>
+                    <input type="text" name="identity" class="form-input mt-1" value="{{ old('identity') }}" required autofocus>
+                </div>
+
+                <div class="mt-4">
+                    <label for="password" class="block font-medium text-sm text-slate-700">
+                        {{__('Password')}}
+                    </label>
+                    <input type="password" name="password" class="form-input mt-1" autocomplete="current-password" required>
+                </div>
+
+                <div class="flex items-center justify-end mt-4">
+                    <a href="{{ route('password.request') }}" class="text-sm text-slate-600 hover:text-slate-900 underline">
+                        {{__('Forgot your password?')}}
+                    </a>
+
+                    <button type="submit" class="btn btn-primary ml-4">
+                        {{__('Log in')}}
+                    </button>
+                </div>
+            </form>
+        </div>
+
+    </div>
 @endsection
--- resources/views/frontend/short.blade.php
+++ resources/views/frontend/short.blade.php
@@ -3,74 +3,74 @@
 @section('css_class', 'frontend view_short')
 
 @section('content')
-<div class="max-w-7xl mx-auto mb-12">
-    <div class="flex flex-wrap mt-6 lg:mt-12 px-4 sm:p-6">
-        <div class="md:w-9/12">
+    <div class="max-w-7xl mx-auto mb-12">
+        <div class="flex flex-wrap mt-6 lg:mt-12 px-4 sm:p-6">
+            <div class="md:w-9/12">
 
-            @include('partials/messages')
+                @include('partials/messages')
 
-            <ul>
-                <li class="inline-block pr-4">
-                    @svg('icon-calendar')
-                    <i>{{$url->created_at->toDayDateTimeString()}}</i>
-                </li>
-                <li class="inline-block pr-4">
-                    @svg('icon-bar-chart')
-                    <i><span title="{{number_format($url->clicks)}}">{{numberShorten($url->clicks)}}</span></i>
-                </li>
-                @auth
-                    @if (Auth::user()->hasRole('admin') || (Auth::user()->id === $url->user_id))
-                        <li class="inline-block pr-2">
-                            <a href="{{route('short_url.edit', $url->keyword)}}" title="{{__('Edit')}}"
-                                class="btn-icon text-xs">
-                                @svg('icon-edit')
-                            </a>
-                        </li>
-                        <li class="inline-block">
-                            <a href="{{route('short_url.delete', $url->getRouteKey())}}" title="{{__('Delete')}}"
-                                class="btn-icon text-xs hover:text-red-700 active:text-red-600">
-                                @svg('icon-trash')
-                            </a>
-                        </li>
-                    @endif
-                @endauth
-            </ul>
-            <div class="text-xl sm:text-2xl lg:text-3xl mt-2 font-light">{!! $url->meta_title !!}</div>
-        </div>
-    </div>
-
-    <div class="common-card-style flex flex-wrap mt-6 sm:mt-0 px-4 py-5 sm:p-6">
-        @if (config('urlhub.qrcode'))
-            <div class="w-full md:w-1/4 flex justify-center">
-                <img class="qrcode" src="{{$qrCode->getDataUri()}}" alt="QR Code">
+                <ul>
+                    <li class="inline-block pr-4">
+                        @svg('icon-calendar')
+                        <i>{{$url->created_at->toDayDateTimeString()}}</i>
+                    </li>
+                    <li class="inline-block pr-4">
+                        @svg('icon-bar-chart')
+                        <i><span title="{{number_format($url->clicks)}}">{{numberShorten($url->clicks)}}</span></i>
+                    </li>
+                    @auth
+                        @if (Auth::user()->hasRole('admin') || (Auth::user()->id === $url->user_id))
+                            <li class="inline-block pr-2">
+                                <a href="{{route('short_url.edit', $url->keyword)}}" title="{{__('Edit')}}"
+                                    class="btn-icon text-xs">
+                                    @svg('icon-edit')
+                                </a>
+                            </li>
+                            <li class="inline-block">
+                                <a href="{{route('short_url.delete', $url->getRouteKey())}}" title="{{__('Delete')}}"
+                                    class="btn-icon text-xs hover:text-red-700 active:text-red-600">
+                                    @svg('icon-trash')
+                                </a>
+                            </li>
+                        @endif
+                    @endauth
+                </ul>
+                <div class="text-xl sm:text-2xl lg:text-3xl mt-2 font-light">{!! $url->meta_title !!}</div>
             </div>
-        @endif
-        <div class="w-full md:w-3/4 mt-8 sm:mt-0">
-            <b class="text-indigo-700">{{__('Shortened URL')}}</b>
-            <button title="{{__('Copy the shortened URL to clipboard')}}"
-                data-clipboard-text="{{urlDisplay($url->short_url, false)}}"
-                class="btn-clipboard btn-icon text-xs ml-4">
-                @svg('icon-clone')
-            </button>
+        </div>
 
-            <br>
+        <div class="common-card-style flex flex-wrap mt-6 sm:mt-0 px-4 py-5 sm:p-6">
+            @if (config('urlhub.qrcode'))
+                <div class="w-full md:w-1/4 flex justify-center">
+                    <img class="qrcode" src="{{$qrCode->getDataUri()}}" alt="QR Code">
+                </div>
+            @endif
+            <div class="w-full md:w-3/4 mt-8 sm:mt-0">
+                <b class="text-indigo-700">{{__('Shortened URL')}}</b>
+                <button title="{{__('Copy the shortened URL to clipboard')}}"
+                    data-clipboard-text="{{urlDisplay($url->short_url, false)}}"
+                    class="btn-clipboard btn-icon text-xs ml-4">
+                    @svg('icon-clone')
+                </button>
 
-            <span class="font-light">
-                <a href="{{ $url->short_url }}" target="_blank" id="copy">
-                    {{urlDisplay($url->short_url, false)}}
-                </a>
-            </span>
+                <br>
 
-            <br> <br>
+                <span class="font-light">
+                    <a href="{{ $url->short_url }}" target="_blank" id="copy">
+                        {{urlDisplay($url->short_url, false)}}
+                    </a>
+                </span>
 
-            <b class="text-indigo-700">{{__('Destination URL')}}</b>
-            <button title="{{__('Copy the destination URL to clipboard')}}" data-clipboard-text="{{ $url->long_url }}"
-                class="btn-clipboard btn-icon text-xs ml-4">
-                @svg('icon-clone')
-            </button>
+                <br> <br>
 
-            <div class="font-light break-all">{{ $url->long_url }}</div>
+                <b class="text-indigo-700">{{__('Destination URL')}}</b>
+                <button title="{{__('Copy the destination URL to clipboard')}}" data-clipboard-text="{{ $url->long_url }}"
+                    class="btn-clipboard btn-icon text-xs ml-4">
+                    @svg('icon-clone')
+                </button>
+
+                <div class="font-light break-all">{{ $url->long_url }}</div>
+            </div>
         </div>
     </div>
-</div>
 @endsection
