502c2622f67ff2bb025a5bdbb6149eb982b15107
Kei <budi03@gmail.com>
2022-12-03 06:41:01 +0700

    ref:: Edit message for `long_url.required`

--- app/Http/Requests/StoreUrl.php
+++ app/Http/Requests/StoreUrl.php
@@ -39,7 +39,7 @@ class StoreUrl extends FormRequest
     public function messages()
     {
         return [
-            'long_url.required' => __('Must be filled, should not be empty.'),
+            'long_url.required' => __('The URL field must be filled, should not be empty.'),
             'custom_key.max'    => __('The custom url may not be greater than :max characters.'),
             'custom_key.unique' => __(':input has already been taken'),
         ];
