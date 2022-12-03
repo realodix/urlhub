fd4985cdb3fd744814b6a136e341db6be6f43523
Kei <budi03@gmail.com>
2022-12-03 18:17:50 +0700

    formatting

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -38,7 +38,7 @@
                     <div class="text-uh-1 w-full sm:w-1/4 mt-4 sm:mt-0">
                         <div class="block">
                             <b>@svg('icon-user', 'mr-1.5') {{__('Users')}}:</b>
-                            <span class="font-light">{{numberShorten($user->count())}}ddd</span>
+                            <span class="font-light">{{numberShorten($user->count())}}</span>
                         </div>
                         <div class="block">
                             <b>@svg('icon-user', 'mr-1.5') {{__('Guests')}}:</b>
