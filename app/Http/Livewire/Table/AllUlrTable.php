b95c96744ddf3ad47b2922a886dff45c3ffde938
Kei <budi03@gmail.com>
2022-12-05 01:19:47 +0700

    ref:: Display the number of clicks in compact number format

--- app/Http/Livewire/Table/AllUlrTable.php
+++ app/Http/Livewire/Table/AllUlrTable.php
@@ -108,7 +108,7 @@ final class AllUlrTable extends PowerGridComponent
                         .Blade::render('@svg(\'icon-open-in-new\', \'!h-[0.7em] ml-1\')').
                     '</a>';
             })
-            ->addColumn('clicks', fn (Url $url) => $url->clicks.Blade::render('@svg(\'icon-bar-chart\', \'ml-2 text-indigo-600\')'))
+            ->addColumn('clicks', fn (Url $url) => numberShorten($url->clicks).Blade::render('@svg(\'icon-bar-chart\', \'ml-2 text-indigo-600\')'))
             ->addColumn('created_at_formatted', function (Url $url) {
                 /** @var \Carbon\Carbon */
                 $urlCreatedAt = $url->created_at;
