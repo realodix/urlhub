71dc97f7d3b6c4a2054b6ec260edfd2eb92e6a81
Kei <budi03@gmail.com>
2022-12-09 00:25:17 +0700

    Revert "test: Move createApplication() to testcase file"
    This reverts commit e952bcc9ebf6b958f2a7674aaef7d25ee34408c0.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,27 +2,13 @@
 
 namespace Tests;
 
-use Illuminate\Contracts\Console\Kernel;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Tests\Support\Auth;
+use Tests\Support\CreatesApplication;
 
 abstract class TestCase extends BaseTestCase
 {
     use RefreshDatabase;
-    use Auth;
-
-    /**
-     * Creates the application.
-     *
-     * @return \Illuminate\Foundation\Application
-     */
-    public function createApplication()
-    {
-        $app = require __DIR__.'/../bootstrap/app.php';
-
-        $app->make(Kernel::class)->bootstrap();
-
-        return $app;
-    }
+    use Auth, CreatesApplication;
 }
