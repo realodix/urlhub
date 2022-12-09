9b8192443cfcc680c2a8175f5ce3517beb1c4920
Kei <budi03@gmail.com>
2022-12-09 18:11:52 +0700

    Add missing PHPDoc in database factory

--- database/factories/UrlFactory.php
+++ database/factories/UrlFactory.php
@@ -6,6 +6,9 @@ use App\Models\Url;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Url>
+ */
 class UrlFactory extends Factory
 {
     /**
