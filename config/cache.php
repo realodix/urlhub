5c9564db789b1fdaf9bb49694b92726ebdcc24b6
Kei <budi03@gmail.com>
2023-08-23 23:52:45 +0700

    sync: add lock path
    https://github.com/laravel/laravel/commit/a6bfbc7f90e33fd6cae3cb23f106c9689858c3b5

--- config/cache.php
+++ config/cache.php
@@ -51,7 +51,8 @@ return [
 
         'file' => [
             'driver' => 'file',
-            'path'   => storage_path('framework/cache/data'),
+            'path' => storage_path('framework/cache/data'),
+            'lock_path' => storage_path('framework/cache/data'),
         ],
 
         'memcached' => [
