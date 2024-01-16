fdff2d3c33bd8cf723ec8701f3482311ff8a284e
Kei <budi03@gmail.com>
2024-01-17 01:19:17 +0700

    sync: Fixes missing property description
    https://github.com/laravel/laravel/commit/73cf5bc5bcbf8af21d70a41a00473b53e6cc04d4

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -12,6 +12,9 @@ use Illuminate\Support\Str;
  */
 class UserFactory extends Factory
 {
+    /**
+     * The current password being used by the factory.
+     */
     protected static ?string $password;
 
     /**
