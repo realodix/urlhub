4f19294e5a993a2b15199249ab6e6e12c2fc4d4c
Kei <budi03@gmail.com>
2024-03-21 04:06:17 +0700

    Delete AppServiceProvider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-    App\Providers\AppServiceProvider::class,
     App\Providers\AuthServiceProvider::class,
     App\Providers\HelperServiceProvider::class,
     App\Providers\FortifyServiceProvider::class,
