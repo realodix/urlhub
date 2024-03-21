c04d7fdba7c66f3538828a98e54f18747780f459
Kei <budi03@gmail.com>
2024-03-22 03:38:06 +0700

    Update providers.php

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+    App\Providers\AppServiceProvider::class,
     App\Providers\AuthServiceProvider::class,
     App\Providers\HelperServiceProvider::class,
     App\Providers\FortifyServiceProvider::class,
