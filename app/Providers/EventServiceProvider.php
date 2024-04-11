27dc1f76bd4e7b5153fdd04efdbbc8739a2d77fd
Bryan Cardiet <78115531+BryanCardiet@users.noreply.github.com>
2024-04-11 13:00:30 -0500

    Update EventServiceProvider.php

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -15,7 +15,7 @@ class EventServiceProvider extends ServiceProvider
      */
     protected $listen = [
         Registered::class => [
-            SendEmailVerificationNotification::class,
+            SendEmailVerificationNotification::class
         ],
     ];
 
