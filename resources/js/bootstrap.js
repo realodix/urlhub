dba97f9bded3bc728748663010764983fd1f8181
Kei <budi03@gmail.com>
2024-04-12 14:31:29 +0700

    remove unneeded packages

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,18 +1,3 @@
-import _ from 'lodash';
-import axios from 'axios';
-
-
-/**
- * We'll load the axios HTTP library which allows us to easily issue requests
- * to our Laravel back-end. This library automatically handles sending the
- * CSRF token as a header based on the value of the "XSRF" token cookie.
- */
-
-window.axios = axios;
-
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
-
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
