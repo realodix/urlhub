30ddfbf641e69bac8f1d92407a01c6446848bf51
Kei <budi03@gmail.com>
2024-04-25 04:30:30 +0700

    About Page: Use HTML DL instead of table
    https://developer.mozilla.org/en-US/docs/Web/HTML/Element/dl

--- resources/views/backend/about.blade.php
+++ resources/views/backend/about.blade.php
@@ -2,7 +2,7 @@
 
 @section('title', __('About System'))
 @section('content')
-    <main>
+    <main class="page_about">
         <div class="text-3xl md:text-4xl text-center">
             <span class="text-[#ff2d20]">@svg('icon-brand-laravel') {{app()->version()}}</span> -
             <span class="text-[#4f5b93]">@svg('icon-brand-php') {{phpversion()}}</span>
@@ -12,58 +12,42 @@
 
         <div class="common-card-style">
             <div class="card_header__sub_header">Links</div>
-            <table>
-                <tbody>
-                    @php
-                        $tUrl = numberAbbreviate($url->count());
-                        $tUrlVisit = numberAbbreviate($visit->count());
-                        $nUrlFromUser = numberAbbreviate($url->where('user_id', '!=' , null)->count());
-                        $nUrlVisitFromUser = numberAbbreviate($visit->count() - $url->numberOfClickFromGuest());
-                        $nUrlFromGuest = numberAbbreviate($url->numberOfUrlFromGuests());
-                        $nUrlVisitFromGuest = numberAbbreviate($url->numberOfClickFromGuest());
-                    @endphp
-                    <tr>
-                        <td class="w-72">Total</td>
-                        <td>{{$tUrl}} ({{$tUrlVisit}} visits)</td>
-                    </tr>
-                    <tr>
-                        <td>From Users</td>
-                        <td>{{$nUrlFromUser}} ({{$nUrlVisitFromUser}} visits)</td>
-                    </tr>
-                    <tr>
-                        <td>From Unregistered Users</td>
-                        <td>{{$nUrlFromGuest}} ({{$nUrlVisitFromGuest}} visits)</td>
-                    </tr>
-                </tbody>
-            </table>
+            <dl>
+                @php
+                    $tUrl = numberAbbreviate($url->count());
+                    $tUrlVisit = numberAbbreviate($visit->count());
+                    $nUrlFromUser = numberAbbreviate($url->where('user_id', '!=' , null)->count());
+                    $nUrlVisitFromUser = numberAbbreviate($visit->count() - $url->numberOfClickFromGuest());
+                    $nUrlFromGuest = numberAbbreviate($url->numberOfUrlFromGuests());
+                    $nUrlVisitFromGuest = numberAbbreviate($url->numberOfClickFromGuest());
+                @endphp
+                <dt>Total</dt>
+                <dd>{{$tUrl}} ({{$tUrlVisit}} visits)</dd>
+
+                <dt>From Users</dt>
+                <dd>{{$nUrlFromUser}} ({{$nUrlVisitFromUser}} visits)</dd>
+
+                <dt>From Unregistered Users</dt>
+                <dd>{{$nUrlFromGuest}} ({{$nUrlVisitFromGuest}} visits)</dd>
+            </dl>
 
             <div class="card_header__sub_header">Users</div>
-            <table>
-                <tbody>
-                    <tr>
-                        <td class="w-72">Registered</td>
-                        <td>{{numberAbbreviate($user->count())}}</td>
-                    </tr>
-                    <tr>
-                        <td>Unregistered</td>
-                        <td>{{numberAbbreviate($user->totalGuestUsers())}}</td>
-                    </tr>
-                </tbody>
-            </table>
+            <dl>
+                <dt>Registered</dt>
+                <dd>{{numberAbbreviate($user->count())}}</dd>
+
+                <dt>Unregistered</dt>
+                <dd>{{numberAbbreviate($user->totalGuestUsers())}}</dd>
+            </dl>
 
             <div class="card_header__sub_header">Random String</div>
-            <table>
-                <tbody>
-                    <tr>
-                        <td class="w-72">Possible Output</td>
-                        <td>( 62<sup>{{config('urlhub.keyword_length')}}</sup> ) {{number_format($keyGenerator->possibleOutput())}}</td>
-                    </tr>
-                    <tr>
-                        <td>Generated</td>
-                        <td>{{number_format($keyGenerator->totalKey())}}</td>
-                    </tr>
-                </tbody>
-            </table>
+            <dl>
+                <dt>Possible Output</dt>
+                <dd>( 62<sup>{{config('urlhub.keyword_length')}}</sup> ) {{number_format($keyGenerator->possibleOutput())}}</dd>
+
+                <dt>Generated</dt>
+                <dd>{{number_format($keyGenerator->totalKey())}}</dd>
+            </dl>
         </div>
 
         <br>
@@ -72,88 +56,64 @@
             <div class="card_header">{{__('Configuration')}}</div>
 
             <div class="card_header__sub_header">Shortened Links</div>
-            <table>
-                <tbody>
-                    @php
-                        $hashLength = config('urlhub.keyword_length');
-                        $customKeywordMinLength = config('urlhub.custom_keyword_min_length');
-                        $customKeywordMaxLength = config('urlhub.custom_keyword_max_length');
-                        $redirectCacheMaxAge = config('urlhub.redirect_cache_max_age');
-                    @endphp
-                    <tr>
-                        <td class="w-72"><code>keyword_length</code></td>
-                        <td>{{$hashLength.' '.str('character')->plural($hashLength)}}</td>
-                    </tr>
-                    <tr>
-                        <td class="w-72"><code>custom_keyword_min_length</code></td>
-                        <td>{{$customKeywordMinLength.' '.str('character')->plural($customKeywordMinLength)}}</td>
-                    </tr>
-                    <tr>
-                        <td class="w-72"><code>custom_keyword_max_length</code></td>
-                        <td>{{$customKeywordMaxLength.' '.str('character')->plural($customKeywordMaxLength)}}</td>
-                    </tr>
-                    <tr>
-                        <td><code>web_title</code></td>
-                        <td>{{var_export(config('urlhub.web_title'))}}</td>
-                    </tr>
-                    <tr>
-                        <td><code>redirect_status_code</code></td>
-                        <td>{{config('urlhub.redirect_status_code')}}</td>
-                    </tr>
-                    <tr>
-                        <td><code>redirect_cache_max_age</code></td>
-                        <td>{{$redirectCacheMaxAge.' '.str('second')->plural($redirectCacheMaxAge)}}</td>
-                    </tr>
-                    <tr>
-                        <td><code>track_bot_visits</code></td>
-                        <td>{{var_export(config('urlhub.track_bot_visits'))}}</td>
-                    </tr>
-                </tbody>
-            </table>
+            <dl>
+                @php
+                    $hashLength = config('urlhub.keyword_length');
+                    $customKeywordMinLength = config('urlhub.custom_keyword_min_length');
+                    $customKeywordMaxLength = config('urlhub.custom_keyword_max_length');
+                    $redirectCacheMaxAge = config('urlhub.redirect_cache_max_age');
+                @endphp
+                <dt><code>keyword_length</code></dt>
+                <dd>{{$hashLength.' '.str('character')->plural($hashLength)}}</dd>
+
+                <dt><code>custom_keyword_min_length</code></dt>
+                <dd>{{$customKeywordMinLength.' '.str('character')->plural($customKeywordMinLength)}}</dd>
+
+                <dt><code>custom_keyword_max_length</code></dt>
+                <dd>{{$customKeywordMaxLength.' '.str('character')->plural($customKeywordMaxLength)}}</dd>
+
+                <dt><code>web_title</code></dt>
+                <dd>{{var_export(config('urlhub.web_title'))}}</dd>
+
+                <dt><code>redirect_status_code</code></dt>
+                <dd>{{config('urlhub.redirect_status_code')}}</dd>
+
+                <dt><code>redirect_cache_max_age</code></dt>
+                <dd>{{$redirectCacheMaxAge.' '.str('second')->plural($redirectCacheMaxAge)}}</dd>
+
+                <dt><code>track_bot_visits</code></dt>
+                <dd>{{var_export(config('urlhub.track_bot_visits'))}}</dd>
+            </dl>
 
             <div class="card_header__sub_header">Guest / Unregistered Users</div>
-            <table>
-                <tbody>
-                    <tr>
-                        <td class="w-72">Create short links</td>
-                        <td>{{var_export(config('urlhub.public_site'))}}</td>
-                    </tr>
-                    <tr>
-                        <td>Sign up</td>
-                        <td>{{var_export(config('urlhub.registration'))}}</td>
-                    </tr>
-                </tbody>
-            </table>
+            <dl>
+                <dt>Create short links</dt>
+                <dd>{{var_export(config('urlhub.public_site'))}}</dd>
+
+                <dt>Sign up</dt>
+                <dd>{{var_export(config('urlhub.registration'))}}</dd>
+            </dl>
 
             <div class="card_header__sub_header">QRCode</div>
-            <table>
-                <tbody>
-                    <tr>
-                        <td class="w-72">Enable</td>
-                        <td>{{var_export(config('urlhub.qrcode'))}}</td>
-                    </tr>
-                    <tr>
-                        <td>Size</td>
-                        <td>{{config('urlhub.qrcode_size')}} px</td>
-                    </tr>
-                    <tr>
-                        <td>Margin</td>
-                        <td>{{config('urlhub.qrcode_margin')}} px</td>
-                    </tr>
-                    <tr>
-                        <td>Format</td>
-                        <td>{{config('urlhub.qrcode_format')}}</td>
-                    </tr>
-                    <tr>
-                        <td>Error correction levels</td>
-                        <td>{{config('urlhub.qrcode_error_correction')}}</td>
-                    </tr>
-                    <tr>
-                        <td>Round block</td>
-                        <td>{{var_export(config('urlhub.qrcode_round_block_size'))}}</td>
-                    </tr>
-                </tbody>
-            </table>
+            <dl>
+                <dt>Enabled</dt>
+                <dd>{{var_export(config('urlhub.qrcode'))}}</dd>
+
+                <dt>Size</dt>
+                <dd>{{config('urlhub.qrcode_size')}} px</dd>
+
+                <dt>Margin</dt>
+                <dd>{{config('urlhub.qrcode_margin')}} px</dd>
+
+                <dt>Format</dt>
+                <dd>{{config('urlhub.qrcode_format')}}</dd>
+
+                <dt>Error correction levels</dt>
+                <dd>{{config('urlhub.qrcode_error_correction')}}</dd>
+
+                <dt>Round block</dt>
+                <dd>{{var_export(config('urlhub.qrcode_round_block_size'))}}</dd>
+            </dl>
         </div>
     </main>
 @endsection
