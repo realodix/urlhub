5c81ebb35cdf545c05f5c19eaf86be95baae09ee
Kei <budi03@gmail.com>
2024-05-03 03:58:56 +0700

    Create AlphaNumHyphen rule

---
+++ app/Rules/AlphaNumHyphen.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+
+/**
+ * The field under validation may have alpha-numeric characters, as well as
+ * hyphen.
+ */
+class AlphaNumHyphen implements ValidationRule
+{
+    /**
+     * Run the validation rule.
+     *
+     * @param \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString $fail
+     */
+    public function validate(string $attribute, mixed $value, \Closure $fail): void
+    {
+        $rule = preg_match('/^[\pL\pM\pN-]+$/u', $value);
+
+        if ($rule === false || $rule === 0) {
+            $fail('The :attribute may only contain letters, numbers and hyphens.');
+        }
+    }
+}
