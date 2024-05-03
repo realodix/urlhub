0f2cc69a7d45fb6fd567eab9043e4b22f8155a48
Kei <budi03@gmail.com>
2024-05-03 20:49:00 +0700

    Move to ShortenUrl folder

--- tests/Feature/FrontPage/ShortenUrl/ValidationTest.php
+++ tests/Feature/FrontPage/ShortenUrl/ValidationTest.php
@@ -1,12 +1,13 @@
 <?php
 
-namespace Tests\Feature\FrontPage;
+namespace Tests\Feature\FrontPage\ShortenUrl;
 
 use App\Livewire\Validation\ValidateCustomKeyword;
 use Livewire\Livewire;
+use PHPUnit\Framework\Attributes\TestWith;
 use Tests\TestCase;
 
-class ShorteningUrlValidationTest extends TestCase
+class ValidationTest extends TestCase
 {
     public function testShortUrlGenerationWithIncorrectUrlFormat(): void
     {
@@ -22,23 +23,33 @@ class ShorteningUrlValidationTest extends TestCase
     /**
      * app\Livewire\Validation\ValidateCustomKeyword.php
      */
-    public function testCustomKeyValidation(): void
+    #[TestWith(['foobar'])]
+    #[TestWith(['foo-bar'])]
+    public function testCustomKeyValidationShouldPass($value): void
     {
-        $component = Livewire::test(ValidateCustomKeyword::class);
+        $response = $this->post(route('su_create'), [
+            'long_url' => 'https://laravel.com/',
+            'custom_key' => $value,
+        ]);
 
-        $component->assertStatus(200)
-            ->set('keyword', 'foobar')
-            ->assertHasNoErrors('keyword')
-            ->set('keyword', '123456')
-            ->assertHasNoErrors('keyword')
-            ->set('keyword', 'foo-b4r')
-            ->assertHasNoErrors('keyword');
+        $response
+            ->assertSessionHasNoErrors('custom_key');
+    }
 
-        $component
-            ->set('keyword', 'FOOBAR')
-            ->assertHasErrors('keyword')
-            ->set('keyword', 'admin') // Dashboard route
-            ->assertHasErrors('keyword');
+    /**
+     * app\Livewire\Validation\ValidateCustomKeyword.php
+     */
+    #[TestWith(['fooBar'])]
+    #[TestWith(['foo_bar'])]
+    public function testCustomKeyValidationShouldFail($value): void
+    {
+        $response = $this->post(route('su_create'), [
+            'long_url' => 'https://laravel.com/',
+            'custom_key' => $value,
+        ]);
+
+        $response
+            ->assertSessionHasErrors('custom_key');
     }
 
     /**
@@ -60,30 +71,6 @@ class ShorteningUrlValidationTest extends TestCase
             ->set('keyword', 'FOOBAR')
             ->assertHasErrors('keyword')
             ->set('keyword', 'admin') // Dashboard route
-            ->assertHasErrors('keyword');
-    }
-
-    /**
-     * app\Livewire\Validation\ValidateCustomKeyword.php
-     */
-    public function testCustomKeywordLengthValidation(): void
-    {
-        $component = Livewire::test(ValidateCustomKeyword::class);
-
-        $minLen = 3;
-        $maxLen = 7;
-
-        config(['urlhub.custom_keyword_min_length' => $minLen]);
-        config(['urlhub.custom_keyword_max_length' => $maxLen]);
-
-        $component->assertStatus(200);
-        $component->set('keyword', str_repeat('a', $minLen))
-            ->assertHasNoErrors('keyword')
-            ->set('keyword', str_repeat('a', $maxLen))
-            ->assertHasNoErrors('keyword');
-        $component->set('keyword', str_repeat('a', $minLen - 1))
-            ->assertHasErrors('keyword')
-            ->set('keyword', str_repeat('a', $maxLen + 1))
             ->assertHasErrors('keyword');
     }
 
