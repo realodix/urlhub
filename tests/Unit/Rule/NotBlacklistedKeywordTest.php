d75aeebbc24af1e3d7aa670fead50d460ea00748
Kei <budi03@gmail.com>
2024-05-05 02:05:39 +0700

    NotBlacklistedKeyword

--- tests/Unit/Rule/NotBlacklistedKeywordTest.php
+++ tests/Unit/Rule/NotBlacklistedKeywordTest.php
@@ -5,11 +5,10 @@ namespace Tests\Unit\Rule;
 use App\Rules\NotBlacklistedKeyword;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Group;
-use PHPUnit\Framework\Attributes\Test;
 use Tests\Support\Helper;
 use Tests\TestCase;
 
-class UrlTest extends TestCase
+class NotBlacklistedKeywordTest extends TestCase
 {
     protected function setUp(): void
     {
@@ -18,46 +17,8 @@ class UrlTest extends TestCase
         config(['urlhub.domain_blacklist' => ['github.com', 't.co']]);
     }
 
-    /**
-     * @param string $value
-     */
-    #[Test]
-    #[Group('f-user')]
-    #[DataProvider('customKeywordBlacklistPassDataProvider')]
-    public function customKeywordBlacklistPass($value): void
-    {
-        $val = Helper::validator(['foo' => $value], ['foo' => new NotBlacklistedKeyword]);
-
-        $this->assertTrue($val->passes());
-        $this->assertSame([], $val->messages()->messages());
-    }
-
-    /**
-     * @param array $value
-     */
-    #[Test]
-    #[Group('f-user')]
-    #[DataProvider('customKeywordContainsRegisteredRouteWillFailDataProvider')]
-    public function customKeywordContainsRegisteredRouteWillFail($value): void
-    {
-        $val = Helper::validator(['foo' => $value], ['foo' => new NotBlacklistedKeyword]);
-
-        $this->assertTrue($val->fails());
-        $this->assertSame(['foo' => ['Not available.']], $val->messages()->messages());
-    }
-
-    public function customKeywordContainsReservedKeywordWillFail(): void
-    {
-        $value = 'css';
-        config(['urlhub.reserved_keyword' => $value]);
-
-        $val = Helper::validator(['foo' => $value], ['foo' => new NotBlacklistedKeyword]);
-
-        $this->assertTrue($val->fails());
-        $this->assertSame(['foo' => ['Not available.']], $val->messages()->messages());
-    }
-
-    public static function customKeywordBlacklistPassDataProvider(): array
+    #[Group('u-rule')]
+    public static function customKeywordIsNotBlacklistedDataProvider(): array
     {
         return [
             ['hello'],
@@ -65,11 +26,54 @@ class UrlTest extends TestCase
         ];
     }
 
-    public static function customKeywordContainsRegisteredRouteWillFailDataProvider(): array
+    /**
+     * @param string $value
+     */
+    #[Group('u-rule')]
+    #[DataProvider('customKeywordIsNotBlacklistedDataProvider')]
+    public function testCutomKeywordIsNotBlacklisted($value): void
+    {
+        $val = Helper::validator(['foo' => $value], ['foo' => new NotBlacklistedKeyword]);
+
+        $this->assertTrue($val->passes());
+        $this->assertSame([], $val->messages()->messages());
+    }
+
+    public static function registeredRouteDataProvider(): array
     {
         return [
             ['login'],
             ['register'],
         ];
+    }
+
+    /**
+     * When custom keyword is registered route, it should not be available.
+     *
+     * @param array $value
+     */
+    #[Group('u-rule')]
+    #[DataProvider('registeredRouteDataProvider')]
+    public function testCustomKeywordIsRegisteredRoute($value): void
+    {
+        $val = Helper::validator(['foo' => $value], ['foo' => new NotBlacklistedKeyword]);
+
+        $this->assertTrue($val->fails());
+        $this->assertSame(['foo' => ['Not available.']], $val->messages()->messages());
+    }
+
+    /**
+     * When custom keyword is reserved keyword, it should not be available.
+     */
+    #[Group('u-rule')]
+    public function testCustomKeywordIsReservedKeyword(): void
+    {
+        $value = 'css';
+        config(['urlhub.reserved_keyword' => [$value]]);
+
+        $val = Helper::validator(['foo' => $value], ['foo' => new NotBlacklistedKeyword]);
+
+        $this->assertTrue($val->fails());
+        $this->assertSame(['foo' => ['Not available.']], $val->messages()->messages());
     }
 }
