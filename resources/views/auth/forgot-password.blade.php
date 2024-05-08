c3cbb19a404a588490a696b48ff714b47e3b360d
Kei <budi03@gmail.com>
2024-05-08 18:58:02 +0700

    Update forgot and reset password page (#980)

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,47 +1,48 @@
 @extends('layouts.auth')
 
-@section('title', __('Login'))
+@section('title', __('Forgot Password'))
 
 @section('css_class', 'auth')
 
 @section('content')
-    <div class="flex flex-col min-h-screen sm:justify-center items-center pt-6 sm:pt-0">
-        <div class="text-center mb-4">
-            {{ __('Forgot your password? No problem.') }} <br>
-            {{ __('Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+<div class="flex flex-col min-h-screen sm:justify-center items-center pt-6 sm:pt-0">
+    @if (session('status'))
+        <div>
+            {{ session('status') }}
         </div>
+    @endif
 
-        @if (session('status'))
-            <div>
-                {{ session('status') }}
-            </div>
-        @endif
+    @if ($errors->any())
+        <div>
+            <div>{{ __('Whoops! Something went wrong.') }}</div>
 
-        @if ($errors->any())
-            <div>
-                <div>{{ __('Whoops! Something went wrong.') }}</div>
-
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
-        <div class="auth-card">
-            <form method="POST" action="{{ route('password.email') }}">
-            @csrf
-                <div>
-                    <label class="form-label">{{ __('Email') }}</label>
-                    <input type="email" name="email" class="form-input" id="email" value="{{ old('email') }}" required autofocus>
-                </div>
-
-                <div class="flex items-center justify-center mt-4">
-                    <button type="submit" class="btn btn-primary ml-4">
-                        {{ __('Email Password Reset Link') }}
-                    </button>
-                </div>
-            </form>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
         </div>
+    @endif
+
+    <div class="auth-card">
+        <p>{{ __('Forgot your password? No problem.') }}</p> <br>
+        <p>{{ __('Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}</p>
+
+        <br>
+
+        <form method="POST" action="{{ route('password.email') }}">
+        @csrf
+            <div>
+                <label class="form-label">{{ __('Email') }}</label>
+                <input type="email" name="email" class="form-input" id="email" value="{{ old('email') }}" required autofocus>
+            </div>
+
+            <div class="flex items-center justify-center mt-8">
+                <button type="submit" class="btn btn-primary ml-4">
+                    {{ __('Email Password Reset Link') }}
+                </button>
+            </div>
+        </form>
     </div>
+</div>
 @endsection
