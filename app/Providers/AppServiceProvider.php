63edbf8d4bf213a3ff3db343ebac3f6204f8b11e
Kei <budi03@gmail.com>
2024-05-25 01:57:13 +0700

    Only alphanumeric characters are counted as generated random string (#999)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // A SQLite UDF for the REGEXP keyword that mimics the behavior in MySQL.
+        if (DB::connection() instanceof \Illuminate\Database\SQLiteConnection) {
+            DB::connection()->getPdo()->sqliteCreateFunction('REGEXP', function (string $pattern, string $value) {
+                mb_regex_encoding('UTF-8');
+
+                return mb_ereg($pattern, $value) !== false ? 1 : 0;
+            });
+        }
     }
 }
