473daf879fbf2bffb17aacb9ed7a160166aa3a45
luanfreitasdev <luanfreitas10@protonmail.com>
2024-05-26 16:16:49 -0300

    fix UrlListOfUsersTable

--- app/Livewire/Table/UrlListOfUsersTable.php
+++ app/Livewire/Table/UrlListOfUsersTable.php
@@ -15,81 +15,13 @@ use PowerComponents\LivewirePowerGrid\PowerGridFields;
 /**
  * @codeCoverageIgnore
  */
-final class UrlListOfUsersTable extends PowerGridComponent
+final class UrlListOfUsersTable extends BaseUrlTable
 {
     public int $user_id;
 
-    public int $perPage = 25;
-
-    public bool $showUpdateMessages = true;
-
-    public string $sortDirection = 'desc';
-
-    public function setUp(): array
+    public function getUserIdBuilder(Builder $query): Builder
     {
-        return [
-            Header::make()
-                ->showToggleColumns()
-                ->showSearchInput(),
-            Footer::make()
-                ->showPerPage($this->perPage)
-                ->showRecordCount('full'),
-        ];
-    }
-
-    public function datasource(): Builder
-    {
-        return Url::query()
-            ->join('users', 'urls.user_id', '=', 'users.id')
-            ->leftJoinSub(DB::table('visits')
-                ->select('url_id', DB::raw('COUNT(id) as visits_count'))
-                ->groupBy('url_id'), 'visits_counts', function ($join) {
-                $join->on('urls.id', '=', 'visits_counts.url_id');
-            })
-            ->leftJoinSub(DB::table('visits')
-                ->select('url_id', DB::raw('SUM(CASE WHEN is_first_click = 1 THEN 1 ELSE 0 END) as unique_click_count'))
-                ->groupBy('url_id'), 'unique_click_counts', function ($join) {
-                $join->on('urls.id', '=', 'unique_click_counts.url_id');
-            })
-            ->where('urls.user_id', '=', $this->user_id)
-            ->select(
-                'urls.id as id',
-                'users.name as author',
-                'urls.title',
-                'urls.keyword',
-                'urls.destination',
-                'urls.created_at',
-                DB::raw('COALESCE(visits_counts.visits_count, 0) as visits_count'),
-                DB::raw('COALESCE(unique_click_counts.unique_click_count, 0) as unique_click_count')
-            );
-    }
-
-    public function fields(): PowerGridFields
-    {
-        return PowerGrid::fields()
-            ->add('keyword', function (Url $url) {
-                return view('components.table.keyword', ['shortUrl' => $url->short_url, 'keyword' => $url->keyword])
-                    ->render();
-            })
-            ->add('destination', function (Url $url) {
-                return view('components.table.destination', [
-                    'title' => $url->title,
-                    'destination' => $url->destination,
-                    'limit' => MyUrlTable::STR_LIMIT,
-                ])->render();
-            })
-            ->add('t_clicks', function (Url $url) {
-                return view('components.table.visit', ['clicks' => $url->visits_count, 'uniqueClicks' => $url->unique_click_count])
-                    ->render();
-            })
-            ->add('created_at_formatted', function (Url $url) {
-                return view('components.table.date-created', ['createdAt' => $url->created_at])
-                    ->render();
-            })
-            ->add('action', function (Url $url) {
-                return view('components.table.action-button', ['url' => $url])
-                    ->render();
-            });
+        return $query->where('urls.user_id', '=', $this->user_id);
     }
 
     /**
