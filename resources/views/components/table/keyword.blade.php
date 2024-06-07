c3fabbe8647d5205ce50c8877b752acb1c800f55
Kei <budi03@gmail.com>
2024-06-07 14:18:25 +0700

    Use `str()` instead `Str::`

--- resources/views/components/table/keyword.blade.php
+++ resources/views/components/table/keyword.blade.php
@@ -1,7 +1,5 @@
-@use('Illuminate\Support\Str')
-
 <div>
     <a href="{{ $shortLink }}" title="{{ $keyword }}" target="_blank" class="font-light text-sky-800">
-        {{ Str::limit($keyword, 12) }}
+        {{ str()->limit($keyword, 12) }}
     </a>
 </div>
