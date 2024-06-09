89c8c76bc3706f41c014f41ba3e5f7f4d2bb6ef7
Kei <budi03@gmail.com>
2024-06-10 01:25:19 +0700

    resources\views\auth\

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,15 +6,15 @@
 
 @section('content')
     <div class="flex flex-col min-h-screen sm:justify-center items-center pt-6 sm:pt-0">
-        <div class="text-uh-blue font-bold text-4xl sm:text-6xl">{{config('app.name')}}</div>
+        <div class="text-uh-blue font-bold text-4xl sm:text-6xl">{{ config('app.name') }}</div>
 
         <div class="auth-card">
             @if ( ! Config::get('urlhub.registration') )
-                <p class="text-muted">{{__('Sorry, not allowed to register by administrator')}}</p>
+                <p class="text-muted">{{ __('Sorry, not allowed to register by administrator') }}</p>
             @else
-                <form method="post" action="{{ route('register') }}" aria-label="{{__('Register')}}">
+                <form method="post" action="{{ route('register') }}" aria-label="{{ __('Register') }}">
                 @csrf
-                    <label class="form-label">{{__('Username')}}</label>
+                    <label class="form-label">{{ __('Username') }}</label>
                     <input name="name" class="form-input mt-1" id="name" required autofocus>
 
                     @if ($errors->has('name'))
@@ -23,7 +23,7 @@
 
                     <div class="mt-4"></div>
 
-                    <label class="form-label">{{__('Email')}}</label>
+                    <label class="form-label">{{ __('Email') }}</label>
                     <input type="email" name="email" class="form-input mt-1" id="email" required>
 
                     @if ($errors->has('email'))
@@ -32,7 +32,7 @@
 
                     <div class="mt-4"></div>
 
-                    <label class="form-label">{{__('Password')}}</label>
+                    <label class="form-label">{{ __('Password') }}</label>
                     <input type="password" name="password" class="form-input mt-1" id="password" required>
 
                     @if ($errors->has('password'))
@@ -41,11 +41,11 @@
 
                     <div class="mt-4"></div>
 
-                    <label class="form-label">{{__('Password Confirmation')}}</label>
+                    <label class="form-label">{{ __('Password Confirmation') }}</label>
                     <input type="password" name="password_confirmation" class="form-input mt-1" id="password-confirm" required>
 
                     <div class="flex items-center justify-end mt-8">
-                        <button type="submit" class="btn btn-primary">{{__('Create Account')}}</button>
+                        <button type="submit" class="btn btn-primary">{{ __('Create Account') }}</button>
                     </div>
                 </form>
             @endif
