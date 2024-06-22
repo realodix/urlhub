f89347b913279ba196fb26d5d2bc3422503a097e
Kei <budi03@gmail.com>
2024-06-23 01:08:36 +0700

    formatting

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -2,52 +2,52 @@
 
 @section('title', __('Dashboard'))
 @section('content')
-    <main>
-        <div class="grid gird-cols-1 md:grid-cols-4 gap-7 mb-8">
-            <div class="bg-uh-bg-1 p-4 sm:rounded-lg
-                border-y border-uh-border-color sm:border-none sm:shadow-md"
-            >
-                <div class="flex flex-row space-x-4 items-center">
-                    @svg('icon-link', 'mr-1.5 text-emerald-600 text-3xl')
-                    <div>
-                        <p class="text-gray-400 text-sm font-medium uppercase leading-4">Total Links</p>
-                        <p class="text-2xl font-bold text-gray-900 inline-flex items-center space-x-2">
-                            {{ n_abb($url->currentUserUrlCount(auth()->id())) }}
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="bg-uh-bg-1 p-4 sm:rounded-lg
-                border-y border-uh-border-color sm:border-none sm:shadow-md"
-            >
-                <div class="flex flex-row space-x-4 items-center">
-                    @svg('icon-chart-line-alt', 'mr-1.5 text-amber-600 text-3xl')
-                    <div>
-                        <p class="text-gray-400 text-sm font-medium uppercase leading-4">Total Clicks</p>
-                        <p class="text-2xl font-bold text-gray-900 inline-flex items-center space-x-2">
-                            {{ $urlVisitCount }}
-                        </p>
-                    </div>
+<main>
+    <div class="grid gird-cols-1 md:grid-cols-4 gap-7 mb-8">
+        <div class="bg-uh-bg-1 p-4 sm:rounded-lg
+            border-y border-uh-border-color sm:border-none sm:shadow-md"
+        >
+            <div class="flex flex-row space-x-4 items-center">
+                @svg('icon-link', 'mr-1.5 text-emerald-600 text-3xl')
+                <div>
+                    <p class="text-gray-400 text-sm font-medium uppercase leading-4">Total Links</p>
+                    <p class="text-2xl font-bold text-gray-900 inline-flex items-center space-x-2">
+                        {{ n_abb($url->currentUserUrlCount(auth()->id())) }}
+                    </p>
                 </div>
             </div>
         </div>
-
-        <div class="common-card-style">
-            <div class="card_header__v2">
-                <div class="w-1/2">
-                    <span class="text-2xl text-uh-1">{{ __('My URLs') }}</span>
-                </div>
-                <div class="w-1/2 text-right">
-                    <a href="{{ url('./') }}" target="_blank" title="{{ __('Add URL') }}" class="btn btn-primary">
-                        @svg('icon-add-link', '!h-[1.5em] mr-1')
-                        {{ __('Add URL') }}
-                    </a>
+        <div class="bg-uh-bg-1 p-4 sm:rounded-lg
+            border-y border-uh-border-color sm:border-none sm:shadow-md"
+        >
+            <div class="flex flex-row space-x-4 items-center">
+                @svg('icon-chart-line-alt', 'mr-1.5 text-amber-600 text-3xl')
+                <div>
+                    <p class="text-gray-400 text-sm font-medium uppercase leading-4">Total Clicks</p>
+                    <p class="text-2xl font-bold text-gray-900 inline-flex items-center space-x-2">
+                        {{ $urlVisitCount }}
+                    </p>
                 </div>
             </div>
-
-            @include('partials/messages')
-
-            @livewire('table.my-url-table')
         </div>
-    </main>
+    </div>
+
+    <div class="common-card-style">
+        <div class="card_header__v2">
+            <div class="w-1/2">
+                <span class="text-2xl text-uh-1">{{ __('My URLs') }}</span>
+            </div>
+            <div class="w-1/2 text-right">
+                <a href="{{ url('./') }}" target="_blank" title="{{ __('Add URL') }}" class="btn btn-primary">
+                    @svg('icon-add-link', '!h-[1.5em] mr-1')
+                    {{ __('Add URL') }}
+                </a>
+            </div>
+        </div>
+
+        @include('partials/messages')
+
+        @livewire('table.my-url-table')
+    </div>
+</main>
 @endsection
--- resources/views/backend/edit.blade.php
+++ resources/views/backend/edit.blade.php
@@ -3,60 +3,60 @@
 @section('title', __('Edit Link').' "'.$url->keyword.'"'.' ‹ '.str()->title(auth()->user()->name))
 
 @section('content')
-    @include('partials/messages')
+@include('partials/messages')
 
-    <main class="flex flex-wrap">
-        <div class="md:w-3/12 flex justify-between">
-            <div class="px-4 sm:px-0">
-                <h3 class="text-lg font-medium text-slate-900">{{ __('Edit URL Details') }}</h3>
-                <br>
-                <div class="inline sm:block mr-2 text-sm text-slate-600">
-                    @svg('icon-person', 'mr-1')
-                    {{ $url->author->name }}
-                </div>
-                <div class="inline sm:block text-sm text-slate-600">
-                    @svg('icon-calendar', 'mr-1')
-                    <span title="{{ $url->created_at->toDayDateTimeString() }}">{{ $url->created_at->diffForHumans() }}</span>
-                </div>
-
-                @if ($url->created_at != $url->updated_at)
-                <div class="inline sm:block text-sm text-slate-600">
-                    @svg('icon-updated', 'mr-1 font-bold')
-                    <span title="{{ $url->updated_at->toDayDateTimeString() }}">{{ $url->updated_at->diffForHumans() }}</span>
-                </div>
-                @endif
+<main class="flex flex-wrap">
+    <div class="md:w-3/12 flex justify-between">
+        <div class="px-4 sm:px-0">
+            <h3 class="text-lg font-medium text-slate-900">{{ __('Edit URL Details') }}</h3>
+            <br>
+            <div class="inline sm:block mr-2 text-sm text-slate-600">
+                @svg('icon-person', 'mr-1')
+                {{ $url->author->name }}
             </div>
+            <div class="inline sm:block text-sm text-slate-600">
+                @svg('icon-calendar', 'mr-1')
+                <span title="{{ $url->created_at->toDayDateTimeString() }}">{{ $url->created_at->diffForHumans() }}</span>
+            </div>
+
+            @if ($url->created_at != $url->updated_at)
+            <div class="inline sm:block text-sm text-slate-600">
+                @svg('icon-updated', 'mr-1 font-bold')
+                <span title="{{ $url->updated_at->toDayDateTimeString() }}">{{ $url->updated_at->diffForHumans() }}</span>
+            </div>
+            @endif
         </div>
-        <div class="w-full md:w-8/12 lg:w-6/12 mt-5 md:mt-0 md:ml-4">
-            <form method="post" action="{{ route('dboard.url.edit.store', $url) }}">
-            @csrf
-                <div class="common-card-style">
-                    <div class="grid grid-cols-6 gap-6">
-                        <div class="col-span-6 lg:col-span-4">
-                            <label for="short-url" class="form-label">{{ __('Short URL') }}</label>
-                            <span class="short-url text-uh-blue">{{ urlFormat($url->short_url, scheme: false) }}</span>
-                        </div>
-
-                        <div class="col-span-6">
-                            <label class="form-label">{{ __('Title') }}</label>
-                            <input name="title" placeholder="{{ __('Title') }}" required
-                                value="{{ $url->title }}" class="form-input">
-                        </div>
-
-                        <div class="col-span-6">
-                            <label for="long-url" class="form-label">{{ __('Destination URL') }}</label>
-                            <input id="long-url" name="long_url" placeholder="http://www.my_long_url.com"
-                                required value="{{ $url->destination }}" class="form-input">
-                        </div>
+    </div>
+    <div class="w-full md:w-8/12 lg:w-6/12 mt-5 md:mt-0 md:ml-4">
+        <form method="post" action="{{ route('dboard.url.edit.store', $url) }}">
+        @csrf
+            <div class="common-card-style">
+                <div class="grid grid-cols-6 gap-6">
+                    <div class="col-span-6 lg:col-span-4">
+                        <label for="short-url" class="form-label">{{ __('Short URL') }}</label>
+                        <span class="short-url text-uh-blue">{{ urlFormat($url->short_url, scheme: false) }}</span>
                     </div>
 
-                    <div class="flex items-center justify-end mt-4 text-right">
-                        <button type="submit" class="btn btn-primary btn-sm">
-                            {{ __('Save Changes') }}
-                        </button>
+                    <div class="col-span-6">
+                        <label class="form-label">{{ __('Title') }}</label>
+                        <input name="title" placeholder="{{ __('Title') }}" required
+                            value="{{ $url->title }}" class="form-input">
+                    </div>
+
+                    <div class="col-span-6">
+                        <label for="long-url" class="form-label">{{ __('Destination URL') }}</label>
+                        <input id="long-url" name="long_url" placeholder="http://www.my_long_url.com"
+                            required value="{{ $url->destination }}" class="form-input">
                     </div>
                 </div>
-            </form>
-        </div>
-    </main>
+
+                <div class="flex items-center justify-end mt-4 text-right">
+                    <button type="submit" class="btn btn-primary btn-sm">
+                        {{ __('Save Changes') }}
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>
+</main>
 @endsection
