eea27578e698e2647ef5ae33cf8dff210b12aef8
Kei <budi03@gmail.com>
2024-07-01 08:18:13 +0700

    realodix/relax v1.12

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -2,12 +2,12 @@
 
 use Realodix\Relax\Config;
 use Realodix\Relax\Finder;
-use Realodix\Relax\RuleSet\Sets\Realodix;
 
 $localRules = [
     'single_import_per_statement' => false,
 ];
 
-return Config::create(new Realodix, $localRules)
+return Config::create('Realodix')
+    ->setRules($localRules)
     ->setFinder(Finder::laravel())
     ->setCacheFile(__DIR__.'/.tmp/.php-cs-fixer.cache');
