958329cffe48977b14b1160cfb4c38fde752feb6
Kei <budi03@gmail.com>
2024-12-15 12:13:29 +0700

    formatting

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -14,11 +14,8 @@
                                 <button class="navbar-toggler items-center">
                                     <div class="text-base font-semibold">{{ str()->title(auth()->user()->name) }}</div>
 
-                                    <svg class="ml-2 -mr-0.5 h-4 w-4"
-                                        xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                                        <path fill-rule="evenodd"
-                                            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                            clip-rule="evenodd"></path>
+                                    <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                                        <path fill-rule="evenodd" clip-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"></path>
                                     </svg>
                                 </button>
                             </span>
@@ -59,8 +56,7 @@
                                 @csrf
                                     <a href="{{ route('logout') }}"
                                         onclick="event.preventDefault(); this.closest('form').submit();"
-                                        class="nav-item"
-                                    >
+                                        class="nav-item">
                                         @svg('icon-log-out', 'mr-1') {{ __('Log Out') }}
                                     </a>
                                 </form>
