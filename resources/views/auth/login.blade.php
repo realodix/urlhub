6a9292f05548ac77b1c2886b53b384c3df0f1916
Kei <budi03@gmail.com>
2024-12-19 18:15:13 +0700

    wip

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,7 +7,7 @@
 @section('content')
 <div class="flex flex-col min-h-screen sm:justify-center items-center pt-6 sm:pt-0">
     <div class="logo text-4xl sm:text-6xl">
-        {{ config('app.name') }}
+        <a href="{{ url('/') }}">{{ config('app.name') }}</a>
     </div>
 
     @if(session()->has('login_error'))
