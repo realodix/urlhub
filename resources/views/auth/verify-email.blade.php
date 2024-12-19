8823ba51f3678dee6a6b686f1a3d09924d841518
Kei <budi03@gmail.com>
2024-12-20 00:58:31 +0700

    Improve the appearance of the verification email page (#1040)

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -5,29 +5,37 @@
 @section('css_class', 'auth')
 
 @section('content')
-<div>
-    {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
-</div>
+<div class="flex flex-col min-h-screen sm:justify-center items-center pt-6 sm:pt-0">
+    @if (session('status') == 'verification-link-sent')
+        <div class="alert alert-success !mb-10">
+            {{ __('A new verification link has been sent to the email address you provided during registration.') }}
+        </div>
+    @endif
 
-@if (session('status') == 'verification-link-sent')
-    <div>
-        {{ __('A new verification link has been sent to the email address you provided during registration.') }}
+    <div class="common-card-style mb-8">
+        <p class="font-bold">
+            {{ __('Thanks for signing up!') }}
+        </p>
+        <p class="text-gray-600">
+            {{ __('Before getting started, could you verify your email address by clicking on the link we just emailed to you?') }}
+        </p>
+        <p class="text-gray-600">
+            {{ __('If you didn\'t receive the email, we will gladly send you another.') }}
+        </p>
     </div>
-@endif
 
-<form method="POST" action="{{ route('verification.send') }}">
-@csrf
+    <form method="POST" action="{{ route('verification.send') }}">
+    @csrf
+        <button type="submit" class="btn btn-primary mt-4">
+            {{ __('Resend Verification Email') }}
+        </button>
+    </form>
 
-    <button type="submit">
-        {{ __('Resend Verification Email') }}
-    </button>
-</form>
-
-<form method="POST" action="{{ route('logout') }}">
-@csrf
-
-    <button type="submit">
-        {{ __('Logout') }}
-    </button>
-</form>
+    <form method="POST" action="{{ route('logout') }}">
+    @csrf
+        <button type="submit" class="btn btn-primary mt-4">
+            {{ __('Logout') }}
+        </button>
+    </form>
+</div>
 @endsection
