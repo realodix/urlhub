e4299506859397500e16e4fde0c709f3a64c4a40
Kei <budi03@gmail.com>
2024-12-20 00:41:34 +0700

    Improve the design of the alert flash  (#1037)

--- resources/views/partials/messages.blade.php
+++ resources/views/partials/messages.blade.php
@@ -1,18 +1,19 @@
 @if($errors->any())
-    <div class="block mb-4 pl-3 pr-4 py-2 font-medium text-base text-orange-700 bg-orange-50 border-l-4 border-orange-400" role="alert">
-        @foreach($errors->all() as $error)
-            {{ $error }} <br>
-        @endforeach
+    <div role="alert" class="alert alert-error">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
     </div>
+
 @elseif (session('flash_success'))
-    <div class="block mb-4 pl-3 pr-4 py-2 font-medium text-base text-emerald-700 bg-emerald-50 border-l-4 border-emerald-400" role="alert">
+    <div role="alert" class="alert alert-success">
         {{ session('flash_success') }}
     </div>
+
 @elseif (session('flash_error'))
-    <div class="block mb-4 pl-3 pr-4 py-2 font-medium text-base text-red-700 bg-red-50 border-l-4 border-red-400" role="alert">
+    <div role="alert" class="alert alert-error">
         {{ session('flash_error') }}
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-        </button>
     </div>
 @endif
