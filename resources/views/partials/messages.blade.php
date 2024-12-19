a27de65e926f8bbe634a5856a80d255a49d9f514
Kei <budi03@gmail.com>
2024-12-19 23:23:05 +0700

    Update messages.blade.php

--- resources/views/partials/messages.blade.php
+++ resources/views/partials/messages.blade.php
@@ -1,5 +1,5 @@
 @if($errors->any())
-    <div role="alert" class="alert alert-error" >
+    <div role="alert" class="alert alert-error">
         <ul>
             @foreach ($errors->all() as $error)
                 <li>{{ $error }}</li>
@@ -8,12 +8,12 @@
     </div>
 
 @elseif (session('flash_success'))
-    <div role="alert" class="alert alert-success" >
+    <div role="alert" class="alert alert-success">
         {{ session('flash_success') }}
     </div>
 
-{{-- @elseif (session('flash_error'))
-    <div class="block mb-4 pl-3 pr-4 py-2 font-medium text-base text-red-700 bg-red-50 border-l-4 border-red-400" role="alert">
+@elseif (session('flash_error'))
+    <div role="alert" class="alert alert-error">
         {{ session('flash_error') }}
-    </div> --}}
+    </div>
 @endif
