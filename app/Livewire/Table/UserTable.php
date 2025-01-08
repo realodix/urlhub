63ce181b08d1d83d5e2c7b1690d8e83c7fda5490
Kei <budi03@gmail.com>
2025-01-09 04:23:18 +0700

    Secondary button (#1050)

--- app/Livewire/Table/UserTable.php
+++ app/Livewire/Table/UserTable.php
@@ -58,12 +58,12 @@ final class UserTable extends PowerGridComponent
             ->add('action', function (User $user) {
                 return
                     '<a role="button" href="' . route('user.edit', $user) . '" title="' . __('Details') . '"
-                        class="btn btn-primary btn-xs"
+                        class="btn btn-secondary btn-square btn-xs"
                     >'
                         . Blade::render('@svg(\'icon-person-edit\')') .
                     '</a>
                     <a role="button" href="' . route('user.password.show', $user) . '" title="' . __('Change Password') . '"
-                        class="btn btn-primary btn-xs"
+                        class="btn btn-secondary btn-square btn-xs"
                     >'
                         . Blade::render('@svg(\'icon-key\')') .
                     '</a>';
