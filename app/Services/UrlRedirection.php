07bce302aafbc99f15f52158e47cc2bf8e99fc06
Kei <budi03@gmail.com>
2025-01-12 12:21:34 +0700

    Improved cache control header handling for zero max age
    - https://github.com/mdn/content/blob/8f4599ce0c63ac1c260933b1458e4da33ce972d5/files/en-us/web/http/headers/cache-control/index.md?plain=1#L418-L422
    - https://stackoverflow.com/questions/1046966/whats-the-difference-between-cache-control-max-age-0-and-no-cache

--- app/Services/UrlRedirection.php
+++ app/Services/UrlRedirection.php
@@ -16,7 +16,11 @@ class UrlRedirection
     {
         $statusCode = config('urlhub.redirect_status_code');
         $maxAge = config('urlhub.redirect_cache_max_age');
+
         $headers = ['Cache-Control' => sprintf('private,max-age=%s', $maxAge)];
+        if ($maxAge === 0) {
+            $headers = ['Cache-Control' => 'max-age=0, must-revalidate'];
+        }
 
         return redirect()->away($url->destination, $statusCode, $headers);
     }
--- tests/Unit/Controllers/UrlRedirectControllerTest.php
+++ tests/Unit/Controllers/UrlRedirectControllerTest.php
@@ -4,6 +4,7 @@ namespace Tests\Unit\Controllers;
 
 use App\Models\Url;
 use App\Models\Visit;
+use App\Services\UrlRedirection;
 use Tests\TestCase;
 
 #[\PHPUnit\Framework\Attributes\Group('controller')]
@@ -18,5 +19,34 @@ class UrlRedirectControllerTest extends TestCase
             ->assertStatus(config('urlhub.redirect_status_code'));
 
         $this->assertCount(1, Visit::all());
+    }
+
+    /**
+     * @see \App\Services\UrlRedirection
+     * @see \App\Http\Controllers\UrlRedirectController
+     */
+    public function testUrlRedirectionHeadersWithMaxAge()
+    {
+        config(['urlhub.redirect_cache_max_age' => 3600]);
+
+        $url = Url::factory()->create();
+        $response = app(UrlRedirection::class)->execute($url);
+
+        $this->assertStringContainsString('max-age=3600', $response->headers->get('Cache-Control'));
+    }
+
+    /**
+     * @see \App\Services\UrlRedirection
+     * @see \App\Http\Controllers\UrlRedirectController
+     */
+    public function testUrlRedirectionHeadersWithMaxAgeZero()
+    {
+        config(['urlhub.redirect_cache_max_age' => 0]);
+
+        $url = Url::factory()->create();
+        $response = app(UrlRedirection::class)->execute($url);
+
+        $this->assertStringContainsString('max-age=0', $response->headers->get('Cache-Control'));
+        $this->assertStringContainsString('must-revalidate', $response->headers->get('Cache-Control'));
     }
 }
