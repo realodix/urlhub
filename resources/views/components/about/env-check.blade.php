844102d433e41ae6debaaf2e9160d0f8e4454263
Kei <budi03@gmail.com>
2025-01-14 08:29:29 +0700

    Update env-check.blade.php

--- resources/views/components/about/env-check.blade.php
+++ resources/views/components/about/env-check.blade.php
@@ -52,12 +52,12 @@
 
             <div class="flex items-start p-4 space-x-2 md:space-x-3 bg-white border border-border-200 rounded-xl">
                 @if ($env !== 'production')
-                    <div class="rounded-full flex items-center p-0 md:p-2.5 md:bg-red-100">
+                    <div class="rounded-full flex items-center justify-center p-0 md:p-2.5 md:bg-red-100">
                         <div class="rounded-full absolute w-3.5 h-3.5 bg-white"></div>
                         @svg('icon-mark-fail', '!h-5 w-5 relative text-red-500')
                     </div>
                 @else
-                    <div class="rounded-full flex items-center p-0 md:p-2.5 md:bg-emerald-100">
+                    <div class="rounded-full flex items-center justify-center p-0 md:p-2.5 md:bg-emerald-100">
                         <div class="rounded-full absolute w-3.5 h-3.5 bg-white"></div>
                         @svg('icon-mark-check', '!h-5 w-5 relative text-emerald-500')
                     </div>
