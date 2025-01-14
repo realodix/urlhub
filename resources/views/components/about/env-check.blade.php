b66b787a1988ba818e5ccdaf99a4d0460d4c4b01
Kei <budi03@gmail.com>
2025-01-14 08:17:14 +0700

    Update env-check.blade.php

--- resources/views/components/about/env-check.blade.php
+++ resources/views/components/about/env-check.blade.php
@@ -22,17 +22,16 @@
 
 <div class="px-2 my-6 md:mt-8 md:px-0">
     <div class="grid grid-cols-1 gap-2.5 sm:gap-3 md:gap-5 md:grid-cols-2">
-        <div class="flex items-start p-4 space-x-2 overflow-hidden text-opacity-0 transition transform bg-white border border-border-200 rounded-xl md:space-x-3">
+        <div class="flex items-start p-4 space-x-2 md:space-x-3 bg-white border border-border-200 rounded-xl">
             @if ($debug === true)
-                <div class="rounded-full p-0 md:p-2.5 md:bg-red-100 justify-center items-center flex">
-                    <div class="absolute w-3.5 h-3.5 rounded-full bg-white"></div>
+                <div class="rounded-full flex items-center justify-center p-0 md:p-2.5 md:bg-red-100">
+                    <div class="rounded-full absolute w-3.5 h-3.5 bg-white"></div>
                     @svg('icon-mark-fail', '!h-5 w-5 relative text-red-500')
                 </div>
             @else
-                <div class="rounded-full p-0 md:p-2.5 md:bg-emerald-100 justify-center items-center flex">
-                    <div class="absolute w-3.5 h-3.5 rounded-full bg-white"></div>
+                <div class="rounded-full flex items-center justify-center p-0 md:p-2.5 md:bg-emerald-100">
+                    <div class="rounded-full absolute w-3.5 h-3.5 bg-white"></div>
                     @svg('icon-mark-check', '!h-5 w-5 relative text-emerald-500')
-                    {{-- <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 relative text-emerald-500" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg> --}}
                 </div>
             @endif
 
@@ -50,15 +49,15 @@
             </div>
         </div>
 
-        <div class="flex items-start p-4 space-x-2 overflow-hidden text-opacity-0 transition transform bg-white border border-border-200 rounded-xl md:space-x-3">
+        <div class="flex items-start p-4 space-x-2 md:space-x-3 bg-white border border-border-200 rounded-xl">
             @if ($env !== 'production')
-                <div class="rounded-full p-0 md:p-2.5 md:bg-red-100 justify-center items-center flex">
-                    <div class="absolute w-3.5 h-3.5 rounded-full bg-white"></div>
+                <div class="rounded-full flex items-center p-0 md:p-2.5 md:bg-red-100">
+                    <div class="rounded-full absolute w-3.5 h-3.5 bg-white"></div>
                     @svg('icon-mark-fail', '!h-5 w-5 relative text-red-500')
                 </div>
             @else
-                <div class="rounded-full p-0 md:p-2.5 md:bg-emerald-100 justify-center items-center flex">
-                    <div class="absolute w-3.5 h-3.5 rounded-full bg-white"></div>
+                <div class="rounded-full flex items-center p-0 md:p-2.5 md:bg-emerald-100">
+                    <div class="rounded-full absolute w-3.5 h-3.5 bg-white"></div>
                     @svg('icon-mark-check', '!h-5 w-5 relative text-emerald-500')
                 </div>
             @endif
