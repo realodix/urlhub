5e52ee0ef04f9c45c1daefb443cb28b7c072f0a3
Kei <budi03@gmail.com>
2025-01-14 07:54:17 +0700

    Use blade icon

--- resources/views/components/about/env-check.blade.php
+++ resources/views/components/about/env-check.blade.php
@@ -26,16 +26,13 @@
             @if ($debug === true)
                 <div class="rounded-full p-0 md:p-2.5 md:bg-red-100 justify-center items-center flex">
                     <div class="absolute w-3.5 h-3.5 rounded-full bg-white"></div>
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 relative text-red-500" viewBox="0 0 20 20" fill="currentColor">
-                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd"></path>
-                    </svg>
+                    @svg('icon-mark-fail', '!h-5 w-5 relative text-red-500')
                 </div>
             @else
                 <div class="rounded-full p-0 md:p-2.5 md:bg-emerald-100 justify-center items-center flex">
                     <div class="absolute w-3.5 h-3.5 rounded-full bg-white"></div>
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 relative text-emerald-500" viewBox="0 0 20 20" fill="currentColor">
-                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
-                    </svg>
+                    @svg('icon-mark-check', '!h-5 w-5 relative text-emerald-500')
+                    {{-- <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 relative text-emerald-500" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg> --}}
                 </div>
             @endif
 
@@ -57,12 +54,12 @@
             @if ($env !== 'production')
                 <div class="rounded-full p-0 md:p-2.5 md:bg-red-100 justify-center items-center flex">
                     <div class="absolute w-3.5 h-3.5 rounded-full bg-white"></div>
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 relative text-red-500" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd"></path></svg>
+                    @svg('icon-mark-fail', '!h-5 w-5 relative text-red-500')
                 </div>
             @else
                 <div class="rounded-full p-0 md:p-2.5 md:bg-emerald-100 justify-center items-center flex">
                     <div class="absolute w-3.5 h-3.5 rounded-full bg-white"></div>
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 relative text-emerald-500" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
+                    @svg('icon-mark-check', '!h-5 w-5 relative text-emerald-500')
                 </div>
             @endif
 
