bb0d3d4c8f22adc1111b56347b1714edd7adc961
Kei <budi03@gmail.com>
2025-01-16 09:23:10 +0700

    wip

--- resources/views/components/about/env.blade.php
+++ resources/views/components/about/env.blade.php
@@ -8,13 +8,13 @@
         <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
             <div class="card shadow-sm p-4 md:col-start-2">
                 <p class="text-[#4f5b93] text-sm font-medium leading-4">PHP</p>
-                <p class="text-2xl font-bold text-slate-600">
+                <p class="text-2xl font-bold text-slate-700">
                     {{ phpversion() }}
                 </p>
             </div>
             <div class="card shadow-sm p-4">
                 <p class="text-[#ff2d20] text-sm font-medium leading-4">Laravel</p>
-                <p class="text-2xl font-bold text-slate-600">
+                <p class="text-2xl font-bold text-slate-700">
                     {{ app()->version() }}
                 </p>
             </div>
@@ -31,9 +31,7 @@
                 @endif
 
                 <div>
-                    <p class="-mt-1 font-bold text-gray-900">
-                        Debug Mode
-                    </p>
+                    <p class="-mt-1 font-bold">Debug Mode</p>
                     <p class="mt-0 text-sm font-light text-gray-600 md:mt-1">
                         @if ($debug === true)
                             The debug mode was expected to be <code class="code">false</code>, but actually was <code class="code">true</code>.
@@ -52,9 +50,7 @@
                 @endif
 
                 <div>
-                    <p class="-mt-1 font-bold text-gray-900">
-                        Environment
-                    </p>
+                    <p class="-mt-1 font-bold">Environment</p>
                     <p class="mt-0 text-sm font-light text-gray-600 md:mt-1">
                         @if ($env !== 'production')
                             The environment was expected to be <code class="code">production</code>, but actually was <code class="code">{{ $env }}</code>.
