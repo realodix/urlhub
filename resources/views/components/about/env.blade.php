1dd2223e090c98ff730ab9d914a4c23465889e0d
Kei <budi03@gmail.com>
2025-01-20 06:02:41 +0700

    Remove the element corner radius when the display mode is mobile
    https://github.com/realodix/urlhub/issues/1055

--- resources/views/components/about/env.blade.php
+++ resources/views/components/about/env.blade.php
@@ -6,13 +6,13 @@
 <div {{ $attributes }}>
     <div class="mb-6">
         <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
-            <div class="card shadow-sm p-4 md:col-start-2">
+            <div class="card card-master shadow-sm p-4 md:col-start-2">
                 <p class="text-[#4f5b93] text-sm font-medium leading-4">PHP</p>
                 <p class="text-2xl font-bold text-slate-700">
                     {{ phpversion() }}
                 </p>
             </div>
-            <div class="card shadow-sm p-4">
+            <div class="card card-master shadow-sm p-4">
                 <p class="text-[#ff2d20] text-sm font-medium leading-4">Laravel</p>
                 <p class="text-2xl font-bold text-slate-700">
                     {{ app()->version() }}
@@ -22,7 +22,7 @@
     </div>
 
     <div class="grid grid-cols-1 gap-2.5 sm:gap-3 md:gap-5 md:grid-cols-2">
-        <div class="card flex items-start p-4 space-x-2 md:space-x-3">
+        <div class="card card-master flex items-start p-4 space-x-2 md:space-x-3">
             @if ($debug === true)
                 <x-about.mark-fail/>
             @else
@@ -41,7 +41,7 @@
             </div>
         </div>
 
-        <div class="card flex items-start p-4 space-x-2 md:space-x-3">
+        <div class="card card-master flex items-start p-4 space-x-2 md:space-x-3">
             @if ($env !== 'production')
                 <x-about.mark-fail/>
             @else
