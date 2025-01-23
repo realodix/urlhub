ba2831320664dba4592a1c40450ebcc24cd457e1
Kei <budi03@gmail.com>
2025-01-23 23:45:45 +0700

    Apply font-semibold to active navigation link for better visual distinction

--- resources/views/components/nav-item_local-menu.blade.php
+++ resources/views/components/nav-item_local-menu.blade.php
@@ -3,9 +3,9 @@
 @endphp
 
 <a href="{{ route($attributes->get('route-name'), $attributes->get('route-params')) }}"
-    class="mr-8 py-3 font-semibold border-b-2 border-transparent
+    class="mr-8 py-3 border-b-2 border-transparent
         {{ ($expRouteName) ?
-        'text-slate-800 !border-primary-500' :
+        'font-semibold text-slate-800 !border-primary-500' :
         'text-slate-600 hover:text-slate-900 hover:border-primary-200 duration-300' }}"
 >
     @svg($attributes->get('icon'), 'mr-1')
