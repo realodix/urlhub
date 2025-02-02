dc6c6b51fe5cb03b8fdf2ccb15ff37eced5f3e0a
Kei <budi03@gmail.com>
2025-02-02 23:49:15 +0700

    $charactersLength -> $charLength

--- app/Services/KeyGeneratorService.php
+++ app/Services/KeyGeneratorService.php
@@ -71,11 +71,11 @@ class KeyGeneratorService
         $length = $this->settings->keyword_length;
 
         if (\PHP_VERSION_ID < 80300) {
-            $charactersLength = strlen($characters);
+            $charLength = strlen($characters);
 
             $randomString = '';
             for ($i = 0; $i < $length; $i++) {
-                $randomString .= $characters[mt_rand(0, $charactersLength - 1)];
+                $randomString .= $characters[mt_rand(0, $charLength - 1)];
             }
 
             return $randomString;
