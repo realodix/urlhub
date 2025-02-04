fa70524550694dd02cdb073f2ea035232c13a61f
Kei <budi03@gmail.com>
2025-02-04 21:41:27 +0700

    atomize

--- app/Livewire/Chart/LinkVisitPerWeekChart.php
+++ app/Livewire/Chart/LinkVisitPerWeekChart.php
@@ -4,6 +4,7 @@ namespace App\Livewire\Chart;
 
 use App\Models\Url;
 use App\Models\Visit;
+use Carbon\Carbon;
 use Carbon\CarbonPeriod;
 use Filament\Widgets\ChartWidget;
 
@@ -13,47 +14,9 @@ class LinkVisitPerWeekChart extends ChartWidget
 
     public Url $model;
 
-    protected function getData(): array
+    public function getDescription(): ?string
     {
-        $startDate = now()->subQuarter()->startOfWeek(); // Monday
-        $endDate = now()->endOfWeek(); // Sunday
-
-        // Create a weekly range
-        $weeks = CarbonPeriod::create($startDate, '1 week', $endDate);
-
-        // Label format per week (Jan 01 - Jan 07)
-        $label = [];
-        foreach ($weeks as $week) {
-            $startOfWeek = $week->copy()->startOfWeek(); // Monday
-            $endOfWeek = $week->copy()->endOfWeek(); // Sunday
-            $label[] = $startOfWeek->format('M d') . ' - ' . $endOfWeek->format('M d');
-        }
-
-        // Retrieve all data in the time range
-        $visitModel = Visit::where('url_id', $this->model->id);
-        $rawData = $visitModel->whereBetween('created_at', [$startDate, $endDate])
-            ->get()
-            // Group by start of week
-            ->groupBy(fn(Visit $visit) => $visit->created_at->startOfWeek()->format('Y-m-d'));
-
-        // Calculate the number of visits per week
-        $data = [];
-        foreach ($weeks as $week) {
-            $startOfWeek = $week->copy()->startOfWeek()->format('Y-m-d');
-            $data[] = $rawData->get($startOfWeek, collect())->count();
-        }
-
-        return [
-            'datasets' => [
-                [
-                    'label'           => 'Visits',
-                    'data'            => $data,
-                    'backgroundColor' => '#006edb',
-                    'borderColor'     => '#006edb',
-                ],
-            ],
-            'labels' => $label,
-        ];
+        return 'Stats for past quarter';
     }
 
     protected function getType(): string
@@ -61,8 +24,57 @@ class LinkVisitPerWeekChart extends ChartWidget
         return 'line';
     }
 
-    public function getDescription(): ?string
+    protected function getData(): array
     {
-        return 'Stats for past quarter';
+        $startDate = Carbon::now()->subQuarter()->startOfWeek(); // Monday
+        $endDate = Carbon::now()->endOfWeek(); // Sunday
+        $period = CarbonPeriod::create($startDate, '1 week', $endDate);
+
+        return [
+            'datasets' => [
+                [
+                    'label'           => 'Visits',
+                    'data'            => $this->chartData($startDate, $endDate, $period),
+                    'backgroundColor' => '#006edb',
+                    'borderColor'     => '#006edb',
+                ],
+            ],
+            'labels' => $this->chartLabel($period),
+        ];
+    }
+
+    /**
+     * Return the chart data
+     */
+    protected function chartData(Carbon $startDate, Carbon $endDate, CarbonPeriod $period): array
+    {
+        $model = Visit::where('url_id', $this->model->id);
+        $rawData = $model->whereBetween('created_at', [$startDate, $endDate])
+            ->get()
+            ->countBy(fn(Visit $visit) => $visit->created_at->startOfWeek()->format('Y-m-d'));
+
+        // Calculate the number of visits per week
+        $data = [];
+        foreach ($period as $week) {
+            $startOfWeek = $week->copy()->startOfWeek()->format('Y-m-d');
+            $data[] = $rawData->get($startOfWeek, 0);
+        }
+
+        return $data;
+    }
+
+    /**
+     * Label format per week (Jan 01 - Jan 07)
+     */
+    public function chartLabel(CarbonPeriod $period): array
+    {
+        $label = [];
+        foreach ($period as $week) {
+            $startOfWeek = $week->copy()->startOfWeek(); // Monday
+            $endOfWeek = $week->copy()->endOfWeek(); // Sunday
+            $label[] = $startOfWeek->format('M d') . ' - ' . $endOfWeek->format('M d');
+        }
+
+        return $label;
     }
 }
