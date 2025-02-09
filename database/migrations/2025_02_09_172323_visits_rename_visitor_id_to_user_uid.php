c125547f192bd2a7f9edd97a601c949c4eb3f0ec
Kei <budi03@gmail.com>
2025-02-10 00:27:01 +0700

    Visit: user_uid

---
+++ database/migrations/2025_02_09_172323_visits_rename_visitor_id_to_user_uid.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('visits', function (Blueprint $table) {
+            $table->renameColumn('visitor_id', 'user_uid');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('visits', function (Blueprint $table) {
+            $table->renameColumn('user_uid', 'visitor_id');
+        });
+    }
+};
