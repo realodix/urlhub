ec5cfad270740d3f2a312ce02f017b0d3b42dcc1
Kei <budi03@gmail.com>
2025-02-11 00:26:57 +0700

    improve the link shortening box

--- resources/views/frontend/homepage.blade.php
+++ resources/views/frontend/homepage.blade.php
@@ -24,7 +24,7 @@
                 @csrf
                     <div class="mt-1 text-center">
                         <input name="long_url" required value="{{ old('long_url') }}" placeholder="{{ __('Shorten your link') }}"
-                            class="w-full md:w-4/6 px-2 md:px-4 h-12 sm:h-14
+                            class="w-full -mb-px md:mb-0 md:w-4/6 px-2 md:px-4 h-12 sm:h-14
                                 text-xl outline-none rounded-t-md md:rounded-l-md md:rounded-r-none
                                 border border-border-200 focus:border-primary-600
                                 dark:bg-dark-900 dark:border-dark-700 dark:focus:border-dark-600
