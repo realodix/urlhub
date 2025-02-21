b57ff060c18a1cb9bf49b0bd2666ceda40592e64
Kei <budi03@gmail.com>
2025-02-21 14:11:47 +0700

    chore(users): Reorder forward_query column
    Moves the `forward_query` column in the `users` table to be positioned after the `password` column.

--- database/migrations/2025_02_16_160415_users_create_forward_query_column.php
+++ database/migrations/2025_02_16_160415_users_create_forward_query_column.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             $table->boolean('forward_query')
-                ->after('remember_token')
+                ->after('password')
                 ->default(true);
         });
     }
