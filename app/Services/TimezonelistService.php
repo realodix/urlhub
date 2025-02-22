1eb747dd3cd6cabbfe789ce3f926db622240a77f
Kei <budi03@gmail.com>
2025-02-23 02:27:58 +0700

    Update TimezonelistService

--- app/Services/TimezonelistService.php
+++ app/Services/TimezonelistService.php
@@ -4,26 +4,10 @@ namespace App\Services;
 
 class TimezonelistService
 {
-    const HTML_MINUS = '&#8722;';
-    const HTML_PLUS = '&#43;';
     const HTML_WHITESPACE = '&#160;';
+    const GENERAL_TIMEZONE = 'UTC';
 
-    /**
-     * General timezones.
-     *
-     * @var array
-     */
-    protected $generalTimezones = [
-        'GMT',
-        'UTC',
-    ];
-
-    /**
-     * All continents of the world.
-     *
-     * @var array
-     */
-    protected $continents = [
+    protected array $continents = [
         'Africa'     => \DateTimeZone::AFRICA,
         'America'    => \DateTimeZone::AMERICA,
         'Antarctica' => \DateTimeZone::ANTARCTICA,
@@ -37,231 +21,44 @@ class TimezonelistService
     ];
 
     /**
-     * The filter of the groups to get.
-     *
-     * @var array
-     */
-    protected $groupsFilter = [];
-
-    /**
-     * Status of grouping the return list.
-     *
-     * @var bool
-     */
-    protected $splitGroup = true;
-
-    /**
-     * Status of showing timezone offset.
-     *
-     * @var bool
-     */
-    protected $showOffset = true;
-
-    /**
-     * The offset prefix in list.
-     *
-     * @var string
-     */
-    protected $offsetPrefix = 'GMT/UTC';
-
-    /**
-     * Set the filter of the groups want to get.
-     *
-     * @param array $groups
-     * @return $this
-     */
-    public function onlyGroups($groups = [])
-    {
-        $this->groupsFilter = $groups;
-
-        return $this;
-    }
-
-    /**
-     * Set the filter of the groups do not want to get.
-     *
-     * @param array $groups
-     * @return $this
-     */
-    public function excludeGroups($groups = [])
-    {
-        if (empty($groups)) {
-            $this->groupsFilter = [];
-
-            return $this;
-        }
-
-        $this->groupsFilter = array_values(array_diff(array_keys($this->continents), $groups));
-
-        if (!in_array('General', $groups)) {
-            $this->groupsFilter[] = 'General';
-        }
-
-        return $this;
-    }
-
-    /**
-     * Decide whether to split group or not.
-     *
-     * @param bool $status
-     * @return $this
-     */
-    public function splitGroup($status = true)
-    {
-        $this->splitGroup = (bool) $status;
-
-        return $this;
-    }
-
-    /**
-     * Decide whether to show the offset or not.
-     *
-     * @param bool $status
-     * @return $this
-     */
-    public function showOffset($status = true)
-    {
-        $this->showOffset = (bool) $status;
-
-        return $this;
-    }
-
-    /**
-     * Return new static to reset all config.
-     *
-     * @return $this
-     */
-    public function reset()
-    {
-        return new static;
-    }
-
-    /**
-     * Create an array of timezones.
-     *
-     * @param bool $htmlencode Use HTML entities for items
-     * @return mixed
-     */
-    public function toArray($htmlencode = true)
-    {
-        $list = [];
-
-        // If do not split group
-        if (!$this->splitGroup) {
-            if ($this->includeGeneral()) {
-                foreach ($this->generalTimezones as $timezone) {
-                    $list[$timezone] = $this->formatTimezone($timezone, null, $htmlencode);
-                }
-            }
-
-            foreach ($this->loadContinents() as $continent => $mask) {
-                $timezones = \DateTimeZone::listIdentifiers($mask);
-
-                foreach ($timezones as $timezone) {
-                    $list[$timezone] = $this->formatTimezone($timezone, null, $htmlencode);
-                }
-            }
-
-            return $list;
-        }
-
-        // If split group
-        if ($this->includeGeneral()) {
-            foreach ($this->generalTimezones as $timezone) {
-                $list['General'][$timezone] = $this->formatTimezone($timezone, null, $htmlencode);
-            }
-        }
-
-        foreach ($this->loadContinents() as $continent => $mask) {
-            $timezones = \DateTimeZone::listIdentifiers($mask);
-
-            foreach ($timezones as $timezone) {
-                $list[$continent][$timezone] = $this->formatTimezone($timezone, $continent, $htmlencode);
-            }
-        }
-
-        return $list;
-    }
-
-    /**
-     * Alias of the `toSelectBox()` method.
-     *
-     * @deprecated 6.0.0 This method name no longer matches the semantics
+     * Create a select box of timezones
      *
      * @param string $name The name of the select tag
-     * @param string|null $selected The selected value
-     * @param array|string|null $attrs The HTML attributes of select tag
-     * @param bool $htmlencode Use HTML entities for values of select tag
-     * @return string
+     * @param string|null $selected HTML <option> selected attribute
+     * @param array|null $attrs HTML attribute of the <select> tag
      */
-    public function create($name, $selected = null, $attrs = null, $htmlencode = true)
+    public function toSelectBox(string $name, ?string $selected = null, ?array $attrs = null): string
     {
-        return $this->toSelectBox($name, $selected, $attrs, $htmlencode);
-    }
-
-    /**
-     * Create a select box of timezones.
-     *
-     * @param string $name The name of the select tag
-     * @param string|null $selected The selected value
-     * @param array|string|null $attrs The HTML attributes of select tag
-     * @param bool $htmlencode Use HTML entities for values of select tag
-     * @return string
-     */
-    public function toSelectBox($name, $selected = null, $attrs = null, $htmlencode = true)
-    {
-        // Attributes for select element
-        $attrString = null;
-
+        $attributes = '';
         if (!empty($attrs)) {
-            if (is_array($attrs)) {
-                foreach ($attrs as $attr_name => $attr_value) {
-                    $attrString .= ' '.$attr_name.'="'.$attr_value.'"';
-                }
-            } else {
-                $attrString = $attrs;
+            foreach ($attrs as $attr_name => $attr_value) {
+                $attributes .= ' '.$attr_name.'="'.$attr_value.'"';
             }
         }
 
-        if ($this->splitGroup) {
-            return $this->makeSelectTagWithGroup($name, $selected, $attrString, $htmlencode);
-        }
+        $output = '<select name="'.$name.'"'.$attributes.'>';
 
-        return $this->makeSelectTagWithoutGroup($name, $selected, $attrString, $htmlencode);
-    }
+        // General
+        $output .= '<optgroup label="General">';
+        $output .= $this->makeOptionTag(
+            $this->formatTimezone(self::GENERAL_TIMEZONE),
+            self::GENERAL_TIMEZONE,
+            ($selected == self::GENERAL_TIMEZONE),
+        );
+        $output .= '</optgroup>';
 
-    /**
-     * Generate select element with the optgroup tag.
-     *
-     * @param string $name The name of the select tag
-     * @param string|null $selected The selected value
-     * @param string|null $attrs The HTML attributes of select tag
-     * @param bool $htmlencode Use HTML entities for values of select tag
-     * @return string
-     */
-    protected function makeSelectTagWithGroup($name, $selected = null, $attrs = null, $htmlencode = true)
-    {
-        $attrs = !empty($attrs) ? ' '.trim((string) $attrs) : '';
-        $output = '<select name="'.(string) $name.'"'.$attrs.'>';
-
-        if ($this->includeGeneral()) {
-            $output .= '<optgroup label="General">';
-
-            foreach ($this->generalTimezones as $timezone) {
-                $output .= $this->makeOptionTag($this->formatTimezone($timezone, null, $htmlencode), $timezone, ($selected == $timezone));
-            }
-
-            $output .= '</optgroup>';
-        }
-
-        foreach ($this->loadContinents() as $continent => $mask) {
+        // Continent
+        foreach ($this->continents as $continent => $mask) {
             $timezones = \DateTimeZone::listIdentifiers($mask);
+
             $output .= '<optgroup label="'.$continent.'">';
-
             foreach ($timezones as $timezone) {
-                $output .= $this->makeOptionTag($this->formatTimezone($timezone, $continent, $htmlencode), $timezone, ($selected == $timezone));
+                $output .= $this->makeOptionTag(
+                    $this->formatTimezone($timezone, $continent),
+                    $timezone,
+                    ($selected == $timezone),
+                );
             }
-
             $output .= '</optgroup>';
         }
 
@@ -271,169 +68,26 @@ class TimezonelistService
     }
 
     /**
-     * Generate select element without the optgroup tag.
-     *
-     * @param string $name The name of the select tag
-     * @param string|null $selected The selected value
-     * @param string|null $attrs The HTML attributes of select tag
-     * @param bool $htmlencode Use HTML entities for values of select tag
-     * @return string
+     * Generate HTML <option> tag
      */
-    protected function makeSelectTagWithoutGroup($name, $selected = null, $attrs = null, $htmlencode = true)
+    protected function makeOptionTag(string $display, string $value, bool $selected): string
     {
-        $attrs = !empty($attrs) ? ' '.trim((string) $attrs) : '';
-        $output = '<select name="'.(string) $name.'"'.$attrs.'>';
-
-        if ($this->includeGeneral()) {
-            foreach ($this->generalTimezones as $timezone) {
-                $output .= $this->makeOptionTag($this->formatTimezone($timezone, null, $htmlencode), $timezone, ($selected == $timezone));
-            }
-        }
-
-        foreach ($this->loadContinents() as $continent => $mask) {
-            $timezones = \DateTimeZone::listIdentifiers($mask);
-
-            foreach ($timezones as $timezone) {
-                $output .= $this->makeOptionTag($this->formatTimezone($timezone, null, $htmlencode), $timezone, ($selected == $timezone));
-            }
-        }
-
-        $output .= '</select>';
-
-        return $output;
-    }
-
-    /**
-     * Generate the option HTML tag.
-     *
-     * @param string $display
-     * @param string $value
-     * @param bool $selected
-     * @return string
-     */
-    protected function makeOptionTag($display, $value, $selected = false)
-    {
-        $attrs = (bool) $selected ? ' selected="selected"' : '';
+        $attrs = $selected ? ' selected="selected"' : '';
 
         return '<option value="'.(string) $value.'"'.$attrs.'>'.(string) $display.'</option>';
     }
 
     /**
-     * DetermineCheck if the general timezones is loaded in the returned result.
-     *
-     * @return bool
+     * Format to display timezones
      */
-    protected function includeGeneral()
+    protected function formatTimezone(string $timezone, ?string $cutOffContinent = null): string
     {
-        return empty($this->groupsFilter) || in_array('General', $this->groupsFilter);
-    }
+        $displayedTz = empty($cutOffContinent) ? $timezone : substr($timezone, strlen($cutOffContinent) + 1);
+        $normalizedTz = str_replace(['St_', '/', '_'], ['St. ', ' / ', ' '], $displayedTz);
+        $offset = (new \DateTime('', new \DateTimeZone($timezone)))
+            ->format('P');
+        $separator = str_repeat(self::HTML_WHITESPACE, 3);
 
-    /**
-     * Load filtered continents.
-     *
-     * @return array
-     */
-    protected function loadContinents()
-    {
-        if (empty($this->groupsFilter)) {
-            return $this->continents;
-        }
-
-        return array_filter($this->continents, function ($key) {
-            return in_array($key, $this->groupsFilter);
-        }, ARRAY_FILTER_USE_KEY);
-    }
-
-    /**
-     * Format to display timezones.
-     *
-     * @param string $timezone
-     * @param string|null $cutOffContinent
-     * @param bool $htmlencode
-     * @return string
-     */
-    protected function formatTimezone($timezone, $cutOffContinent = null, $htmlencode = true)
-    {
-        $displayedTimezone = empty($cutOffContinent) ? $timezone : substr($timezone, strlen($cutOffContinent) + 1);
-        $normalizedTimezone = $this->normalizeTimezone($displayedTimezone, $htmlencode);
-
-        if (!$this->showOffset) {
-            return $normalizedTimezone;
-        }
-
-        $notmalizedOffset = $this->normalizeOffset($this->getOffset($timezone), $htmlencode);
-        $separator = $this->normalizeSeparator($htmlencode);
-
-        return '('.$this->offsetPrefix.$notmalizedOffset.')'.$separator.$normalizedTimezone;
-    }
-
-    /**
-     * Normalize the offset.
-     *
-     * @param string $offset
-     * @param bool $htmlencode
-     * @return string
-     */
-    protected function normalizeOffset($offset, $htmlencode = true)
-    {
-        $search = ['-', '+'];
-        $replace = $htmlencode ? [' '.self::HTML_MINUS.' ', ' '.self::HTML_PLUS.' '] : [' - ', ' + '];
-
-        return str_replace($search, $replace, $offset);
-    }
-
-    /**
-     * Normalize the timezone.
-     *
-     * @param string $timezone
-     * @param bool $htmlencode
-     * @return string
-     */
-    protected function normalizeTimezone($timezone, $htmlencode = true)
-    {
-        $search = ['St_', '/', '_'];
-        $replace = ['St. ', ' / ', ' '];
-
-        return str_replace($search, $replace, $timezone);
-    }
-
-    /**
-     * Normalize the separator beetween the timezone and offset.
-     *
-     * @param bool $htmlencode
-     * @return string
-     */
-    protected function normalizeSeparator($htmlencode = true)
-    {
-        return $htmlencode ? str_repeat(self::HTML_WHITESPACE, 5) : ' ';
-    }
-
-    /**
-     * Get the timezone offset.
-     *
-     * @param string $timezone
-     * @return string
-     */
-    protected function getOffset($timezone)
-    {
-        $time = new \DateTime('', new \DateTimeZone($timezone));
-
-        return $time->format('P');
-    }
-
-    /**
-     * Get the difference of timezone to Coordinated Universal Time (UTC).
-     *
-     * @param string $timezone
-     * @return string
-     */
-    protected function getUTCOffset($timezone)
-    {
-        $dateTimeZone = new \DateTimeZone($timezone);
-        $utcTime = new \DateTime('', new \DateTimeZone('UTC'));
-        $offset = $dateTimeZone->getOffset($utcTime);
-        $format = gmdate('H:i', abs($offset));
-
-        return $offset >= 0 ? "+{$format}" : "-{$format}";
+        return '('.self::GENERAL_TIMEZONE.$offset.')'.$separator.$normalizedTz;
     }
 }
--- tests/Unit/Services/TimezonelistServiceTest.php
+++ tests/Unit/Services/TimezonelistServiceTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit\Services;
 
 use App\Services\TimezonelistService;
+use PHPUnit\Framework\Attributes as PHPUnit;
 use PHPUnit\Framework\TestCase;
 
 class TimezonelistServiceTest extends TestCase
@@ -14,108 +15,37 @@ class TimezonelistServiceTest extends TestCase
         $this->service = new TimezonelistService;
     }
 
-    public function testOnlyGroups(): void
+    #[PHPUnit\Test]
+    public function toSelectBox(): void
     {
-        $service = $this->service->onlyGroups(['Africa', 'America']);
-        $this->assertInstanceOf(TimezonelistService::class, $service);
-
-        $output = $this->service->onlyGroups(['Africa'])->toSelectBox('timezone_only_africa');
+        $output = $this->service->toSelectBox('timezone_default');
+        $this->assertStringStartsWith('<select name="timezone_default"', $output);
+        $this->assertStringContainsString('<optgroup label="General">', $output);
+        $this->assertStringContainsString('<option value="UTC">(UTC+00:00)&#160;&#160;&#160;UTC</option>', $output);
         $this->assertStringContainsString('<optgroup label="Africa">', $output);
-        $this->assertStringNotContainsString('<optgroup label="America">', $output);
-
-        $output = $this->service->onlyGroups(['America'])->toSelectBox('timezone_only_america');
-        $this->assertStringNotContainsString('<optgroup label="Africa">', $output);
+        $this->assertStringContainsString('<option value="Africa/Abidjan">(UTC', $output);
         $this->assertStringContainsString('<optgroup label="America">', $output);
-    }
-
-    public function testExcludeGroups(): void
-    {
-        $service = $this->service->excludeGroups(['Africa']);
-        $this->assertInstanceOf(TimezonelistService::class, $service);
-
-        $output = $this->service->excludeGroups(['Africa'])->toSelectBox('timezone_exclude_africa');
-        $this->assertStringNotContainsString('<optgroup label="Africa">', $output);
-        $this->assertStringContainsString('<optgroup label="America">', $output);
-
-        $output = $this->service->excludeGroups(['America'])->toSelectBox('timezone_exclude_america');
-        $this->assertStringContainsString('<optgroup label="Africa">', $output);
-        $this->assertStringNotContainsString('<optgroup label="America">', $output);
-
-        $output = $this->service->excludeGroups(['General'])->toSelectBox('timezone_exclude_general');
-        $this->assertStringNotContainsString('<optgroup label="General">', $output);
-        $this->assertStringContainsString('<optgroup label="Africa">', $output);
-    }
-
-    public function testSplitGroup(): void
-    {
-        $service = $this->service->splitGroup(true);
-        $this->assertInstanceOf(TimezonelistService::class, $service);
-        $outputWithGroup = $this->service->splitGroup(true)->toSelectBox('timezone_split_true');
-        $this->assertStringContainsString('<optgroup', $outputWithGroup, 'Asserting optgroup tag exists when splitGroup is true');
-
-        $outputWithoutGroup = $this->service->splitGroup(false)->toSelectBox('timezone_split_false');
-        $this->assertStringNotContainsString('<optgroup', $outputWithoutGroup, 'Asserting optgroup tag does not exist when splitGroup is false');
-    }
-
-    public function testShowOffset(): void
-    {
-        $service = $this->service->showOffset(true);
-        $this->assertInstanceOf(TimezonelistService::class, $service);
-
-        $outputWithOffset = $this->service->showOffset(true)->toSelectBox('timezone_offset_true');
-        $this->assertStringContainsString('(GMT/UTC', $outputWithOffset, 'Asserting offset prefix exists when showOffset is true');
-
-        $outputWithoutOffset = $this->service->showOffset(false)->toSelectBox('timezone_offset_false');
-        $this->assertStringNotContainsString('(GMT/UTC', $outputWithoutOffset, 'Asserting offset prefix does not exist when showOffset is false');
-    }
-
-    public function testToSelectBox_WithGroup_WithOffset(): void
-    {
-        $output = $this->service->splitGroup()->showOffset()->toSelectBox('timezone_with_group_offset');
-        $this->assertStringStartsWith('<select name="timezone_with_group_offset"', $output);
-        $this->assertStringContainsString('<optgroup label="Africa">', $output);
-        $this->assertStringContainsString('<option value="Africa/Abidjan">(GMT/UTC', $output);
+        $this->assertStringContainsString('<option value="America/New_York">(UTC-05:00)', $output);
+        $this->assertStringContainsString('<optgroup label="Antarctica">', $output);
+        $this->assertStringContainsString('<option value="Antarctica/Casey">(UTC', $output);
+        $this->assertStringContainsString('<optgroup label="Arctic">', $output);
+        $this->assertStringContainsString('<option value="Arctic/Longyearbyen">(UTC', $output);
+        $this->assertStringContainsString('<optgroup label="Asia">', $output);
+        $this->assertStringContainsString('<option value="Asia/Aden">(UTC', $output);
+        $this->assertStringContainsString('<optgroup label="Atlantic">', $output);
+        $this->assertStringContainsString('<option value="Atlantic/Azores">(UTC', $output);
+        $this->assertStringContainsString('<optgroup label="Australia">', $output);
+        $this->assertStringContainsString('<option value="Australia/Adelaide">(UTC', $output);
+        $this->assertStringContainsString('<optgroup label="Europe">', $output);
+        $this->assertStringContainsString('<option value="Europe/Amsterdam">(UTC', $output);
+        $this->assertStringContainsString('<optgroup label="Indian">', $output);
+        $this->assertStringContainsString('<option value="Indian/Antananarivo">(UTC', $output);
+        $this->assertStringContainsString('<optgroup label="Pacific">', $output);
+        $this->assertStringContainsString('<option value="Pacific/Apia">(UTC+13:00)', $output);
         $this->assertStringEndsWith('</select>', $output);
 
-        // Implicitly tests normalizeOffset, getOffset, normalizeTimezone, normalizeSeparator via output
-        $this->assertStringContainsString('&#8722;', $output, 'Ensure HTML_MINUS is in offset (normalizeOffset)');
-        $this->assertStringContainsString('&#43;', $output, 'Ensure HTML_PLUS is in offset (normalizeOffset)');
-        $this->assertStringContainsString('&#160;&#160;&#160;&#160;&#160;', $output, 'Ensure normalizeSeparator works');
-    }
-
-    public function testToSelectBox_WithoutGroup_WithOffset(): void
-    {
-        $output = $this->service->splitGroup(false)->showOffset()->toSelectBox('timezone_without_group_offset');
-        $this->assertStringStartsWith('<select name="timezone_without_group_offset"', $output);
-        $this->assertStringNotContainsString('<optgroup label="Africa">', $output);
-        $this->assertStringContainsString('<option value="Africa/Abidjan">(GMT/UTC', $output);
-        $this->assertStringEndsWith('</select>', $output);
-
-        // Implicitly tests normalizeOffset, getOffset, normalizeTimezone, normalizeSeparator via output
-        $this->assertStringContainsString('&#8722;', $output, 'Ensure HTML_MINUS is in offset (normalizeOffset)');
-        $this->assertStringContainsString('&#43;', $output, 'Ensure HTML_PLUS is in offset (normalizeOffset)');
-        $this->assertStringContainsString('&#160;&#160;&#160;&#160;&#160;', $output, 'Ensure normalizeSeparator works');
-    }
-
-    public function testToSelectBox_WithGroup_WithoutOffset(): void
-    {
-        $output = $this->service->splitGroup()->showOffset(false)->toSelectBox('timezone_with_group_no_offset');
-        $this->assertStringStartsWith('<select name="timezone_with_group_no_offset"', $output);
-        $this->assertStringContainsString('<optgroup label="Africa">', $output);
-        $this->assertStringNotContainsString('(GMT/UTC', $output);
-        $this->assertStringContainsString('<option value="Africa/Abidjan">Abidjan</option>', $output);
-        $this->assertStringEndsWith('</select>', $output);
-    }
-
-    public function testToSelectBox_WithoutGroup_WithoutOffset(): void
-    {
-        $output = $this->service->splitGroup(false)->showOffset(false)->toSelectBox('timezone_no_group_no_offset');
-        $this->assertStringStartsWith('<select name="timezone_no_group_no_offset"', $output);
-        $this->assertStringNotContainsString('<optgroup label="Africa">', $output);
-        $this->assertStringNotContainsString('(GMT/UTC', $output);
-        $this->assertStringContainsString('<option value="Africa/Abidjan">Africa / Abidjan</option>', $output); // Corrected Assertion
-        $this->assertStringEndsWith('</select>', $output);
-        // Removed the problematic implicit assertion for normalizeTimezone here
+        // Implicitly tests normalizeSeparator via output
+        $this->assertStringContainsString('&#160;&#160;&#160;', $output, 'Ensure normalizeSeparator works');
     }
 
     public function testToSelectBox_WithSelectedValue(): void
@@ -130,27 +60,23 @@ class TimezonelistServiceTest extends TestCase
         $attrsArray = ['class' => 'form-control', 'id' => 'timezone-select'];
         $outputArray = $this->service->toSelectBox('timezone_attrs_array', null, $attrsArray);
         $this->assertStringContainsString('<select name="timezone_attrs_array" class="form-control" id="timezone-select">', $outputArray);
+    }
 
-        $attrsString = 'class="form-control" id="timezone-select"';
-        $outputString = $this->service->toSelectBox('timezone_attrs_string', null, $attrsString);
-        $this->assertStringContainsString('<select name="timezone_attrs_string" class="form-control" id="timezone-select">', $outputString);
+    public function testNormalizeTimezone(): void
+    {
+        $output = $this->service->toSelectBox('timezone_default');
+        $this->assertStringContainsString(
+            '<option value="America/Argentina/Rio_Gallegos">(UTC-03:00)&#160;&#160;&#160;Argentina / Rio Gallegos</option>',
+            $output
+        );
+        $this->assertStringContainsString(
+            '<option value="America/St_Johns">(UTC-03:30)&#160;&#160;&#160;St. Johns</option>',
+            $output
+        );
     }
 
     public function testConstants(): void
     {
-        $this->assertSame('&#8722;', TimezonelistService::HTML_MINUS);
-        $this->assertSame('&#43;', TimezonelistService::HTML_PLUS);
         $this->assertSame('&#160;', TimezonelistService::HTML_WHITESPACE);
     }
-
-    // Example of a dedicated test for normalizeTimezone (if you really need it for isolated testing)
-    // In general, it's better to test via public methods.
-    /*
-    public function testNormalizeTimezone_Dedicated(): void
-    {
-        $this->assertSame('St. Helena', $this->service->normalizeTimezone('St_Helena'));
-        $this->assertSame('America / Argentina / Buenos Aires', $this->service->normalizeTimezone('America/Argentina/Buenos_Aires'));
-        $this->assertSame('America / Argentina / Buenos Aires', $this->service->normalizeTimezone('America/Argentina/Buenos_Aires'));
-    }
-    */
 }
