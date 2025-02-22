488f31037a5362ff338a4f13ce219b6f43bacd7b
Kei <budi03@gmail.com>
2025-02-23 00:28:07 +0700

    Move REGEXP SQLite UDF to MacroServiceProvider
    https://github.com/realodix/urlhub/pull/999

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,14 +22,5 @@ class AppServiceProvider extends ServiceProvider
     {
         // Prevent destructive commands from running in production environments.
         DB::prohibitDestructiveCommands($this->app->isProduction());
-
-        // A SQLite UDF for the REGEXP keyword that mimics the behavior in MySQL.
-        if (DB::connection() instanceof \Illuminate\Database\SQLiteConnection) {
-            DB::connection()->getPdo()->sqliteCreateFunction('REGEXP', function (string $pattern, string $value) {
-                mb_regex_encoding('UTF-8');
-
-                return mb_ereg($pattern, $value) !== false ? 1 : 0;
-            });
-        }
     }
 }
