e7b218554c71d57d7d78d7d4a02d375f387da53a
Kei <budi03@gmail.com>
2025-03-12 01:50:36 +0700

    style: enforce single quotes for consistency
    To improve code consistency, this commit standardizes all string literals to use single quotes. Double quotes are only used when a string contains single quotes.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,13 @@ import colors from 'tailwindcss/colors';
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        "./app/Livewire/**/*Table.php",
-        "./resources/**/*.blade.php",
-        "./resources/**/*.js",
-        "./resources/**/*.vue",
-        "./vendor/filament/**/*.blade.php",
-        "./vendor/power-components/livewire-powergrid/resources/views/**/*.php",
-        "./vendor/power-components/livewire-powergrid/src/Themes/Tailwind.php",
+        './app/Livewire/**/*Table.php',
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
+        './vendor/filament/**/*.blade.php',
+        './vendor/power-components/livewire-powergrid/resources/views/**/*.php',
+        './vendor/power-components/livewire-powergrid/src/Themes/Tailwind.php',
     ],
     presets: [
         presetPowerGrid,
@@ -20,8 +20,8 @@ export default {
     theme: {
         extend: {
             colors: {
-                "primary": colors.blue,
-                "dark": colors.neutral,
+                'primary': colors.blue,
+                'dark': colors.neutral,
                 'pg-primary': colors.gray, // PowerGrid
             },
         },
