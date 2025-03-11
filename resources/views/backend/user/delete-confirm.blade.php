4b5938e5e4c5525bb56ef2b38752a6cd31a8bf6b
Kei <budi03@gmail.com>
2025-03-11 13:01:29 +0700

    improve delete confirmation dialog box
    - https://github.com/realodix/urlhub/pull/1100
    - https://github.com/realodix/urlhub/pull/1099

--- resources/views/backend/user/delete-confirm.blade.php
+++ resources/views/backend/user/delete-confirm.blade.php
@@ -7,7 +7,7 @@
     <div class="card max-w-4xl ml-0 lg:ml-12 p-8">
         <div class="font-bold text-xl mb-4">Confirm User Deletion</div>
         <div class="card-body">
-            <p>Are you sure you want to delete user: <strong>{{ $user->name }} ({{ $user->email }})</strong>? All of the user's data will be permanently deleted. This action cannot be undone.</p>
+            <p>Are you sure you want to delete user: <strong class="text-primary-700 dark:text-primary-500">{{ $user->name }} ({{ $user->email }})</strong>? All of the user's data will be permanently deleted. This action cannot be undone.</p>
 
             <br>
             <br>
@@ -15,10 +15,10 @@
             <div class="flex justify-end">
                 <form action="{{ route('user.delete', $user) }}" method="POST">
                     @csrf @method('DELETE')
-                    <button type="submit" class="btn btn-sm bg-red-600 !text-white hover:bg-red-500">
+                    <button type="submit" class="btn btn-sm btn-delete-danger">
                         {{ __('Confirm Deletion') }}
                     </button>
-                    <a href="{{ url()->previous() }}" class="btn btn-sm btn-secondary">{{ __('Cancel') }}</a>
+                    <a href="{{ url()->previous() }}" class="btn btn-sm btn-secondary ml-4">{{ __('Cancel') }}</a>
                 </form>
             </div>
         </div>
