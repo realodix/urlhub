ca6c0635ae15854d15b840145db84dcaacecf841
Kei <budi03@gmail.com>
2025-03-20 02:29:30 +0700

    Update backend.blade.php

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -20,8 +20,6 @@
     </main>
 
     @livewireScripts
-    {{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css"> --}}
-{{-- <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script> --}}
 </body>
 
 </html>
