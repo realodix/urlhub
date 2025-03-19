3626b21d17f4a4025e9b5751ca328903807fee5c
Kei <budi03@gmail.com>
2025-03-20 00:43:29 +0700

    Update expired.blade.php

--- resources/views/frontend/expired.blade.php
+++ resources/views/frontend/expired.blade.php
@@ -4,22 +4,15 @@
 
 @section('content')
 <div class="flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-    <div class="card !rounded-3xl max-w-3xl w-full space-y-8 p-12 relative overflow-hidden">
-        <div>
-            <div class="flex justify-center">
-                <x-icon-link-expired class="!w-32 !h-32 text-orange-500 animate-pulse" />
-            </div>
-            <h2 class="mt-6 text-center text-4xl font-extrabold text-gray-800 dark:text-dark-400 drop-shadow-md">
+    <div class="card !rounded-2xl  relative mx-auto mt-4 w-full max-w-screen-lg overflow-hidden p-6 text-center sm:p-10 sm:px-0">
+        <div class="relative mx-auto flex w-full max-w-md flex-col items-center">
+            <x-icon-link-expired class="!w-32 !h-32 text-orange-500 animate-pulse" />
+
+            <h1 class="font-display mt-10 text-center text-4xl font-medium text-neutral-900 dark:text-dark-400 sm:text-5xl sm:leading-[1.15]">
                 Expired link
-            </h2>
-        </div>
+            </h1>
 
-        <div class="text-center">
-            <code class="block break-all text-orange-700 font-mono text-sm mt-3">
-                {{ $url->short_url }}
-            </code>
-
-            <p class="text-lg text-gray-700 dark:text-dark-300 leading-relaxed mt-4">
+            <p class="mt-8 text-pretty text-base text-neutral-700 dark:text-dark-300 sm:text-xl">
                 This link has expired. Please contact the owner of this link to get a new one.
             </p>
         </div>
