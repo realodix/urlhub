aa490d0f379895cb3c8b8b2997d2ddda47de5282
Kei <budi03@gmail.com>
2025-03-31 02:47:11 +0700

    Put the author name and status in the destination column
    Protect links with password
    https://github.com/realodix/urlhub/pull/1108
    
    Link Expiration
    https://github.com/realodix/urlhub/pull/1109
    
    Merge the author's name into the destination link field
    https://github.com/realodix/urlhub/commit/6929f2de40f47350c24f9dbe322d8b145f79073f

--- resources/views/components/table/destination.blade.php
+++ resources/views/components/table/destination.blade.php
@@ -1,5 +1,12 @@
 <div>
-    <a href="{{ route('link.edit', $model) }}" class="text-slate-700 dark:text-dark-400">
+    @php
+        $linkClasses = 'text-slate-700 dark:text-dark-400';
+        if ($model->isExpired()) {
+            $linkClasses .= ' line-through !text-red-500 dark:!text-red-400';
+        }
+    @endphp
+
+    <a href="{{ route('link.edit', $model) }}" class="{{ $linkClasses }}">
         {{ urlDisplay($destination, $limit) }}
     </a>
 
@@ -8,13 +15,25 @@
     >
         @svg('open-link-in-new')
     </a>
+</div>
 
-    @if (request()->routeIs('dboard.allurl'))
-        <p class="text-sm text-slate-600 dark:text-dark-400">
-            Created by
-            <a href="{{ route('dboard.allurl.u-user', $model->author) }}" class="underline decoration-dotted">
-                {{ $model->author->name }}
-            </a>
-        </p>
+@if (request()->routeIs('dboard.allurl'))
+<div class="text-sm text-slate-600 dark:text-dark-400 inline">
+    @svg('icon-person', 'text-primary-600')
+    <a href="{{ route('dboard.allurl.u-user', $model->author) }}" class="underline decoration-dotted">
+        {{ $model->author->name }}
+    </a>
+</div>
+@endif
+
+<div class="text-sm text-slate-600 dark:text-dark-400 inline">
+    @if ($model->password)
+    @svg('icon-key', 'text-emerald-600 size-[14px] ml-1', ['title' => 'Password protected'])
+    Password protected
+    @endif
+
+    @if ($model->isExpired())
+    @svg('link-expired', 'text-orange-600 size-[16px] ml-1')
+    Expired
     @endif
 </div>
--- resources/views/components/table/keyword.blade.php
+++ resources/views/components/table/keyword.blade.php
@@ -1,14 +1,4 @@
 <div class="mb-2">
-    <div class="mb-1">
-        @if ($model->password)
-            @svg('icon-key', 'text-emerald-600 size-[10px]', ['title' => 'Password protected'])
-        @endif
-
-        @if ($model->isExpired())
-            @svg('link-expired', 'text-orange-600 size-4', ['title' => 'This link has expired'])
-        @endif
-    </div>
-
     <a href="{{ $model->short_url }}" title="{{ $model->keyword }}" target="_blank" class="bg-primary-50 dark:bg-dark-800 text-primary-700 dark:text-emerald-500 font-light p-1 rounded">
         {{ mb_strimwidth($model->keyword, 0, 15, '...') }}
     </a>
