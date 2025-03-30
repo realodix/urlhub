ebb12fd35c02605f017e00e7b7810cbf3c47fe89
Kei <budi03@gmail.com>
2025-03-30 07:41:45 +0700

    change detail icon

--- resources/views/components/table/action-button.blade.php
+++ resources/views/components/table/action-button.blade.php
@@ -2,7 +2,7 @@
     <a role="button" href="{{ $detail_link }}" target="_blank" title="{{ __('Details') }}"
         class="btn btn-secondary btn-square btn-xs"
     >
-        @svg('icon-chart-line')
+        @svg('icon-item-detail')
     </a>
     <a role="button" href="{{ $delete_link }}" title="{{ __('Delete') }}"
         class="btn btn-delete btn-square btn-xs"
