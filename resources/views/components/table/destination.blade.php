9f43bff3422358156ebf847640626042d22660b9
Kei <budi03@gmail.com>
2025-04-04 04:03:29 +0700

    Update destination.blade.php

--- resources/views/components/table/destination.blade.php
+++ resources/views/components/table/destination.blade.php
@@ -18,7 +18,7 @@
 </div>
 
 @if (request()->routeIs('dboard.allurl'))
-<span class="text-[11px] text-indigo-600 dark:text-indigo-900 bg-indigo-100 dark:bg-indigo-300 rounded-sm px-1">
+<span class="text-[11px] text-indigo-600 dark:text-indigo-300 bg-indigo-100 dark:bg-dark-800 rounded-sm px-1">
     @svg('icon-person')
     <a href="{{ route('dboard.allurl.u-user', $model->author) }}">
         {{ $model->author->name }}
@@ -27,14 +27,14 @@
 @endif
 
 @if ($model->password)
-<span class="text-[11px] text-emerald-600 dark:text-emerald-900 bg-emerald-100 dark:bg-emerald-300 rounded-sm px-1 ml-1">
+<span class="text-[11px] text-emerald-600 dark:text-emerald-300 bg-emerald-100 dark:bg-dark-800 rounded-sm px-1 ml-1">
     @svg('icon-key', 'size-[14px]', ['title' => 'Password protected'])
     Password protected
 </span>
 @endif
 
 @if ($model->isExpired())
-<span class="text-[11px] text-orange-600 dark:text-orange-900 bg-orange-100 dark:bg-orange-300 rounded-sm px-1 ml-1">
+<span class="text-[11px] text-orange-600 dark:text-orange-300 bg-orange-100 dark:bg-dark-800 rounded-sm px-1 ml-1">
     @svg('link-expired', 'size-[16px]')
     Expired
 </span>
