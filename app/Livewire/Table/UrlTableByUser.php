cb29c35b5772960fb2024015f939124a0be7d590
Kei <budi03@gmail.com>
2025-04-11 00:18:47 +0700

    url_table: auth user

--- app/Livewire/Table/UrlTableByUser.php
+++ app/Livewire/Table/UrlTableByUser.php
@@ -7,12 +7,14 @@ use Illuminate\Database\Eloquent\Builder;
 /**
  * @codeCoverageIgnore
  */
-final class MyUrlTable extends BaseUrlTable
+final class UrlTableByUser extends BaseUrlTable
 {
-    public string $tableName = 'my-url-table';
+    public string $tableName = 'url_table_by_user';
+
+    public ?int $user_id;
 
     protected function scopeByUser(Builder $query): Builder
     {
-        return $query->where('urls.user_id', auth()->id());
+        return $query->where('urls.user_id', $this->user_id);
     }
 }
