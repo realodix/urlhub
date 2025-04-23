a7a0ddabd30678a17f67d412869bfca12ea316e7
Kei <budi03@gmail.com>
2025-04-23 11:14:39 +0700

    keyword space used

--- app/Providers/MacroServiceProvider.php
+++ app/Providers/MacroServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 
 class MacroServiceProvider extends ServiceProvider
@@ -26,5 +27,14 @@ class MacroServiceProvider extends ServiceProvider
 
             return self::this()->copy()->tz($userTimezone);
         });
+
+        // A SQLite UDF for the REGEXP keyword that mimics the behavior in MySQL.
+        if (DB::connection() instanceof \Illuminate\Database\SQLiteConnection) {
+            DB::connection()->getPdo()->sqliteCreateFunction('REGEXP', function (string $pattern, string $value) {
+                mb_regex_encoding('UTF-8');
+
+                return mb_ereg($pattern, $value) !== false ? 1 : 0;
+            });
+        }
     }
 }
