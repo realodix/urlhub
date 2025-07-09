4e1659dbf4cd5412d2bf14b1a9474af6cfe052f5
Kei <budi03@gmail.com>
2025-07-09 08:05:02 +0700

    Convert to anonymous component

--- resources/views/components/flat-pickr.blade.php
+++ resources/views/components/flat-pickr.blade.php
@@ -1,10 +1,30 @@
+@props([
+    'name',
+    'id' => null,
+    'value' => '',
+    'format' => 'Y-m-d H:i',
+    'placeholder' => null,
+    'options' => [],
+])
+
+@php
+    $id = $id ?? $name;
+    $value = old($name, $value ?? '');
+    $placeholder = $placeholder ?? $format;
+    $componentOptions = array_merge([
+        'dateFormat' => $format,
+        'altInput' => true,
+        'enableTime' => true,
+    ], (array) $options);
+@endphp
+
 <div wire:ignore>
     <input
         x-data="{ picker: null, }"
         x-init="$nextTick(() => {
             if (picker) return;
 
-            picker = flatpickr($root, {{ $jsonOptions() }});
+            picker = flatpickr($root, {{ json_encode((object) $componentOptions) }});
         })"
         name="{{ $name }}"
         type="text"
