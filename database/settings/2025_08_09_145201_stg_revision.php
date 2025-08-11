c93f8c1e74bd0dd6b7fe3f48cd0a8948fd8e7074
Kei <budi03@gmail.com>
2025-08-11 23:44:44 +0700

    Combine multiple settings-related migrations into a single file

--- database/settings/2025_08_09_145201_stg_revision.php
+++ database/settings/2025_08_09_145201_stg_revision.php
@@ -6,10 +6,15 @@ return new class extends SettingsMigration
 {
     public function up(): void
     {
+        // 2025_07_09_172711
+        if (! $this->migrator->exists('general.favicon_provider')) {
+            $this->migrator->add('general.favicon_provider', 'google');
+        }
+
+        // 2025_08_09_145201
         if (! $this->migrator->exists('general.public_shortening')) {
             $this->migrator->rename('general.anyone_can_shorten', 'general.public_shortening');
         }
-
         if (! $this->migrator->exists('general.public_registration')) {
             $this->migrator->rename('general.anyone_can_register', 'general.public_registration');
         }
